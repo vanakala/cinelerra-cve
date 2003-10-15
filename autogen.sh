9dc9906a6180f5943cb582f14df6545b758a45aa
Gergely Erdelyi <dyce@rovidzar.hu>
2003-10-15 17:39:10 +0000

    r82: Workaround for the 'missing )' gettextize bug

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,11 @@
 echo "Running gettextize ..." &&
 gettextize --force &&
 
+# Workaround for the 'missing )' gettextize bug
+# Ugly but works
+sed 's/AM_GNU_GETTEXT_VERSION(\([.0-9]\+\))\?/AM_GNU_GETTEXT_VERSION(\1)/' < configure.in >configure.in.tmp 
+mv -f configure.in.tmp configure.in
+
 echo "Running libtoolize ..."
 libtoolize --force &&
 
