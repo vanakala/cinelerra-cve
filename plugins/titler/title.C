6e2610974b739fe863607f715214d5c32610be20
Gergely Erdelyi <dyce@rovidzar.hu>
2003-10-15 09:18:43 +0000

    r81: FreeType compilation fix. FreeType includes are not version dependent anymore.

--- plugins/titler/title.C
+++ plugins/titler/title.C
@@ -7,10 +7,12 @@
 #include "colormodels.h"
 #include "filexml.h"
 #include "filesystem.h"
-#include "freetype/ftbbox.h"
-#include "freetype/ftglyph.h"
-#include "freetype/ftoutln.h"
-#include "freetype/ftstroke.h"
+#include "ft2build.h"
+#include FT_GLYPH_H
+#include FT_BBOX_H
+#include FT_OUTLINE_H
+#include FT_STROKER_H
+
 #include "picon_png.h"
 #include "plugincolors.h"
 #include "title.h"
