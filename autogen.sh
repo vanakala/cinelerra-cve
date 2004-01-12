e8a25a3d36bf089e457969da923868a014ac01ad
Johannes Sixt <johannes.sixt@telecom.at>
2004-01-12 19:13:08 +0000

    r168: Don't invoke gettextize; this fixes the duplicate po/Makefile.in problem.

--- autogen.sh
+++ autogen.sh
@@ -2,15 +2,6 @@
 
 # autogen.sh for Cinelerra Unofficial CVS  Oct 14, 2003
 
-echo "Running gettextize ..." &&
-gettextize --force &&
-
-# Workaround for the 'missing )' gettextize bug
-# Ugly but works
-sed 's/AM_GNU_GETTEXT_VERSION(\([.0-9]\+\))\?/AM_GNU_GETTEXT_VERSION(\1)/' < configure.in >configure.in.tmp 
-mv -f configure.in.tmp configure.in
-
-
 echo "Running libtoolize ..."
 libtoolize --force &&
 
