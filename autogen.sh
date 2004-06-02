8ecc3066e89275ef9b0a119a08d1378018d936c2
Johannes Sixt <johannes.sixt@telecom.at>
2004-06-02 19:59:58 +0000

    r296: config.h.in is generated automatically by autoheader.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# autogen.sh for Cinelerra Unofficial CVS  Oct 14, 2003
+# autogen.sh for Cinelerra Unofficial CVS
 
 echo "Running libtoolize ..."
 libtoolize --force &&
+
+echo "Running autoheader ..." &&
+autoheader &&
 
 echo "Running aclocal -I m4 ..." &&
 aclocal -I m4 &&
@@ -14,8 +17,11 @@ automake --foreign --add-missing &&
 echo "Running autoconf ..." &&
 autoconf &&
 
-echo "Running libtoolize ..."
+echo "Running libtoolize in libsndfile ..."
 (cd libsndfile && libtoolize --force) &&
+
+echo "Running autoheader in libsndfile ..." &&
+(cd libsndfile && autoheader) &&
 
 echo "Running aclocal in libsndfile ..." &&
 (cd libsndfile && aclocal) &&
