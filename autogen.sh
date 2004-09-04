7a887c210f0f4c69edb05c81cf8cbf2573549c7f
Nathan Kurz <nate@verse.com>
2004-09-04 06:38:46 +0000

    r405: Changed so aclocal runs first.
    Apologies for changing something I don't much understand, but on my
    system at least, I've never been able to checkout and build without
    having to blunder around blindly typing 'aclocal' here and there.
    With this, I seem to be able to autogen.sh, configure, make.

--- autogen.sh
+++ autogen.sh
@@ -14,14 +14,14 @@ case "$amversion" in
 esac
 # automake ok
 
+echo "Running aclocal -I m4 ..." &&
+aclocal -I m4 &&
+
 echo "Running libtoolize ..."
 libtoolize --force &&
 
 echo "Running autoheader ..." &&
 autoheader &&
-
-echo "Running aclocal -I m4 ..." &&
-aclocal -I m4 &&
 
 echo "Running automake ..." &&
 automake --foreign --add-missing &&
@@ -29,14 +29,14 @@ automake --foreign --add-missing &&
 echo "Running autoconf ..." &&
 autoconf &&
 
+echo "Running aclocal in libsndfile ..." &&
+(cd libsndfile && aclocal) &&
+
 echo "Running libtoolize in libsndfile ..."
 (cd libsndfile && libtoolize --force) &&
 
 echo "Running autoheader in libsndfile ..." &&
 (cd libsndfile && autoheader) &&
-
-echo "Running aclocal in libsndfile ..." &&
-(cd libsndfile && aclocal) &&
 
 echo "Running automake in libsndfile ..." &&
 (cd libsndfile && automake --foreign --add-missing) &&
