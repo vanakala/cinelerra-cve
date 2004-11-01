280cabe15c9fa04b43bb0ea5985b010992b75b26
Andraz Tori <Andraz.tori1@guest.arnes.si>
2004-11-01 14:54:47 +0000

    r459: small fix for initialization

--- cinelerra/playbackconfig.C
+++ cinelerra/playbackconfig.C
@@ -3,11 +3,11 @@
 #include "videodevice.inc"
 #include <string.h>
 
-AudioOutConfig::AudioOutConfig(int playback_strategy, int engine_number, int duplex)
+AudioOutConfig::AudioOutConfig(int duplex)
 {
 	this->duplex = duplex;
-	this->playback_strategy = playback_strategy;
-	this->engine_number = engine_number;
+
+	fragment_size = 16384;
 	driver = AUDIO_OSS;
 
 	oss_out_bits = 16;
@@ -21,7 +21,7 @@ AudioOutConfig::AudioOutConfig(int playback_strategy, int engine_number, int dup
 	sprintf(esound_out_server, "");
 	esound_out_port = 0;
 
-	sprintf(alsa_out_device, "");
+	sprintf(alsa_out_device, "default");
 	alsa_out_channels = 2;
 	alsa_out_bits = 16;
 
@@ -54,7 +54,8 @@ int AudioOutConfig::operator!=(AudioOutConfig &that)
 int AudioOutConfig::operator==(AudioOutConfig &that)
 {
 	return 
-		(driver == that.driver) &&
+		(fragment_size == that.fragment_size &&
+		driver == that.driver) &&
 		!strcmp(oss_out_device[0], that.oss_out_device[0]) && 
 		(oss_out_channels[0] == that.oss_out_channels[0]) && 
 		(oss_out_bits == that.oss_out_bits) && 
@@ -81,54 +82,51 @@ int AudioOutConfig::operator==(AudioOutConfig &that)
 
 AudioOutConfig& AudioOutConfig::operator=(AudioOutConfig &that)
 {
-	driver = that.driver;
-	strcpy(esound_out_server, that.esound_out_server);
-	esound_out_port = that.esound_out_port;
+	copy_from(&that);
+	return *this;
+}
+
+void AudioOutConfig::copy_from(AudioOutConfig *src)
+{
+	fragment_size = src->fragment_size;
+	driver = src->driver;
+	strcpy(esound_out_server, src->esound_out_server);
+	esound_out_port = src->esound_out_port;
 	for(int i = 0; i < MAXDEVICES; i++)
 	{
-		oss_enable[i] = that.oss_enable[i];
-		strcpy(oss_out_device[i], that.oss_out_device[i]);
-		oss_out_channels[i] = that.oss_out_channels[i];
+		oss_enable[i] = src->oss_enable[i];
+		strcpy(oss_out_device[i], src->oss_out_device[i]);
+		oss_out_channels[i] = src->oss_out_channels[i];
 	}
-	oss_out_bits = that.oss_out_bits;
+	oss_out_bits = src->oss_out_bits;
 
-	strcpy(alsa_out_device, that.alsa_out_device);
-	alsa_out_channels = that.alsa_out_channels;
-	alsa_out_bits = that.alsa_out_bits;
+	strcpy(alsa_out_device, src->alsa_out_device);
+	alsa_out_channels = src->alsa_out_channels;
+	alsa_out_bits = src->alsa_out_bits;
 
-	firewire_channels = that.firewire_channels;
-	firewire_channel = that.firewire_channel;
-	firewire_port = that.firewire_port;
-	firewire_syt = that.firewire_syt;
-	strcpy(firewire_path, that.firewire_path);
+	firewire_channels = src->firewire_channels;
+	firewire_channel = src->firewire_channel;
+	firewire_port = src->firewire_port;
+	firewire_syt = src->firewire_syt;
+	strcpy(firewire_path, src->firewire_path);
 
-	dv1394_channels = that.dv1394_channels;
-	dv1394_channel = that.dv1394_channel;
-	dv1394_port = that.dv1394_port;
-	dv1394_syt = that.dv1394_syt;
-	strcpy(dv1394_path, that.dv1394_path);
+	dv1394_channels = src->dv1394_channels;
+	dv1394_channel = src->dv1394_channel;
+	dv1394_port = src->dv1394_port;
+	dv1394_syt = src->dv1394_syt;
+	strcpy(dv1394_path, src->dv1394_path);
 
 	for(int i = 0; i < MAXCHANNELS; i++)
-		do_channel[i] = that.do_channel[i];
-// printf("AudioOutConfig::operator= ");
-// for(int i = 0; i < MAXCHANNELS; i++)
-// printf("%d", do_channel[i]);
-// printf("\n");
-	return *this;
+		do_channel[i] = src->do_channel[i];
 }
 
 int AudioOutConfig::load_defaults(Defaults *defaults)
 {
 	char string[BCTEXTLEN];
 
-	sprintf(string, "AUDIO_OUT_DRIVER_%d_%d_%d", playback_strategy, engine_number, duplex);
+	fragment_size = defaults->get("FRAGMENT_SIZE", fragment_size);
+	sprintf(string, "AUDIO_OUT_DRIVER_%d", duplex);
 	driver =  		              defaults->get(string, driver);
-// 	sprintf(string, "OSS_OUT_DEVICE_%d_%d_%d", playback_strategy, engine_number, duplex);
-// 	defaults->get(string, oss_out_device);
-// 	sprintf(string, "OSS_OUT_CHANNELS_%d_%d_%d", playback_strategy, engine_number, duplex);
-// 	oss_out_channels =  		  defaults->get(string, oss_out_channels);
-// 	sprintf(string, "OSS_OUT_BITS_%d_%d_%d", playback_strategy, engine_number, duplex);
-// 	oss_out_bits =                defaults->get(string, oss_out_bits);
 
 	for(int i = 0; i < MAXDEVICES; i++)
 	{
@@ -146,31 +144,31 @@ int AudioOutConfig::load_defaults(Defaults *defaults)
 	alsa_out_channels = defaults->get("ALSA_OUT_CHANNELS", alsa_out_channels);
 	alsa_out_bits = defaults->get("ALSA_OUT_BITS", alsa_out_bits);
 
-	sprintf(string, "ESOUND_OUT_SERVER_%d_%d_%d", playback_strategy, engine_number, duplex);
+	sprintf(string, "ESOUND_OUT_SERVER_%d", duplex);
 	defaults->get(string, esound_out_server);
-	sprintf(string, "ESOUND_OUT_PORT_%d_%d_%d", playback_strategy, engine_number, duplex);
+	sprintf(string, "ESOUND_OUT_PORT_%d", duplex);
 	esound_out_port =             defaults->get(string, esound_out_port);
 
-	sprintf(string, "AFIREWIRE_OUT_CHANNELS_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_CHANNELS");
 	firewire_channels = defaults->get(string, firewire_channels);
-	sprintf(string, "AFIREWIRE_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_CHANNEL");
 	firewire_channel = defaults->get(string, firewire_channel);
-	sprintf(string, "AFIREWIRE_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_PORT");
 	firewire_port = defaults->get(string, firewire_port);
-	sprintf(string, "AFIREWIRE_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_PATH");
 	defaults->get(string, firewire_path);
-	sprintf(string, "AFIREWIRE_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_SYT");
 	firewire_syt = defaults->get(string, firewire_syt);
 
-	sprintf(string, "ADV1394_OUT_CHANNELS_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_CHANNELS");
 	dv1394_channels = defaults->get(string, dv1394_channels);
-	sprintf(string, "ADV1394_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_CHANNEL");
 	dv1394_channel = defaults->get(string, dv1394_channel);
-	sprintf(string, "ADV1394_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_PORT");
 	dv1394_port = defaults->get(string, dv1394_port);
-	sprintf(string, "ADV1394_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_PATH");
 	defaults->get(string, dv1394_path);
-	sprintf(string, "ADV1394_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_SYT");
 	dv1394_syt = defaults->get(string, dv1394_syt);
 
 	return 0;
@@ -180,14 +178,9 @@ int AudioOutConfig::save_defaults(Defaults *defaults)
 {
 	char string[BCTEXTLEN];
 
-	sprintf(string, "AUDIO_OUT_DRIVER_%d_%d_%d", playback_strategy, engine_number, duplex);
+	defaults->update("FRAGMENT_SIZE", fragment_size);
+	sprintf(string, "AUDIO_OUT_DRIVER_%d", duplex);
 	defaults->update(string, driver);
-// 	sprintf(string, "OSS_OUT_DEVICE_%d_%d_%d", playback_strategy, engine_number, duplex);
-// 	defaults->update(string, oss_out_device);
-// 	sprintf(string, "OSS_OUT_CHANNELS_%d_%d_%d", playback_strategy, engine_number, duplex);
-// 	defaults->update(string, oss_out_channels);
-// 	sprintf(string, "OSS_OUT_BITS_%d_%d_%d", playback_strategy, engine_number, duplex);
-// 	defaults->update(string, oss_out_bits);
 
 	for(int i = 0; i < MAXDEVICES; i++)
 	{
@@ -206,32 +199,32 @@ int AudioOutConfig::save_defaults(Defaults *defaults)
 	defaults->update("ALSA_OUT_CHANNELS", alsa_out_channels);
 	defaults->update("ALSA_OUT_BITS", alsa_out_bits);
 
-	sprintf(string, "ESOUND_OUT_SERVER_%d_%d_%d", playback_strategy, engine_number, duplex);
+	sprintf(string, "ESOUND_OUT_SERVER_%d", duplex);
 	defaults->update(string, esound_out_server);
-	sprintf(string, "ESOUND_OUT_PORT_%d_%d_%d", playback_strategy, engine_number, duplex);
+	sprintf(string, "ESOUND_OUT_PORT_%d", duplex);
 	defaults->update(string, esound_out_port);
 
-	sprintf(string, "AFIREWIRE_OUT_CHANNELS_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_CHANNELS");
 	defaults->update(string, firewire_channels);
-	sprintf(string, "AFIREWIRE_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_CHANNEL");
 	defaults->update(string, firewire_channel);
-	sprintf(string, "AFIREWIRE_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_PORT");
 	defaults->update(string, firewire_port);
-	sprintf(string, "AFIREWIRE_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_PATH");
 	defaults->update(string, firewire_path);
-	sprintf(string, "AFIREWIRE_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "AFIREWIRE_OUT_SYT");
 	defaults->update(string, firewire_syt);
 
 
-	sprintf(string, "ADV1394_OUT_CHANNELS_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_CHANNELS");
 	defaults->update(string, dv1394_channels);
-	sprintf(string, "ADV1394_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_CHANNEL");
 	defaults->update(string, dv1394_channel);
-	sprintf(string, "ADV1394_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_PORT");
 	defaults->update(string, dv1394_port);
-	sprintf(string, "ADV1394_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_PATH");
 	defaults->update(string, dv1394_path);
-	sprintf(string, "ADV1394_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "ADV1394_OUT_SYT");
 	defaults->update(string, dv1394_syt);
 
 	return 0;
@@ -306,12 +299,10 @@ int AudioOutConfig::playable_channel_number(int number)
 
 
 
-VideoOutConfig::VideoOutConfig(int playback_strategy, int engine_number)
+VideoOutConfig::VideoOutConfig()
 {
 	sprintf(lml_out_device, "/dev/mvideo/stream");
 	sprintf(buz_out_device, "/dev/video0");
-	this->playback_strategy = playback_strategy;
-	this->engine_number = engine_number;
 	driver = PLAYBACK_X11_XV;
 	for(int i = 0; i < MAX_CHANNELS; i++) do_channel[i] = 0;
 	buz_out_channel = 0;
@@ -328,6 +319,12 @@ VideoOutConfig::VideoOutConfig(int playback_strategy, int engine_number)
 	dv1394_port = 0;
 	strcpy(dv1394_path, "/dev/dv1394");
 	dv1394_syt = 30000;
+
+	brightness = 32768;
+	hue = 32768;
+	color = 32768;
+	contrast = 32768;
+	whiteness = 32768;
 }
 
 VideoOutConfig::~VideoOutConfig()
@@ -373,27 +370,31 @@ int VideoOutConfig::operator==(VideoOutConfig &that)
 
 VideoOutConfig& VideoOutConfig::operator=(VideoOutConfig &that)
 {
-	this->driver = that.driver;
-	strcpy(this->lml_out_device, that.lml_out_device);
-	strcpy(this->buz_out_device, that.buz_out_device);
-	this->buz_out_channel = that.buz_out_channel;
-	this->buz_swap_fields = that.buz_swap_fields;
-	strcpy(this->x11_host, that.x11_host);
-	this->x11_use_fields = that.x11_use_fields;
-	for(int i = 0; i < MAX_CHANNELS; i++) 
-		this->do_channel[i] = that.do_channel[i];
-
-	firewire_channel = that.firewire_channel;
-	firewire_port = that.firewire_port;
-	strcpy(firewire_path, that.firewire_path);
-	firewire_syt = that.firewire_syt;
-
-	dv1394_channel = that.dv1394_channel;
-	dv1394_port = that.dv1394_port;
-	strcpy(dv1394_path, that.dv1394_path);
-	dv1394_syt = that.dv1394_syt;
-
+	copy_from(&that);
 	return *this;
+}
+
+void VideoOutConfig::copy_from(VideoOutConfig *src)
+{
+	this->driver = src->driver;
+	strcpy(this->lml_out_device, src->lml_out_device);
+	strcpy(this->buz_out_device, src->buz_out_device);
+	this->buz_out_channel = src->buz_out_channel;
+	this->buz_swap_fields = src->buz_swap_fields;
+	strcpy(this->x11_host, src->x11_host);
+	this->x11_use_fields = src->x11_use_fields;
+	for(int i = 0; i < MAX_CHANNELS; i++) 
+		this->do_channel[i] = src->do_channel[i];
+
+	firewire_channel = src->firewire_channel;
+	firewire_port = src->firewire_port;
+	strcpy(firewire_path, src->firewire_path);
+	firewire_syt = src->firewire_syt;
+
+	dv1394_channel = src->dv1394_channel;
+	dv1394_port = src->dv1394_port;
+	strcpy(dv1394_path, src->dv1394_path);
+	dv1394_syt = src->dv1394_syt;
 }
 
 char* VideoOutConfig::get_path()
@@ -420,38 +421,38 @@ char* VideoOutConfig::get_path()
 int VideoOutConfig::load_defaults(Defaults *defaults)
 {
 	char string[BCTEXTLEN];
-	sprintf(string, "VIDEO_OUT_DRIVER_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VIDEO_OUT_DRIVER");
 	driver = defaults->get(string, driver);
-	sprintf(string, "LML_OUT_DEVICE_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "LML_OUT_DEVICE");
 	defaults->get(string, lml_out_device);
-	sprintf(string, "BUZ_OUT_DEVICE_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "BUZ_OUT_DEVICE");
 	defaults->get(string, buz_out_device);
-	sprintf(string, "BUZ_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "BUZ_OUT_CHANNEL");
 	buz_out_channel = defaults->get(string, buz_out_channel);
-	sprintf(string, "BUZ_SWAP_FIELDS_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "BUZ_SWAP_FIELDS");
 	buz_swap_fields = defaults->get(string, buz_swap_fields);
-	sprintf(string, "X11_OUT_DEVICE_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "X11_OUT_DEVICE");
 	defaults->get(string, x11_host);
 	x11_use_fields = defaults->get("X11_USE_FIELDS", x11_use_fields);
 
 
 
-	sprintf(string, "VFIREWIRE_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_CHANNEL");
 	firewire_channel = defaults->get(string, firewire_channel);
-	sprintf(string, "VFIREWIRE_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_PORT");
 	firewire_port = defaults->get(string, firewire_port);
-	sprintf(string, "VFIREWIRE_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_PATH");
 	defaults->get(string, firewire_path);
-	sprintf(string, "VFIREWIRE_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_SYT");
 	firewire_syt = defaults->get(string, firewire_syt);
 
-	sprintf(string, "VDV1394_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_CHANNEL");
 	dv1394_channel = defaults->get(string, dv1394_channel);
-	sprintf(string, "VDV1394_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_PORT");
 	dv1394_port = defaults->get(string, dv1394_port);
-	sprintf(string, "VDV1394_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_PATH");
 	defaults->get(string, dv1394_path);
-	sprintf(string, "VDV1394_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_SYT");
 	dv1394_syt = defaults->get(string, dv1394_syt);
 	return 0;
 }
@@ -459,36 +460,36 @@ int VideoOutConfig::load_defaults(Defaults *defaults)
 int VideoOutConfig::save_defaults(Defaults *defaults)
 {
 	char string[BCTEXTLEN];
-	sprintf(string, "VIDEO_OUT_DRIVER_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VIDEO_OUT_DRIVER");
 	defaults->update(string, driver);
-	sprintf(string, "LML_OUT_DEVICE_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "LML_OUT_DEVICE");
 	defaults->update(string, lml_out_device);
-	sprintf(string, "BUZ_OUT_DEVICE_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "BUZ_OUT_DEVICE");
 	defaults->update(string, buz_out_device);
-	sprintf(string, "BUZ_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "BUZ_OUT_CHANNEL");
 	defaults->update(string, buz_out_channel);
-	sprintf(string, "BUZ_SWAP_FIELDS_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "BUZ_SWAP_FIELDS");
 	defaults->update(string, buz_swap_fields);
-	sprintf(string, "X11_OUT_DEVICE_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "X11_OUT_DEVICE");
 	defaults->update(string, x11_host);
 	defaults->update("X11_USE_FIELDS", x11_use_fields);
 
-	sprintf(string, "VFIREWIRE_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_CHANNEL");
 	defaults->update(string, firewire_channel);
-	sprintf(string, "VFIREWIRE_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_PORT");
 	defaults->update(string, firewire_port);
-	sprintf(string, "VFIREWIRE_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_PATH");
 	defaults->update(string, firewire_path);
-	sprintf(string, "VFIREWIRE_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VFIREWIRE_OUT_SYT");
 	defaults->update(string, firewire_syt);
 
-	sprintf(string, "VDV1394_OUT_CHANNEL_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_CHANNEL");
 	defaults->update(string, dv1394_channel);
-	sprintf(string, "VDV1394_OUT_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_PORT");
 	defaults->update(string, dv1394_port);
-	sprintf(string, "VDV1394_OUT_PATH_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_PATH");
 	defaults->update(string, dv1394_path);
-	sprintf(string, "VDV1394_OUT_SYT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "VDV1394_OUT_SYT");
 	defaults->update(string, dv1394_syt);
 	return 0;
 }
@@ -509,12 +510,10 @@ int VideoOutConfig::total_playable_channels()
 
 
 
-PlaybackConfig::PlaybackConfig(int playback_strategy, int engine_number)
+PlaybackConfig::PlaybackConfig()
 {
-	aconfig = new AudioOutConfig(playback_strategy, engine_number, 0);
-	vconfig = new VideoOutConfig(playback_strategy, engine_number);
-	this->playback_strategy = playback_strategy;
-	this->engine_number = engine_number;
+	aconfig = new AudioOutConfig(0);
+	vconfig = new VideoOutConfig;
 	sprintf(hostname, "localhost");
 	port = 23456;
 }
@@ -527,19 +526,24 @@ PlaybackConfig::~PlaybackConfig()
 
 PlaybackConfig& PlaybackConfig::operator=(PlaybackConfig &that)
 {
-	*aconfig = *that.aconfig;
-	*vconfig = *that.vconfig;
-	strcpy(hostname, that.hostname);
-	port = that.port;
+	copy_from(&that);
 	return *this;
+}
+
+void PlaybackConfig::copy_from(PlaybackConfig *src)
+{
+	aconfig->copy_from(src->aconfig);
+	vconfig->copy_from(src->vconfig);
+	strcpy(hostname, src->hostname);
+	port = src->port;
 }
 
 int PlaybackConfig::load_defaults(Defaults *defaults)
 {
 	char string[1024];
-	sprintf(string, "PLAYBACK_HOSTNAME_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "PLAYBACK_HOSTNAME");
 	defaults->get(string, hostname);
-	sprintf(string, "PLAYBACK_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "PLAYBACK_PORT");
 	port = defaults->get(string, port);
 	aconfig->load_defaults(defaults);
 	vconfig->load_defaults(defaults);
@@ -549,9 +553,9 @@ int PlaybackConfig::load_defaults(Defaults *defaults)
 int PlaybackConfig::save_defaults(Defaults *defaults)
 {
 	char string[1024];
-	sprintf(string, "PLAYBACK_HOSTNAME_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "PLAYBACK_HOSTNAME");
 	defaults->update(string, hostname);
-	sprintf(string, "PLAYBACK_PORT_%d_%d", playback_strategy, engine_number);
+	sprintf(string, "PLAYBACK_PORT");
 	defaults->update(string, port);
 	aconfig->save_defaults(defaults);
 	vconfig->save_defaults(defaults);
