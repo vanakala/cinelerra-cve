5bd3b48b5596a061aafcc844c13a8bc109112f26
Andraz Tori <Andraz.tori1@guest.arnes.si>
2005-05-07 08:32:50 +0000

    r558: add two files from previous patch

---
+++ quicktime/interlacemodes.c
@@ -0,0 +1,156 @@
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 
+ * USA
+ */
+#include "interlacemodes.h"
+#include <stdlib.h>
+#include <mjpegtools/yuv4mpeg.h>
+
+void ilaceautofixoption_to_text(char *string, int autofixoption)
+{
+	switch(autofixoption)
+	{
+		case BC_ILACE_AUTOFIXOPTION_AUTO:  	strcpy(string, BC_ILACE_AUTOFIXOPTION_AUTO_T);  	break;
+		case BC_ILACE_AUTOFIXOPTION_MANUAL:	strcpy(string, BC_ILACE_AUTOFIXOPTION_MANUAL_T);	break;
+		default: strcpy(string, BC_ILACE_UNKNOWN_T);	break;
+	}
+}
+
+int ilaceautofixoption_from_text(char *text, int thedefault)
+{
+	if(!strcasecmp(text, BC_ILACE_AUTOFIXOPTION_AUTO_T))  	return BC_ILACE_AUTOFIXOPTION_AUTO;
+	if(!strcasecmp(text, BC_ILACE_AUTOFIXOPTION_MANUAL_T))	return BC_ILACE_AUTOFIXOPTION_MANUAL;
+	return thedefault;
+}
+
+void ilacemode_to_text(char *string, int ilacemode)
+{
+	switch(ilacemode)
+	{
+		case BC_ILACE_MODE_UNDETECTED:   	strcpy(string, BC_ILACE_MODE_UNDETECTED_T);   	break;
+		case BC_ILACE_MODE_ODDLEADS:     	strcpy(string, BC_ILACE_MODE_ODDLEADS_T);     	break;
+		case BC_ILACE_MODE_EVENLEADS:    	strcpy(string, BC_ILACE_MODE_EVENLEADS_T);    	break;
+		case BC_ILACE_MODE_NOTINTERLACED:	strcpy(string, BC_ILACE_MODE_NOTINTERLACED_T);	break;
+		default: strcpy(string, BC_ILACE_UNKNOWN_T); break;
+	}
+}
+
+int ilacemode_from_text(char *text, int thedefault)
+{
+	if(!strcasecmp(text, BC_ILACE_MODE_UNDETECTED_T))   	return BC_ILACE_MODE_UNDETECTED;
+	if(!strcasecmp(text, BC_ILACE_MODE_ODDLEADS_T))     	return BC_ILACE_MODE_ODDLEADS;
+	if(!strcasecmp(text, BC_ILACE_MODE_EVENLEADS_T))    	return BC_ILACE_MODE_EVENLEADS;
+	if(!strcasecmp(text, BC_ILACE_MODE_NOTINTERLACED_T))	return BC_ILACE_MODE_NOTINTERLACED;
+	return thedefault;
+}
+
+void ilacefixmethod_to_text(char *string, int fixmethod)
+{
+	switch(fixmethod)
+	{
+		case BC_ILACE_FIXMETHOD_NONE:   	strcpy(string, BC_ILACE_FIXMETHOD_NONE_T);   	break;
+		case BC_ILACE_FIXMETHOD_UPONE:  	strcpy(string, BC_ILACE_FIXMETHOD_UPONE_T);  	break;
+		case BC_ILACE_FIXMETHOD_DOWNONE:	strcpy(string, BC_ILACE_FIXMETHOD_DOWNONE_T);	break;
+		default: strcpy(string, BC_ILACE_UNKNOWN_T);	break;
+	}
+}
+
+int ilacefixmethod_from_text(char *text, int thedefault)
+{
+	if(!strcasecmp(text, BC_ILACE_FIXMETHOD_NONE_T))   	return BC_ILACE_FIXMETHOD_NONE;
+	if(!strcasecmp(text, BC_ILACE_FIXMETHOD_UPONE_T))  	return BC_ILACE_FIXMETHOD_UPONE;
+	if(!strcasecmp(text, BC_ILACE_FIXMETHOD_DOWNONE_T))	return BC_ILACE_FIXMETHOD_DOWNONE;
+	return thedefault; 
+}
+
+int  ilaceautofixmethod(int projectmode, int assetmode) 
+{
+  if (projectmode == assetmode)
+    return BC_ILACE_FIXMETHOD_NONE;
+      
+  if (
+      (projectmode == BC_ILACE_MODE_EVENLEADS && assetmode == BC_ILACE_MODE_ODDLEADS )
+      ||
+      (projectmode == BC_ILACE_MODE_ODDLEADS  && assetmode == BC_ILACE_MODE_EVENLEADS)
+      )
+    return BC_ILACE_FIXDEFAULT;
+
+  // still to implement anything else...
+   return BC_ILACE_FIXMETHOD_NONE;
+}
+
+int  ilaceautofixmethod2(int projectilacemode, int assetautofixoption, int assetilacemode, int assetfixmethod)
+{
+	if (assetautofixoption == BC_ILACE_AUTOFIXOPTION_AUTO)
+		return (ilaceautofixmethod(projectilacemode, assetilacemode));
+	else
+		return (assetfixmethod);
+}
+
+int ilace_bc_to_yuv4mpeg(int ilacemode) 
+{
+  switch (ilacemode) 
+    {
+    case BC_ILACE_MODE_UNDETECTED:
+      return(Y4M_UNKNOWN);
+      break;
+    case BC_ILACE_MODE_ODDLEADS:
+      return(Y4M_ILACE_TOP_FIRST);
+      break;
+    case BC_ILACE_MODE_EVENLEADS:
+      return(Y4M_ILACE_BOTTOM_FIRST);
+      break;
+    case BC_ILACE_MODE_NOTINTERLACED:
+      return(Y4M_ILACE_NONE);
+      break;
+    }
+}
+
+int ilace_yuv4mpeg_to_bc(int ilacemode) 
+{
+  switch (ilacemode) 
+    {
+    case Y4M_UNKNOWN:
+      return (BC_ILACE_MODE_UNDETECTED);
+      break;
+    case Y4M_ILACE_NONE:
+      return (BC_ILACE_MODE_NOTINTERLACED);
+      break;
+    case Y4M_ILACE_TOP_FIRST:
+      return (BC_ILACE_MODE_ODDLEADS);
+      break;
+    case Y4M_ILACE_BOTTOM_FIRST:
+      return (BC_ILACE_MODE_EVENLEADS);
+      break;
+    case Y4M_ILACE_MIXED:
+      return (BC_ILACE_MODE_UNDETECTED);  // fixme!!
+      break;
+    }      
+}
+
+
+void ilace_yuv4mpeg_mode_to_text(char *string, int ilacemode)
+{
+	switch(ilacemode)
+	{
+	case Y4M_UNKNOWN:     	     	strcpy(string, BC_ILACE_Y4M_UKNOWN_T);   	break;
+	case Y4M_ILACE_NONE:   	     	strcpy(string, BC_ILACE_Y4M_NONE_T);    	break;
+	case Y4M_ILACE_TOP_FIRST:     	strcpy(string, BC_ILACE_Y4M_TOP_FIRST_T);     	break;
+	case Y4M_ILACE_BOTTOM_FIRST:    strcpy(string, BC_ILACE_Y4M_BOTTOM_FIRST_T);   	break;
+	case Y4M_ILACE_MIXED:     	strcpy(string, BC_ILACE_Y4M_MIXED_T);           break;
+
+	default:                        strcpy(string, BC_ILACE_UNKNOWN_T);             break;
+	}
+}
---
+++ quicktime/interlacemodes.h
@@ -0,0 +1,87 @@
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 
+ * USA
+ */
+ 
+#ifndef INTERLACEMODES_H
+#define INTERLACEMODES_H
+
+#define BC_ILACE_UNKNOWN_T      "Error!"
+
+//Interlace Automatic fixing options
+#define BC_ILACE_AUTOFIXOPTION_MANUAL  	0
+#define BC_ILACE_AUTOFIXOPTION_MANUAL_T	"Manual compensation using selection"
+#define BC_ILACE_AUTOFIXOPTION_AUTO    	1
+#define BC_ILACE_AUTOFIXOPTION_AUTO_T  	"Automatic compensation using modes"
+//Note: Do not change what the numbers mean as this will make backward-compatability have erroraneous settings.
+
+//Interlace Modes
+#define BC_ILACE_MODE_UNDETECTED     	0
+#define BC_ILACE_MODE_UNDETECTED_T   	"Unknown"
+#define BC_ILACE_MODE_ODDLEADS       	1
+#define BC_ILACE_MODE_ODDLEADS_T     	"Bottom Fields First"
+#define BC_ILACE_MODE_EVENLEADS      	2
+#define BC_ILACE_MODE_EVENLEADS_T    	"Top Fields First"
+#define BC_ILACE_MODE_NOTINTERLACED  	3
+#define BC_ILACE_MODE_NOTINTERLACED_T	"Not Interlaced"
+
+#define BC_ILACE_ASSET_MODEDEFAULT  	BC_ILACE_MODE_UNDETECTED
+#define BC_ILACE_PROJECT_MODEDEFAULT	BC_ILACE_MODE_NOTINTERLACED_T
+//Note: Do not change what the numbers mean as this will make backward-compatability have erroraneous settings.
+
+//Interlace Compensation Methods
+#define BC_ILACE_FIXMETHOD_NONE     	0
+#define BC_ILACE_FIXMETHOD_NONE_T   	"Do Nothing"
+#define BC_ILACE_FIXMETHOD_UPONE    	1
+#define BC_ILACE_FIXMETHOD_UPONE_T  	"Shift Up 1 pixel"
+#define BC_ILACE_FIXMETHOD_DOWNONE  	2
+#define BC_ILACE_FIXMETHOD_DOWNONE_T	"Shift Down 1 pixel"
+// the following is for project/asset having odd/even, or even/odd  
+#define BC_ILACE_FIXDEFAULT         	BC_ILACE_FIXMETHOD_UPONE
+//Note: Do not change what the numbers mean as this will make backward-compatability have erroraneous settings.
+
+// Refer to <mjpegtools/yuv4mpeg.h> (descriptions where cut-and-pasted!)
+#define BC_ILACE_Y4M_UKNOWN_T         "unknown"
+#define BC_ILACE_Y4M_NONE_T           "non-interlaced, progressive frame"
+#define BC_ILACE_Y4M_TOP_FIRST_T      "interlaced, top-field first"
+#define BC_ILACE_Y4M_BOTTOM_FIRST_T   "interlaced, bottom-field first"
+#define BC_ILACE_Y4M_MIXED_T          "mixed, \"refer to frame header\""
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ilaceautofixoption_to_text(char *string, int autofixoption);
+int  ilaceautofixoption_from_text(char *text, int thedefault);
+
+void ilacemode_to_text(char *string, int ilacemode);
+int  ilacemode_from_text(char *text, int thedefault);
+
+void ilacefixmethod_to_text(char *string, int fixmethod);
+int  ilacefixmethod_from_text(char *text, int thedefault);
+
+int  ilaceautofixmethod(int projectilacemode, int assetilacemode);
+int  ilaceautofixmethod2(int projectilacemode, int assetautofixoption, int assetilacemode, int assetfixmethod);
+
+int ilace_bc_to_yuv4mpeg(int ilacemode);
+int ilace_yuv4mpeg_to_bc(int ilacemode);
+
+void ilace_yuv4mpeg_mode_to_text(char *string, int ilacemode);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // INTERLACEMODES_H
