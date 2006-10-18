fba5d0875e7b6ac2b0c2574d6343b2d2a40d5b32
Andraz Tori <Andraz.tori1@guest.arnes.si>
2006-10-18 12:04:59 +0000

    r941: Fixes bug 340 http://bugs.cinelerra.org/show_bug.cgi?id=340
    The solution is highly suboptimal and might bring performance degradation 
    
    // The proper solution is HARD because of :
    // 1. idiotic xlib does not have XTryLockDisplay - we will need to _completely_ implement XLockDisplay by ourselves (including cascaded locking - it is not the same as mutex!)
    // 2. the locking shemantic inside new lock_window and here will be really tricky, we should:
    //      in lock_window check wheather BC_WindowEvents is in XNextEvent and it is send custom xevent to break out of the loop and make sure lock is not taken again if locck_window is waiting for it
    // 3. Send custom events from previous point through _separate_ xwindows display connection since XSendEvent would need to be protected by XLockDisplay which obviously can't be done inside the same thread!

--- guicast/bcwindowevents.C
+++ guicast/bcwindowevents.C
@@ -1,5 +1,6 @@
 #include "bcwindowbase.h"
 #include "bcwindowevents.h"
+#include "bctimer.h"
 
 BC_WindowEvents::BC_WindowEvents(BC_WindowBase *window)
  : Thread(1, 0, 0)
@@ -35,14 +36,31 @@ void BC_WindowEvents::start()
 
 void BC_WindowEvents::run()
 {
+// Can't cancel in XNextEvent because X server never figures out it's not
+// listening anymore and XCloseDisplay locks up.
 	XEvent *event;
 	while(!done)
 	{
-		event = new XEvent;
-// Can't cancel in XNextEvent because X server never figures out it's not
-// listening anymore and XCloseDisplay locks up.
-		XNextEvent(window->display, event);
-		window->put_event(event);
+// XNextEvent should also be protected by XLockDisplay ...
+// Currently implemented is a hackish solution, FIXME
+// Impact of this solution on the performance has not been analyzed
+
+// The proper solution is HARD because of :
+// 1. idiotic xlib does not have XTryLockDisplay - we will need to _completely_ implement XLockDisplay by ourselves (including cascaded locking - it is not the same as mutex!)
+// 2. the locking shemantic inside new lock_window and here will be really tricky, we should:
+//	in lock_window check wheather BC_WindowEvents is in XNextEvent and it is send custom xevent to break out of the loop and make sure lock is not taken again if lock_window() is waiting on it
+// 3. Send custom events from previous point through _separate_ xwindows display connection since XSendEvent would need to be protected by XLockDisplay which obviously can't be
+
+		window->lock_window();
+		while (XPending(window->display))
+		{
+			event = new XEvent;
+			XNextEvent(window->display, event);
+			window->put_event(event);
+		}
+		
+		window->unlock_window();
+		Timer::delay(20);    // sleep 20ms
 	}
 }
 
