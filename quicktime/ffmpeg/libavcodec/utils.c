f79e6f3545d9546ad7f8b3248f910042a868121e
Andraz Tori <Andraz.tori1@guest.arnes.si>
2007-02-25 08:37:45 +0000

    r1004: Fix http://article.gmane.org/gmane.comp.video.ffmpeg.devel/26558

--- quicktime/ffmpeg/libavcodec/utils.c
+++ quicktime/ffmpeg/libavcodec/utils.c
@@ -332,6 +332,15 @@ int avcodec_default_get_buffer(AVCodecContext *s, AVFrame *pic){
             const int h_shift= i==0 ? 0 : h_chroma_shift;
             const int v_shift= i==0 ? 0 : v_chroma_shift;
 
+	    if(s->pix_fmt == PIX_FMT_PAL8 && i == 1)
+	    {
+	        buf->base[i] = av_malloc(256 * 4);
+	        if(buf->base[i] == NULL)
+	            return -1;
+	        buf->data[i] = buf->base[i];
+	        continue;
+	    }
+
             //FIXME next ensures that linesize= 2^x uvlinesize, thats needed because some MC code assumes it
             buf->linesize[i]= ALIGN(pixel_size*w>>h_shift, STRIDE_ALIGN<<(h_chroma_shift-h_shift)); 
 
