vanakala/cinelerra-cve

commit 4d2d4adef6ab5fcaf8ca0e3e47ae03bb0a0641d6

Changed some defaults used on first start
To be more new user friendly changed some defaults
of cinelerra to more intuitive for a new user.
New defaults
    = YUVA-color
    = play-every-frame disabled,
    = ALSA-audio (if ALSA has configured)
    = v4l2 video input
    = -40.0 to 6.0 for audio fade
    = timeruler of the timeline counting every 2 seconds
    = Hours:Minutes:Seconds:Frames as unit of time in the timeline,
    = duration fo still images = 3 sec
    = compositor zoom set to "auto"
    = 500 index files to keep.
Added icon to tipwindow

cinelerra/recordconfig.C
@@ -28,7 +28,11 @@
 
 AudioInConfig::AudioInConfig()
 {
+#ifdef HAVE_ALSA
+	driver = AUDIO_ALSA;
+#else
 	driver = AUDIO_OSS;
+#endif
 	for(int i = 0; i < MAXDEVICES; i++)
 	{
 		oss_enable[i] = (i == 0);
@@ -147,7 +151,11 @@ int AudioInConfig::save_defaults(BC_Hash *defaults)
 
 VideoInConfig::VideoInConfig()
 {
+#ifdef HAVE_VIDEO4LINUX2
 	driver = VIDEO4LINUX;
+#else
+	driver = VIDEO4LINUX2;
+#endif
 	strcpy(v4l_in_device, "/dev/video0");
 	strcpy(v4l2_in_device, "/dev/video0");
 	strcpy(v4l2jpeg_in_device, "/dev/video0");

cinelerra/edl.inc
@@ -100,7 +100,7 @@ class EDL;
 #define CWINDOW_MASK_CONTROL_OUT 0x16
 #define CWINDOW_MASK_TRANSLATE 0x17
 
-# define DEFAULT_ZOOM_TIME (exp2(-2))
+# define DEFAULT_ZOOM_TIME (exp2(-6))
 // Inclusive limits for zoom panel
 #define MIN_ZOOM_TIME (exp2(-10))
 #define MAX_ZOOM_TIME (exp2(6))

cinelerra/edlsession.C
@@ -172,7 +172,7 @@ int EDLSession::load_defaults(BC_Hash *defaults)
 	auto_conf->load_defaults(defaults);
 	autos_follow_edits = defaults->get("AUTOS_FOLLOW_EDITS", 1);
 	brender_start = defaults->get("BRENDER_START", brender_start);
-	cmodel_to_text(string, BC_RGBA8888);
+	cmodel_to_text(string, BC_YUVA8888);
 	color_model = cmodel_from_text(defaults->get("COLOR_MODEL", string));
 	ilacemode_to_xmltext(string, BC_ILACE_MODE_NOTINTERLACED);
 	interlace_mode = ilacemode_from_xmltext(defaults->get("INTERLACE_MODE",string), BC_ILACE_MODE_NOTINTERLACED);
@@ -187,7 +187,7 @@ int EDLSession::load_defaults(BC_Hash *defaults)
 	cwindow_mask = defaults->get("CWINDOW_MASK", 0);
 	cwindow_meter = defaults->get("CWINDOW_METER", 1);
 	cwindow_operation = defaults->get("CWINDOW_OPERATION", 0);
-	cwindow_scrollbars = defaults->get("CWINDOW_SCROLLBARS", 1);
+	cwindow_scrollbars = defaults->get("CWINDOW_SCROLLBARS", 0);
 	cwindow_xscroll = defaults->get("CWINDOW_XSCROLL", 0);
 	cwindow_yscroll = defaults->get("CWINDOW_YSCROLL", 0);
 	cwindow_zoom = defaults->get("CWINDOW_ZOOM", (float)1);
@@ -237,12 +237,12 @@ int EDLSession::load_defaults(BC_Hash *defaults)
 	safe_regions = defaults->get("SAFE_REGIONS", 1);
 	sample_rate = defaults->get("SAMPLERATE", 48000);
 	scrub_speed = defaults->get("SCRUB_SPEED", (float)2);
-	si_useduration = defaults->get("SI_USEDURATION",0);
-	si_duration = defaults->get("SI_DURATION",5);
-	
+	si_useduration = defaults->get("SI_USEDURATION",1);
+	si_duration = defaults->get("SI_DURATION",3);
+
 	show_assets = defaults->get("SHOW_ASSETS", 1);
 	show_titles = defaults->get("SHOW_TITLES", 1);
-	time_format = defaults->get("TIME_FORMAT", TIME_HMS);
+	time_format = defaults->get("TIME_FORMAT", TIME_HMSF);
 	for(int i = 0; i < 4; i++)
 	{
 		sprintf(string, "TIMECODE_OFFSET_%d", i);
@@ -260,7 +260,7 @@ int EDLSession::load_defaults(BC_Hash *defaults)
 		vchannel_y[i] = defaults->get(string, 0);
 	}
 	video_channels = defaults->get("VCHANNELS", 1);
-	video_every_frame = defaults->get("VIDEO_EVERY_FRAME", 1);
+	video_every_frame = defaults->get("VIDEO_EVERY_FRAME", 0);
 	video_asynchronous = defaults->get("VIDEO_ASYNCHRONOUS", 0);
 	video_tracks = defaults->get("VTRACKS", 1);
 	video_write_length = defaults->get("VIDEO_WRITE_LENGTH", 30);

cinelerra/localsession.C
@@ -70,7 +70,7 @@ LocalSession::LocalSession(EDL *edl)
 	view_start = 0;
 	track_start = 0;
 
-	automation_mins[AUTOGROUPTYPE_AUDIO_FADE] = -80;
+	automation_mins[AUTOGROUPTYPE_AUDIO_FADE] = -40;
 	automation_maxs[AUTOGROUPTYPE_AUDIO_FADE] = 6;
 
 	automation_mins[AUTOGROUPTYPE_VIDEO_FADE] = 0;

cinelerra/playbackconfig.C
@@ -30,7 +30,11 @@ AudioOutConfig::AudioOutConfig(int duplex)
 	this->duplex = duplex;
 
 	fragment_size = 16384;
+#ifdef HAVE_ALSA
+	driver = AUDIO_ALSA;
+#else
 	driver = AUDIO_OSS;
+#endif
 
 	audio_offset = 0.0;
 

cinelerra/preferences.C
@@ -45,7 +45,7 @@ Preferences::Preferences()
 	fs.complete_path(index_directory);
 	cache_size = 0xa00000;
 	index_size = 0x300000;
-	index_count = 100;
+	index_count = 500;
 	use_thumbnails = 1;
 	theme[0] = 0;
 	use_renderfarm = 0;

cinelerra/tipwindow.C
@@ -148,6 +148,8 @@ void TipWindowGUI::create_objects()
 	add_subwindow(tip_text = new BC_Title(x, y, thread->get_current_tip()));
 	y = get_h() - 30;
 
+	set_icon(mwindow->theme->get_image("mwindow_icon"));
+
 	BC_CheckBox *checkbox; 
 	add_subwindow(checkbox = new TipDisable(mwindow, this, x, y));
 

commit 2247d5aed6e4f24393c6178d78607829633c16a6

Fixed video device selection in VideoInConfig
Fixed bug introduced in: 4d2d4adef6ab5fcaf8ca0e3e47ae03bb0a0641d6
Credits: Johannes Sixt, who noticed it

cinelerra/recordconfig.C
@@ -152,9 +152,9 @@ int AudioInConfig::save_defaults(BC_Hash *defaults)
 VideoInConfig::VideoInConfig()
 {
 #ifdef HAVE_VIDEO4LINUX2
-	driver = VIDEO4LINUX;
-#else
 	driver = VIDEO4LINUX2;
+#else
+	driver = VIDEO4LINUX;
 #endif
 	strcpy(v4l_in_device, "/dev/video0");
 	strcpy(v4l2_in_device, "/dev/video0");