412221f5b1193dac10e8132a679fb6f735f7e117
Einar Rünkaru <einarry@smail.ee>
2012-02-10 22:32:22 +0200

    New video transition "Null"
    This is a skeletion for creating new video transitions
    Simple passtrhrough with configurable ability to blank output

---
+++ plugins/nullvidtrans/nullvidtrans.C
@@ -0,0 +1,172 @@
+
+/*
+ * CINELERRA
+ * Copyright (C) 2012 Einar Rünkaru <einarry at smail dot ee>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "bchash.h"
+#include "filexml.h"
+#include "picon_png.h"
+#include "vframe.h"
+#include "nullvidtrans.h"
+
+#include <stdint.h>
+#include <string.h>
+
+// Registers plugin
+REGISTER_PLUGIN
+
+NVSwitch::NVSwitch(NVTransition *plugin, 
+	NVTransitionWindow *window,
+	int x,
+	int y)
+ : BC_Radial(x, 
+		y, 
+		plugin->onoff,
+		_("Clear"))
+{
+	this->plugin = plugin;
+	this->window = window;
+}
+
+int NVSwitch::handle_event()
+{
+	plugin->onoff = plugin->onoff ? 0 : 1;
+	update(plugin->onoff);
+	plugin->send_configure_change();
+	return 0;
+}
+
+/*
+ * The gui of the plugin
+ * Using the constuctor macro is mandatory
+ * The constructor macro must be near the end of constructor after
+ * creating the elements of gui.
+ */
+NVTransitionWindow::NVTransitionWindow(NVTransition *plugin, int x, int y)
+ : BC_Window(plugin->gui_string, 
+	x,
+	y,
+	200,
+	50,
+	200,
+	50,
+	0,
+	0,
+	1)
+{
+	x = y = 10;
+
+	add_subwindow(new BC_Title(x, y, _("Status:")));
+	add_subwindow(toggle = new NVSwitch(plugin,
+		this,
+		x + 60,
+		y));
+	PLUGIN_GUI_CONSTRUCTOR_MACRO
+}
+
+/*
+ * All methods of the plugin thread class
+ */
+PLUGIN_THREAD_METHODS
+
+/*
+ * The processor of the plugin
+ * Using the constructor and the destructor macro is mandatory
+ */
+NVTransition::NVTransition(PluginServer *server)
+ : PluginVClient(server)
+{
+	onoff = 1;
+	PLUGIN_CONSTRUCTOR_MACRO
+}
+
+NVTransition::~NVTransition()
+{
+	PLUGIN_DESTRUCTOR_MACRO
+}
+
+/*
+ * Creates most of the api methods of the plugin
+ */
+PLUGIN_CLASS_METHODS
+
+void NVTransition::load_defaults()
+{
+// Loads defaults from the named rc-file
+	defaults = load_defaults_file("nullvideotransition.rc");
+
+	onoff = defaults->get("ONOFF", onoff);
+}
+
+void NVTransition::save_defaults()
+{
+	defaults->update("ONOFF", onoff);
+	defaults->save();
+}
+
+/*
+ * Saves the values of the parameters to the project
+ */
+void NVTransition::save_data(KeyFrame *keyframe)
+{
+	FileXML output;
+	output.set_shared_string(keyframe->data, MESSAGESIZE);
+	output.tag.set_title("NVTRANSITION");
+	output.tag.set_property("ONOFF", onoff);
+	output.append_tag();
+	output.tag.set_title("/NVTRANSITION");
+	output.append_tag();
+	output.terminate_string();
+}
+
+void NVTransition::read_data(KeyFrame *keyframe)
+{
+	FileXML input;
+
+	input.set_shared_string(keyframe->data, strlen(keyframe->data));
+
+	while(!input.read_tag())
+	{
+		if(input.tag.title_is("NVTRANSITION"))
+		{
+			onoff = input.tag.get_property("ONOFF", onoff);
+		}
+	}
+}
+
+/*
+ * Loads parameters from keyframe
+ */
+int NVTransition::load_configuration()
+{
+	read_data(prev_keyframe_pts(source_pts));
+	return 0;
+}
+
+/*
+ * Here incoming and outgoing frames must me mixed together into
+ * the outgoing frame
+ */
+void NVTransition::process_realtime(VFrame *incoming, VFrame *outgoing)
+{
+	load_configuration();
+
+	if(onoff)
+		outgoing->clear_frame();
+}
---
+++ plugins/nullvidtrans/nullvidtrans.h
@@ -0,0 +1,111 @@
+
+/*
+ * CINELERRA
+ * Copyright (C) 2012 Einar Rünkaru <einarry at smail dot ee>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+/*
+ * Null Video Transition
+ * The main purpose of this plugin is to be a skeletion
+ * of usable video transitions
+ */
+
+#ifndef NULLVIDTRANS_H
+#define NULLVIDTRANS_H
+
+/*
+ * Define the type of the plugin
+ */
+#define PLUGIN_IS_VIDEO
+#define PLUGIN_IS_TRANSITION
+
+/*
+ * Define the name and api related class names of the plugin
+ */
+#define PLUGIN_TITLE N_("Null")
+#define PLUGIN_CLASS NVTransition
+#define PLUGIN_THREAD_CLASS NVTransitionThread
+#define PLUGIN_GUI_CLASS NVTransitionWindow
+
+#include "pluginmacros.h"
+
+#include "language.h"
+#include "pluginvclient.h"
+#include "vframe.inc"
+
+class NVSwitch : public BC_Radial
+{
+public:
+	NVSwitch(NVTransition *plugin,
+		NVTransitionWindow *window,
+		int x,
+		int y);
+
+	int handle_event();
+	NVTransition *plugin;
+	NVTransitionWindow *window;
+};
+
+/*
+ * Gui of the plugin - may be omitted
+ */
+class NVTransitionWindow : public BC_Window
+{
+public:
+	NVTransitionWindow(NVTransition *plugin, int x, int y);
+
+	NVSwitch *toggle;
+	PLUGIN_GUI_CLASS_MEMBERS
+};
+
+/*
+ * Macro creates header part of the plugin thread class
+ * Must be used if the plugin has a gui
+ */
+PLUGIN_THREAD_HEADER
+
+/*
+ * Main class of the plugin
+ */
+class NVTransition : public PluginVClient
+{
+public:
+	NVTransition(PluginServer *server);
+	~NVTransition();
+/*
+ * Macro defines members of the plugin class
+ * Macro must be always used
+ */
+	PLUGIN_CLASS_MEMBERS
+// Processing is here
+	void process_realtime(VFrame *incoming, VFrame *outgoing);
+// Loading and saving of defaults - optional: needed if plugin has parameters
+	void load_defaults();
+	void save_defaults();
+// Loading and saving parameters to the project - optional
+	void save_data(KeyFrame *keyframe);
+	void read_data(KeyFrame *keyframe);
+// Calculates (interpolates) parameter values for current position
+	int load_configuration();
+/*
+ * The parameter of the plugin
+ */
+	int onoff;
+};
+
+#endif
