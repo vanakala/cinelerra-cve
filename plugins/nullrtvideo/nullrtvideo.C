4406887fd294a0044b01a8cc9ffcdd2d4455f1a9
Einar Rünkaru <einarry@smail.ee>
2012-02-12 22:29:47 +0200

    New video realtime plugin "Null"
    This is a skeletion for creating new video realtime plugins
    Simple passtrhrough with configurable ability to clear output

---
+++ plugins/nullrtvideo/nullrtvideo.C
@@ -0,0 +1,185 @@
+
+/*
+ * CINELERRA
+ * Copyright (C) 2012 Einar Rünkaru <einarry at smail dot ee>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "bchash.h"
+#include "filexml.h"
+#include "picon_png.h"
+#include "vframe.h"
+#include "nullrtvideo.h"
+
+#include <stdint.h>
+#include <string.h>
+
+// Registers plugin
+REGISTER_PLUGIN
+
+NRTVideoConfig::NRTVideoConfig()
+{
+	onoff = 0;
+}
+
+NRTVideoSwitch::NRTVideoSwitch(NRTVideo *plugin, 
+	int x,
+	int y)
+ : BC_Radial(x,
+		y,
+		plugin->config.onoff,
+		_("Clear"))
+{
+	this->plugin = plugin;
+}
+
+int NRTVideoSwitch::handle_event()
+{
+	plugin->config.onoff = plugin->config.onoff ? 0 : 1;
+	plugin->send_configure_change();
+	return 0;
+}
+
+/*
+ * The gui of the plugin
+ * Using the constuctor macro is mandatory
+ * The constructor macro must be near the end of constructor after
+ * creating the elements of gui.
+ */
+NRTVideoWindow::NRTVideoWindow(NRTVideo *plugin, int x, int y)
+ : BC_Window(plugin->gui_string, 
+	x,
+	y,
+	200,
+	50,
+	200,
+	50,
+	0,
+	0,
+	1)
+{
+	x = y = 10;
+
+	add_subwindow(new BC_Title(x, y, _("Status:")));
+	add_subwindow(toggle = new NRTVideoSwitch(plugin,
+		x + 60,
+		y));
+	PLUGIN_GUI_CONSTRUCTOR_MACRO
+}
+
+void NRTVideoWindow::update()
+{
+	toggle->update(plugin->config.onoff);
+}
+
+/*
+ * All methods of the plugin thread class
+ */
+PLUGIN_THREAD_METHODS
+
+/*
+ * The processor of the plugin
+ * Using the constructor and the destructor macro is mandatory
+ */
+NRTVideo::NRTVideo(PluginServer *server)
+ : PluginVClient(server)
+{
+	PLUGIN_CONSTRUCTOR_MACRO
+}
+
+NRTVideo::~NRTVideo()
+{
+	PLUGIN_DESTRUCTOR_MACRO
+}
+
+/*
+ * Creates most of the api methods of the plugin
+ */
+PLUGIN_CLASS_METHODS
+
+void NRTVideo::load_defaults()
+{
+// Loads defaults from the named rc-file
+	defaults = load_defaults_file("nullrtvideo.rc");
+
+	config.onoff = defaults->get("ONOFF", config.onoff);
+}
+
+void NRTVideo::save_defaults()
+{
+	defaults->update("ONOFF", config.onoff);
+	defaults->save();
+}
+
+/*
+ * Saves the values of the parameters to the project
+ */
+void NRTVideo::save_data(KeyFrame *keyframe)
+{
+	FileXML output;
+	output.set_shared_string(keyframe->data, MESSAGESIZE);
+	output.tag.set_title("NRTVIDEO");
+	output.tag.set_property("ONOFF", config.onoff);
+	output.append_tag();
+	output.tag.set_title("/NRTVIDEO");
+	output.append_tag();
+	output.terminate_string();
+}
+
+void NRTVideo::read_data(KeyFrame *keyframe)
+{
+	FileXML input;
+
+	input.set_shared_string(keyframe->data, strlen(keyframe->data));
+
+	while(!input.read_tag())
+	{
+		if(input.tag.title_is("NRTVIDEO"))
+		{
+			config.onoff = input.tag.get_property("ONOFF", config.onoff);
+		}
+	}
+}
+
+/*
+ * Loads parameters from keyframe
+ */
+int NRTVideo::load_configuration()
+{
+	int prev_val = config.onoff;
+	read_data(prev_keyframe_pts(source_pts));
+	return !(prev_val == config.onoff);
+}
+
+/*
+ * Pull frame and apply modifications
+ * It is possible to use here
+ * void process_realtime(VFrame *input, VFrame *output)
+ *    - here input is already filled
+ * multichannel plugin gets here arrays of pointers
+ * prototype: void process_frame(VFrame **frame)
+ *    - number of channels is total_in_buffers
+ */
+void NRTVideo::process_frame(VFrame *frame)
+{
+	load_configuration();
+
+	get_frame(frame);
+
+	if(config.onoff)
+		frame->clear_frame();
+}
---
+++ plugins/nullrtvideo/nullrtvideo.h
@@ -0,0 +1,126 @@
+
+/*
+ * CINELERRA
+ * Copyright (C) 2012 Einar Rünkaru <einarry at smail dot ee>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+/*
+ * Null Video realtime plugin
+ * The main purpose of this plugin is to be a skeletion
+ * of an usable video realtime effect
+ */
+
+#ifndef NULLRTVIDEO_H
+#define NULLRTVIDEO_H
+
+/*
+ * Define the type of the plugin
+ * This plugin is not multichannel
+ */
+#define PLUGIN_IS_VIDEO
+#define PLUGIN_IS_REALTIME
+
+/*
+ * We have own load_configiration (no need of parameter interpolation)
+ */
+#define PLUGIN_CUSTOM_LOAD_CONFIGURATION
+/*
+ * Define the name and api related class names of the plugin
+ */
+#define PLUGIN_TITLE N_("Null")
+#define PLUGIN_CLASS NRTVideo
+#define PLUGIN_CONFIG_CLASS NRTVideoConfig
+#define PLUGIN_THREAD_CLASS NRTVideoThread
+#define PLUGIN_GUI_CLASS NRTVideoWindow
+
+#include "pluginmacros.h"
+
+#include "language.h"
+#include "pluginvclient.h"
+#include "vframe.inc"
+
+class NRTVideoConfig
+{
+public:
+	NRTVideoConfig();
+/*
+ * If load_configuration from macros is used, next methods must be defined
+ * int equivalent(NRTVideoConfig &that);
+ * void copy_from(NRTVideoConfig &that);
+ * void interpolate(NRTVideoConfig &prev, NRTVideoConfig &next
+ *     ptstime prev_pts, ptstime next_pts, ptstime current_pts);
+ */
+	int onoff;
+	PLUGIN_CONFIG_CLASS_MEMBERS
+};
+
+class NRTVideoSwitch : public BC_Radial
+{
+public:
+	NRTVideoSwitch(NRTVideo *plugin,
+		int x,
+		int y);
+
+	int handle_event();
+	NRTVideo *plugin;
+};
+
+/*
+ * Gui of the plugin - may be omitted
+ */
+class NRTVideoWindow : public BC_Window
+{
+public:
+	NRTVideoWindow(NRTVideo *plugin, int x, int y);
+
+	void update();
+	NRTVideoSwitch *toggle;
+	PLUGIN_GUI_CLASS_MEMBERS
+};
+
+/*
+ * Macro creates header part of the plugin thread class
+ * Must be used if the plugin has a gui
+ */
+PLUGIN_THREAD_HEADER
+
+/*
+ * Main class of the plugin
+ */
+class NRTVideo : public PluginVClient
+{
+public:
+	NRTVideo(PluginServer *server);
+	~NRTVideo();
+/*
+ * Macro defines members of the plugin class
+ * Macro must be always used
+ * Macro declares int load_configuration, if the plugin has confiuguration class
+ */
+	PLUGIN_CLASS_MEMBERS
+// Processing is here
+	void process_frame(VFrame *frame);
+// Loading and saving of defaults - optional: needed if plugin has parameters
+	void load_defaults();
+	void save_defaults();
+// Loading and saving parameters to the project - optional
+	void save_data(KeyFrame *keyframe);
+	void read_data(KeyFrame *keyframe);
+};
+
+#endif
