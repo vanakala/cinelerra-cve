36640fbccfbd7740da321dafeeb5ed35d393bfd1
Einar Rünkaru <einarry@smail.ee>
2012-02-13 21:53:29 +0200

    New audio realtime plugin "Null"
    This is a skeletion for creating new audio realtime plugins
    Simple passtrhrough with configurable ability to clear output

---
+++ plugins/nullrtaudio/nullrtaudio.C
@@ -0,0 +1,185 @@
+
+/*
+ * CINELERRA
+ * Copyright (C) 2012 Einar Rünkaru <einarry at smail dot ee>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "bchash.h"
+#include "filexml.h"
+#include "picon_png.h"
+#include "vframe.h"
+#include "nullrtaudio.h"
+
+#include <stdint.h>
+#include <string.h>
+
+// Registers plugin
+REGISTER_PLUGIN
+
+NRTAudioConfig::NRTAudioConfig()
+{
+	onoff = 0;
+}
+
+NRTAudioSwitch::NRTAudioSwitch(NRTAudio *plugin, 
+	int x,
+	int y)
+ : BC_Radial(x,
+		y,
+		plugin->config.onoff,
+		_("Clear"))
+{
+	this->plugin = plugin;
+}
+
+int NRTAudioSwitch::handle_event()
+{
+	plugin->config.onoff = plugin->config.onoff ? 0 : 1;
+	plugin->send_configure_change();
+	return 0;
+}
+
+/*
+ * The gui of the plugin
+ * Using the constuctor macro is mandatory
+ * The constructor macro must be near the end of constructor after
+ * creating the elements of gui.
+ */
+NRTAudioWindow::NRTAudioWindow(NRTAudio *plugin, int x, int y)
+ : BC_Window(plugin->gui_string, 
+	x,
+	y,
+	200,
+	50,
+	200,
+	50,
+	0,
+	0,
+	1)
+{
+	x = y = 10;
+
+	add_subwindow(new BC_Title(x, y, _("Status:")));
+	add_subwindow(toggle = new NRTAudioSwitch(plugin,
+		x + 60,
+		y));
+	PLUGIN_GUI_CONSTRUCTOR_MACRO
+}
+
+void NRTAudioWindow::update()
+{
+	toggle->update(plugin->config.onoff);
+}
+
+/*
+ * All methods of the plugin thread class
+ */
+PLUGIN_THREAD_METHODS
+
+/*
+ * The processor of the plugin
+ * Using the constructor and the destructor macro is mandatory
+ */
+NRTAudio::NRTAudio(PluginServer *server)
+ : PluginAClient(server)
+{
+	PLUGIN_CONSTRUCTOR_MACRO
+}
+
+NRTAudio::~NRTAudio()
+{
+	PLUGIN_DESTRUCTOR_MACRO
+}
+
+/*
+ * Creates most of the api methods of the plugin
+ */
+PLUGIN_CLASS_METHODS
+
+void NRTAudio::load_defaults()
+{
+// Loads defaults from the named rc-file
+	defaults = load_defaults_file("nullrtaudio.rc");
+
+	config.onoff = defaults->get("ONOFF", config.onoff);
+}
+
+void NRTAudio::save_defaults()
+{
+	defaults->update("ONOFF", config.onoff);
+	defaults->save();
+}
+
+/*
+ * Saves the values of the parameters to the project
+ */
+void NRTAudio::save_data(KeyFrame *keyframe)
+{
+	FileXML output;
+	output.set_shared_string(keyframe->data, MESSAGESIZE);
+	output.tag.set_title("NRTAUDIO");
+	output.tag.set_property("ONOFF", config.onoff);
+	output.append_tag();
+	output.tag.set_title("/NRTAUDIO");
+	output.append_tag();
+	output.terminate_string();
+}
+
+void NRTAudio::read_data(KeyFrame *keyframe)
+{
+	FileXML input;
+
+	input.set_shared_string(keyframe->data, strlen(keyframe->data));
+
+	while(!input.read_tag())
+	{
+		if(input.tag.title_is("NRTAUDIO"))
+		{
+			config.onoff = input.tag.get_property("ONOFF", config.onoff);
+		}
+	}
+}
+
+/*
+ * Loads parameters from keyframe
+ */
+int NRTAudio::load_configuration()
+{
+	int prev_val = config.onoff;
+	read_data(prev_keyframe_pts(source_pts));
+	return !(prev_val == config.onoff);
+}
+
+/*
+ * Pull frame and apply modifications
+ * It is possible to use here
+ * void process_realtime(AFrame *input, AFrame *output)
+ *    - here input is already filled
+ * multichannel plugin gets here arrays of pointers
+ * prototype: void process_frame(AFrame **frame)
+ *    - number of channels is total_in_buffers
+ */
+void NRTAudio::process_frame(AFrame *frame)
+{
+	load_configuration();
+
+	get_frame(frame);
+
+	if(config.onoff)
+		memset(frame->buffer, 0, frame->length * sizeof(double));
+}
---
+++ plugins/nullrtaudio/nullrtaudio.h
@@ -0,0 +1,126 @@
+
+/*
+ * CINELERRA
+ * Copyright (C) 2012 Einar Rünkaru <einarry at smail dot ee>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+/*
+ * Null audio realtime plugin
+ * The main purpose of this plugin is to be a skeletion
+ * of an usable audio realtime effect
+ */
+
+#ifndef NULLRTAUDIO_H
+#define NULLRTAUDIO_H
+
+/*
+ * Define the type of the plugin
+ * This plugin is not multichannel
+ */
+#define PLUGIN_IS_AUDIO
+#define PLUGIN_IS_REALTIME
+
+/*
+ * We have own load_configiration (no need of parameter interpolation)
+ */
+#define PLUGIN_CUSTOM_LOAD_CONFIGURATION
+/*
+ * Define the name and api related class names of the plugin
+ */
+#define PLUGIN_TITLE N_("Null")
+#define PLUGIN_CLASS NRTAudio
+#define PLUGIN_CONFIG_CLASS NRTAudioConfig
+#define PLUGIN_THREAD_CLASS NRTAudioThread
+#define PLUGIN_GUI_CLASS NRTAudioWindow
+
+#include "pluginmacros.h"
+
+#include "language.h"
+#include "pluginaclient.h"
+#include "vframe.inc"
+
+class NRTAudioConfig
+{
+public:
+	NRTAudioConfig();
+/*
+ * If load_configuration from macros is used, next methods must be defined
+ * int equivalent(NRTAudioConfig &that);
+ * void copy_from(NRTAudioConfig &that);
+ * void interpolate(NRTAudioConfig &prev, NRTAudioConfig &next
+ *     ptstime prev_pts, ptstime next_pts, ptstime current_pts);
+ */
+	int onoff;
+	PLUGIN_CONFIG_CLASS_MEMBERS
+};
+
+class NRTAudioSwitch : public BC_Radial
+{
+public:
+	NRTAudioSwitch(NRTAudio *plugin,
+		int x,
+		int y);
+
+	int handle_event();
+	NRTAudio *plugin;
+};
+
+/*
+ * Gui of the plugin - may be omitted
+ */
+class NRTAudioWindow : public BC_Window
+{
+public:
+	NRTAudioWindow(NRTAudio *plugin, int x, int y);
+
+	void update();
+	NRTAudioSwitch *toggle;
+	PLUGIN_GUI_CLASS_MEMBERS
+};
+
+/*
+ * Macro creates header part of the plugin thread class
+ * Must be used if the plugin has a gui
+ */
+PLUGIN_THREAD_HEADER
+
+/*
+ * Main class of the plugin
+ */
+class NRTAudio : public PluginAClient
+{
+public:
+	NRTAudio(PluginServer *server);
+	~NRTAudio();
+/*
+ * Macro defines members of the plugin class
+ * Macro must be always used
+ * Macro declares int load_configuration, if the plugin has confiuguration class
+ */
+	PLUGIN_CLASS_MEMBERS
+// Processing is here
+	void process_frame(AFrame *frame);
+// Loading and saving of defaults - optional: needed if plugin has parameters
+	void load_defaults();
+	void save_defaults();
+// Loading and saving parameters to the project - optional
+	void save_data(KeyFrame *keyframe);
+	void read_data(KeyFrame *keyframe);
+};
+
+#endif
