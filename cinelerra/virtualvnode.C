vanakala/cinelerra-cve

commit 76a7b6dab578837b11889c3c83fcfbe43edf3e86

Fixed crash in VirtualVNode::render_projector - mode autos can be empty

cinelerra/virtualvnode.C
@@ -371,7 +371,11 @@ void VirtualVNode::render_projector(VFrame *input, VFrame *output)
 					input->get_pts(), 
 					(Auto* &)mode_keyframe);
 
-			int mode = mode_keyframe->value;
+			int mode;
+			if(mode_keyframe)
+				mode_keyframe->value;
+			else
+				mode = TRANSFER_NORMAL;
 
 // Fade is performed in render_fade so as to allow this module
 // to be chained in another module, thus only 4 component colormodels

commit e57e46f5f385c4d01a160f95501b57e1ea67fc99

Bugfix in VirtualVNode::render_projector
Read mode value from keyframe.
Fixes bug introduced in 76a7b6d

cinelerra/virtualvnode.C
@@ -373,7 +373,7 @@ void VirtualVNode::render_projector(VFrame *input, VFrame *output)
 
 			int mode;
 			if(mode_keyframe)
-				mode_keyframe->value;
+				mode = mode_keyframe->value;
 			else
 				mode = TRANSFER_NORMAL;
 