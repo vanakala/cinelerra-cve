vanakala/cinelerra-cve

commit d80a1019171ee877d771c433525e7f8cf78853d2

Moved limiting of frame size to selection.C
Frame size limits should be checked witdh FrameSizeSelection::limits
Minimum and maximum frame sizes are defined in cinelerra.h.
The solution is weird while entering the value manually.

cinelerra/selection.C
@@ -27,6 +27,7 @@
 #include "bcwindow.h"
 #include "bcwindowbase.h"
 #include "bcresources.h"
+#include "cinelerra.h"
 #include "language.h"
 #include "mwindow.h"
 #include "theme.h"
@@ -131,6 +132,59 @@ void FrameSizeSelection::handle_swapvalues(int value1, int value2)
 	update(value1, value2);
 }
 
+int FrameSizeSelection::handle_event()
+{
+	int result = Selection::handle_event();
+
+	if(limits(intvalue2, intvalue))
+		update(*intvalue2, *intvalue);
+
+	return result;
+}
+
+int FrameSizeSelection::limits(int *width, int *height)
+{
+	int v, result = 0;
+
+	if(width)
+	{
+		v = *width;
+		if(v > MAX_FRAME_WIDTH)
+		{
+			result = -1;
+			v = MAX_FRAME_WIDTH;
+		}
+		if(v < MIN_FRAME_WIDTH)
+		{
+			result = -1;
+			v = MIN_FRAME_WIDTH;
+		}
+		v &= ~1;
+		if(!result && v != *width)
+			result = 1;
+		*width = v;
+	}
+
+	if(height)
+	{
+		v = *height;
+		if(v < MIN_FRAME_HEIGHT)
+		{
+			result = -1;
+			v = MIN_FRAME_WIDTH;
+		}
+		if(v > MAX_FRAME_WIDTH)
+		{
+			result = -1;
+			v = MAX_FRAME_WIDTH;
+		}
+		v &= ~1;
+		if(!result && v != *height)
+			result = 1;
+		*height = v;
+	}
+	return result;
+}
 
 AspectRatioSelection::AspectRatioSelection(int x1, int y1, int x2, int y2,
 	BC_WindowBase *base, double *value1, double *value2)

cinelerra/selection.h
@@ -153,6 +153,8 @@ public:
 
 	void update(int value1, int value2);
 	void handle_swapvalues(int value1, int value2);
+	int handle_event();
+	static int limits(int *width, int *height);
 
 private:
 	static const struct selection_2int frame_sizes[];

cinelerra/setformat.C
@@ -217,6 +217,7 @@ void SetFormatThread::update_window()
 
 	if(dimension_modified)
 	{
+		FrameSizeSelection::limits(&dimension[0], &dimension[1]);
 		window->framesize_selection->update(dimension[0], dimension[1]);
 		update_aspect();
 	}
@@ -662,7 +663,7 @@ SetFrameSize::SetFrameSize(int x1, int y1, int x2, int y2,
 
 int SetFrameSize::handle_event()
 {
-	Selection::handle_event();
+	FrameSizeSelection::handle_event();
 	if(oldvalue1 != *intvalue2)
 	{
 		oldvalue1 = *intvalue2;

cinelerra/edlsession.C
@@ -33,6 +33,7 @@
 #include "overlayframe.inc"
 #include "playbackconfig.h"
 #include "recordconfig.h"
+#include "selection.h"
 #include "tracks.h"
 #include "workarounds.h"
 
@@ -398,13 +399,8 @@ void EDLSession::boundaries()
 	Workarounds::clamp(min_meter_db, -80, -20);
 	Workarounds::clamp(max_meter_db, 0, 10);
 	Workarounds::clamp(frames_per_foot, 1, 32);
-	Workarounds::clamp(output_w, 16, (int)BC_INFINITY);
-	Workarounds::clamp(output_h, 16, (int)BC_INFINITY);
 	Workarounds::clamp(video_write_length, 1, 1000);
-	output_w /= 2;
-	output_w *= 2;
-	output_h /= 2;
-	output_h *= 2;
+	FrameSizeSelection::limits(&output_w, &output_h);
 
 	Workarounds::clamp(crop_x1, 0, output_w);
 	Workarounds::clamp(crop_x2, 0, output_w);

include/cinelerra.h
@@ -45,4 +45,10 @@
 #define MAXCHANNELS 16
 #define MAX_CHANNELS 16
 
+// Frame size limits
+#define MIN_FRAME_HEIGHT 16
+#define MAX_FRAME_HEIGHT 4096
+#define MIN_FRAME_WIDTH 16
+#define MAX_FRAME_WIDTH 4096
+
 #endif

commit 34e6a53de852e5d047cc9b0ad38beb6d30aaab6c

Fixed weirdess of entering frame size manually
Fixing problem introduced in commit d80a1019171ee877d771c433525e7f8cf78853d2

cinelerra/new.C
@@ -115,6 +115,10 @@ void New::create_new_project()
 	if(FrameRateSelection::limits(&new_edl->session->frame_rate) < 0)
 		errorbox(_("Frame rate is out of limits (%d..%d).\nCorrection applied."),
 			MIN_FRAME_RATE, MAX_FRAME_RATE);
+	if(FrameSizeSelection::limits(&new_edl->session->output_w,
+			&new_edl->session->output_h) < 0)
+		errorbox(_("Frame size is out of limits (%d..%dx%d..%d).\nCorrection applied."),
+			MIN_FRAME_WIDTH, MAX_FRAME_WIDTH, MIN_FRAME_HEIGHT, MAX_FRAME_WIDTH);
 	mwindow->edl = new_edl;
 	mwindow->save_defaults();
 

cinelerra/selection.C
@@ -182,16 +182,6 @@ void FrameSizeSelection::handle_swapvalues(int value1, int value2)
 	update(value1, value2);
 }
 
-int FrameSizeSelection::handle_event()
-{
-	int result = Selection::handle_event();
-
-	if(limits(intvalue2, intvalue))
-		update(*intvalue2, *intvalue);
-
-	return result;
-}
-
 int FrameSizeSelection::limits(int *width, int *height)
 {
 	int v, result = 0;

cinelerra/selection.h
@@ -155,7 +155,6 @@ public:
 
 	void update(int value1, int value2);
 	void handle_swapvalues(int value1, int value2);
-	int handle_event();
 	static int limits(int *width, int *height);
 
 private:

cinelerra/setformat.C
@@ -128,6 +128,9 @@ void SetFormatThread::apply_changes()
 	if(FrameRateSelection::limits(&new_settings->session->frame_rate) < 0)
 		errorbox(_("Frame rate is out of limits (%d..%d).\nCorrection applied."),
 			MIN_FRAME_RATE, MAX_FRAME_RATE);
+	if(FrameSizeSelection::limits(&dimension[0], &dimension[1]) < 0)
+		errorbox(_("Frame size is out of limits (%d..%dx%d..%d).\nCorrection applied."),
+			MIN_FRAME_WIDTH, MAX_FRAME_WIDTH, MIN_FRAME_HEIGHT, MAX_FRAME_WIDTH);
 
 	mwindow->edl->copy_session(new_settings, 1);
 	mwindow->edl->session->output_w = dimension[0];
@@ -669,7 +672,7 @@ SetFrameSize::SetFrameSize(int x1, int y1, int x2, int y2,
 
 int SetFrameSize::handle_event()
 {
-	FrameSizeSelection::handle_event();
+	Selection::handle_event();
 	if(oldvalue1 != *intvalue2)
 	{
 		oldvalue1 = *intvalue2;