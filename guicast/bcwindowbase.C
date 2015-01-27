vanakala/cinelerra-cve

commit 297b1768e91305ec9f71aff6c1c1973035b95f33

New functions BC_WindowBase::get_text_height
Variants for wchar_t string and without string

guicast/bcwindowbase.C
@@ -2162,20 +2162,36 @@ int BC_WindowBase::get_text_descent(int font)
 	return 0;
 }
 
+int BC_WindowBase::get_text_height(int font)
+{
+	return get_xft_struct(font)->height;
+}
+
 int BC_WindowBase::get_text_height(int font, const char *text)
 {
-	int rowh;
-	XftFont *fstruct;
-
-	if(fstruct = get_xft_struct(font))
-		rowh = fstruct->height;
-	else
-		rowh = get_text_ascent(font) + get_text_descent(font);
-
-	if(!text) return rowh;
+	int rowh = get_xft_struct(font)->height;
 
 // Add height of lines
 	int h = 0, i, length = strlen(text);
+
+	for(i = 0; i <= length; i++)
+	{
+		if(text[i] == '\n')
+			h++;
+		else
+		if(text[i] == 0)
+			h++;
+	}
+	return h * rowh;
+}
+
+int BC_WindowBase::get_text_height(int font, const wchar_t *text)
+{
+	int rowh = get_xft_struct(font)->height;
+
+	// Add height of lines
+	int h = 0, i, length = wcslen(text);
+
 	for(i = 0; i <= length; i++)
 	{
 		if(text[i] == '\n')

guicast/bcwindowbase.h
@@ -271,7 +271,9 @@ public:
 	BC_Pixmap* get_bg_pixmap();
 	int get_text_ascent(int font);
 	int get_text_descent(int font);
-	int get_text_height(int font, const char *text = 0);
+	int get_text_height(int font);
+	int get_text_height(int font, const char *text);
+	int get_text_height(int font, const wchar_t *text);
 	int get_text_width(int font, const char *text, int length = -1);
 	int get_text_width(int font, const wchar_t *text, int length = -1);
 	BC_Clipboard* get_clipboard();

commit 056bf74b224a11e1d4e80d5d9981b20542be36aa

Fixed crash in BC_WindowBase::get_text_height
Not all fonts are xft-fonts
Bug was introduced with commit 297b1768e9

guicast/bcwindowbase.C
@@ -2177,7 +2177,13 @@ int BC_WindowBase::get_text_descent(int font)
 
 int BC_WindowBase::get_text_height(int font, const wchar_t *text)
 {
-	int rowh = get_xft_struct(font)->height;
+	XftFont *fstruct;
+	int rowh;
+
+	if(fstruct = get_xft_struct(font))
+		rowh = fstruct->height;
+	else
+		rowh = get_text_ascent(font) + get_text_descent(font);
 
 	if(!text)
 		return rowh;