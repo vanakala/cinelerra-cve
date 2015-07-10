vanakala/cinelerra-cve

commit 7f0981aebbdba06626d9fd74cdbd70fc55edd868

Cleanup in vdeviceprefs
Removed create_objects, empty destructors, empty lines,
pointless return values.
Functionality of create_objects moved to constructor.

cinelerra/vdeviceprefs.C
@@ -58,7 +58,6 @@ VDevicePrefs::VDevicePrefs(int x,
 	this->y = y;
 	menu = 0;
 	reset_objects();
-
 }
 
 VDevicePrefs::~VDevicePrefs()
@@ -84,7 +83,7 @@ void VDevicePrefs::reset_objects()
 	channel_picker = 0;
 }
 
-int VDevicePrefs::initialize(int creation)
+void VDevicePrefs::initialize(int creation)
 {
 	int *driver = 0;
 	delete_objects();
@@ -108,7 +107,6 @@ int VDevicePrefs::initialize(int creation)
 			this, 
 			(mode == MODERECORD), 
 			driver));
-		menu->create_objects();
 	}
 
 	switch(this->driver)
@@ -136,18 +134,15 @@ int VDevicePrefs::initialize(int creation)
 		break;
 	}
 
-
 // Update driver dependancies in file format
 	if(mode == MODERECORD && dialog && !creation)
 	{
 		RecordPrefs *record_prefs = (RecordPrefs*)dialog;
 		record_prefs->recording_format->update_driver(this->driver);
 	}
-
-	return 0;
 }
 
-int VDevicePrefs::delete_objects()
+void VDevicePrefs::delete_objects()
 {
 	delete output_title;
 	delete channel_picker;
@@ -163,10 +158,9 @@ int VDevicePrefs::delete_objects()
 
 	reset_objects();
 	driver = -1;
-	return 0;
 }
 
-int VDevicePrefs::create_dvb_objs()
+void VDevicePrefs::create_dvb_objs()
 {
 	int x1 = x + menu->get_w() + 5;
 	dialog->add_subwindow(device_title = new BC_Title(x1, y, _("Host:")));
@@ -179,7 +173,7 @@ int VDevicePrefs::create_dvb_objs()
 	device_number = new VDeviceTumbleBox(this, x1, y + 20,  &in_config->dvb_in_number, 0, 16);
 }
 
-int VDevicePrefs::create_v4l_objs()
+void VDevicePrefs::create_v4l_objs()
 {
 	char *output_char;
 	BC_Resources *resources = BC_WindowBase::get_resources();
@@ -187,10 +181,9 @@ int VDevicePrefs::create_v4l_objs()
 	output_char = pwindow->thread->edl->session->vconfig_in->v4l_in_device;
 	dialog->add_subwindow(device_title = new BC_Title(x1, y, _("Device path:"), MEDIUMFONT, resources->text_default));
 	dialog->add_subwindow(device_text = new VDeviceTextBox(x1, y + 20, output_char));
-	return 0;
 }
 
-int VDevicePrefs::create_v4l2_objs()
+void VDevicePrefs::create_v4l2_objs()
 {
 	char *output_char;
 	BC_Resources *resources = BC_WindowBase::get_resources();
@@ -198,11 +191,9 @@ int VDevicePrefs::create_v4l2_objs()
 	output_char = pwindow->thread->edl->session->vconfig_in->v4l2_in_device;
 	dialog->add_subwindow(device_title = new BC_Title(x1, y, _("Device path:"), MEDIUMFONT, resources->text_default));
 	dialog->add_subwindow(device_text = new VDeviceTextBox(x1, y + 20, output_char));
-
-	return 0;
 }
 
-int VDevicePrefs::create_v4l2jpeg_objs()
+void VDevicePrefs::create_v4l2jpeg_objs()
 {
 	BC_Resources *resources = BC_WindowBase::get_resources();
 	char *output_char;
@@ -210,13 +201,9 @@ int VDevicePrefs::create_v4l2jpeg_objs()
 	output_char = pwindow->thread->edl->session->vconfig_in->v4l2jpeg_in_device;
 	dialog->add_subwindow(device_title = new BC_Title(x1, y, _("Device path:"), MEDIUMFONT, resources->text_default));
 	dialog->add_subwindow(device_text = new VDeviceTextBox(x1, y + 20, output_char));
-
-	return 0;
 }
 
-
-
-int VDevicePrefs::create_screencap_objs()
+void VDevicePrefs::create_screencap_objs()
 {
 	char *output_char;
 	BC_Resources *resources = BC_WindowBase::get_resources();
@@ -224,10 +211,9 @@ int VDevicePrefs::create_screencap_objs()
 	output_char = pwindow->thread->edl->session->vconfig_in->screencapture_display;
 	dialog->add_subwindow(device_title = new BC_Title(x1, y, _("Display:"), MEDIUMFONT, resources->text_default));
 	dialog->add_subwindow(device_text = new VDeviceTextBox(x1, y + 20, output_char));
-	return 0;
 }
 
-int VDevicePrefs::create_x11_objs()
+void VDevicePrefs::create_x11_objs()
 {
 	char *output_char;
 	BC_Resources *resources = BC_WindowBase::get_resources();
@@ -235,7 +221,6 @@ int VDevicePrefs::create_x11_objs()
 	output_char = out_config->x11_host;
 	dialog->add_subwindow(device_title = new BC_Title(x1, y, _("Display for compositor:"), MEDIUMFONT, resources->text_default));
 	dialog->add_subwindow(device_text = new VDeviceTextBox(x1, y + 20, output_char));
-	return 0;
 }
 
 
@@ -249,10 +234,29 @@ VDriverMenu::VDriverMenu(int x,
 	this->output = output;
 	this->do_input = do_input;
 	this->device_prefs = device_prefs;
-}
 
-VDriverMenu::~VDriverMenu()
-{
+	if(do_input)
+	{
+#ifdef HAVE_VIDEO4LINUX
+		add_item(new VDriverItem(this, VIDEO4LINUX_TITLE, VIDEO4LINUX));
+#endif
+#ifdef HAVE_VIDEO4LINUX2
+		add_item(new VDriverItem(this, VIDEO4LINUX2_TITLE, VIDEO4LINUX2));
+		add_item(new VDriverItem(this, VIDEO4LINUX2JPEG_TITLE, VIDEO4LINUX2JPEG));
+#endif
+		add_item(new VDriverItem(this, SCREENCAPTURE_TITLE, SCREENCAPTURE));
+		add_item(new VDriverItem(this, CAPTURE_DVB_TITLE, CAPTURE_DVB));
+	}
+	else
+	{
+		add_item(new VDriverItem(this, PLAYBACK_X11_TITLE, PLAYBACK_X11));
+		add_item(new VDriverItem(this, PLAYBACK_X11_XV_TITLE, PLAYBACK_X11_XV));
+#ifdef HAVE_GL
+// Check runtime glx version. pbuffer needs >= 1.3
+		if(get_opengl_version() >= 103)
+			add_item(new VDriverItem(this, PLAYBACK_X11_GL_TITLE, PLAYBACK_X11_GL));
+#endif
+	}
 }
 
 const char* VDriverMenu::driver_to_string(int driver)
@@ -290,43 +294,12 @@ const char* VDriverMenu::driver_to_string(int driver)
 	return sp;
 }
 
-int VDriverMenu::create_objects()
-{
-	if(do_input)
-	{
-#ifdef HAVE_VIDEO4LINUX
-		add_item(new VDriverItem(this, VIDEO4LINUX_TITLE, VIDEO4LINUX));
-#endif
-#ifdef HAVE_VIDEO4LINUX2
-		add_item(new VDriverItem(this, VIDEO4LINUX2_TITLE, VIDEO4LINUX2));
-		add_item(new VDriverItem(this, VIDEO4LINUX2JPEG_TITLE, VIDEO4LINUX2JPEG));
-#endif
-		add_item(new VDriverItem(this, SCREENCAPTURE_TITLE, SCREENCAPTURE));
-		add_item(new VDriverItem(this, CAPTURE_DVB_TITLE, CAPTURE_DVB));
-	}
-	else
-	{
-		add_item(new VDriverItem(this, PLAYBACK_X11_TITLE, PLAYBACK_X11));
-		add_item(new VDriverItem(this, PLAYBACK_X11_XV_TITLE, PLAYBACK_X11_XV));
-#ifdef HAVE_GL
-// Check runtime glx version. pbuffer needs >= 1.3
-		if(get_opengl_version() >= 103)
-			add_item(new VDriverItem(this, PLAYBACK_X11_GL_TITLE, PLAYBACK_X11_GL));
-#endif
-	}
-	return 0;
-}
-
 
 VDriverItem::VDriverItem(VDriverMenu *popup, const char *text, int driver)
  : BC_MenuItem(text)
 {
 	this->popup = popup;
 	this->driver = driver;
-}
-
-VDriverItem::~VDriverItem()
-{
 }
 
 int VDriverItem::handle_event()
@@ -336,8 +309,6 @@ int VDriverItem::handle_event()
 	popup->device_prefs->initialize();
 	return 1;
 }
-
-
 
 
 VDeviceTextBox::VDeviceTextBox(int x, int y, char *output)
@@ -350,6 +321,7 @@ int VDeviceTextBox::handle_event()
 { 
 	strcpy(output, get_text()); 
 }
+
 
 VDeviceTumbleBox::VDeviceTumbleBox(VDevicePrefs *prefs, 
 	int x, 
@@ -393,6 +365,7 @@ VDeviceCheckBox::VDeviceCheckBox(int x, int y, int *output, const char *text)
 {
 	this->output = output;
 }
+
 int VDeviceCheckBox::handle_event()
 {
 	*output = get_value();

cinelerra/vdeviceprefs.h
@@ -57,8 +57,8 @@ public:
 
 // creation - set if this is the first initialize of the object
 //            to prevent file format from being overwritten
-	int initialize(int creation = 0);
-	int delete_objects();
+	void initialize(int creation = 0);
+	void delete_objects();
 	void reset_objects();
 
 	PreferencesWindow *pwindow;
@@ -68,12 +68,12 @@ public:
 	PrefsChannelPicker *channel_picker;
 
 private:
-	int create_v4l_objs();
-	int create_v4l2_objs();
-	int create_v4l2jpeg_objs();
-	int create_screencap_objs();
-	int create_x11_objs();
-	int create_dvb_objs();
+	void create_v4l_objs();
+	void create_v4l2_objs();
+	void create_v4l2jpeg_objs();
+	void create_screencap_objs();
+	void create_x11_objs();
+	void create_dvb_objs();
 
 	VDriverMenu *menu;
 
@@ -96,6 +96,7 @@ public:
 	VDeviceTextBox(int x, int y, char *output);
 
 	int handle_event();
+
 	char *output;
 };
 
@@ -140,11 +141,9 @@ public:
 		VDevicePrefs *device_prefs, 
 		int do_input, 
 		int *output);
-	~VDriverMenu();
-	
+
 	const char* driver_to_string(int driver);
-	int create_objects();
-	
+
 	VDevicePrefs *device_prefs;
 	int do_input;
 	int *output;
@@ -156,14 +155,11 @@ class VDriverItem : public BC_MenuItem
 {
 public:
 	VDriverItem(VDriverMenu *popup, const char *text, int driver);
-	~VDriverItem();
-	
+
 	int handle_event();
 
 	VDriverMenu *popup;
 	int driver;
 };
-
-
 
 #endif

commit 7fc395da58178edb08969cc04434533a406ca8d7

Check OpenGL version of Composer display
Displays may have different capabilities, OpenGL is used
only in Composer. Fixes crash caused by commit 7f0981ae.

cinelerra/vdeviceprefs.C
@@ -20,6 +20,7 @@
  */
 
 #include "bcsignals.h"
+#include "cwindow.h"
 #include "channeldb.h"
 #include "channelpicker.h"
 #ifdef HAVE_CONFIG_H
@@ -253,7 +254,7 @@ VDriverMenu::VDriverMenu(int x,
 		add_item(new VDriverItem(this, PLAYBACK_X11_XV_TITLE, PLAYBACK_X11_XV));
 #ifdef HAVE_GL
 // Check runtime glx version. pbuffer needs >= 1.3
-		if(get_opengl_version() >= 103)
+		if(get_opengl_version((BC_WindowBase *)device_prefs->pwindow->mwindow->cwindow->gui) >= 103)
 			add_item(new VDriverItem(this, PLAYBACK_X11_GL_TITLE, PLAYBACK_X11_GL));
 #endif
 	}

guicast/bcwindow3d.C
@@ -86,11 +86,11 @@ void BC_WindowBase::put_shader(unsigned int handle, char *source)
 	get_resources()->get_synchronous()->put_shader(handle, source);
 }
 
-int BC_WindowBase::get_opengl_version()
+int BC_WindowBase::get_opengl_version(BC_WindowBase *window)
 {
 #ifdef HAVE_GL
 	int maj, min;
-	if(glXQueryVersion(get_display(), &maj, &min))
+	if(glXQueryVersion(window->get_display(), &maj, &min))
 		return 100 * maj + min;
 #endif
 	return 0;

guicast/bcwindowbase.h
@@ -205,7 +205,7 @@ public:
 // Not run in OpenGL thread because it has its own lock.
 	unsigned int get_shader(char *title, int *got_it);
 	void put_shader(unsigned int handle, char *title);
-	int get_opengl_version(void);
+	int get_opengl_version(BC_WindowBase *window);
 
 	int flash(int x, int y, int w, int h, int flush = 1);
 	int flash(int flush = 1);