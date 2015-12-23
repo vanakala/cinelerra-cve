vanakala/cinelerra-cve

commit b5867560165077aade0321f0584bb67663797e7b

Disable temporarily some items in rendering menu
Disabilng 'Microsoft AVI' and 'Quicktime for Linux'
until rendereing in FileAVlibs will be implemented.

cinelerra/formattools.C
@@ -41,12 +41,12 @@ const struct container_type ContainerSelection::media_containers[] =
 	{ N_("Sun/NeXT AU"), FILE_AU, "AU", "au" },
 	{ N_("JPEG"), FILE_JPEG, "JPEG", "jpg" },
 	{ N_("JPEG Sequence"), FILE_JPEG_LIST, "JPEG_LIST", "list" },
-	{ N_("Microsoft AVI"), FILE_AVI, "AVI", "avi" },
+//	{ N_("Microsoft AVI"), FILE_AVI, "AVI", "avi" },
 	{ N_("EXR"), FILE_EXR, "EXR", "exr" },
 	{ N_("EXR Sequence"), FILE_EXR_LIST, "EXR_LIST", "list" },
 	{ N_("YUV4MPEG Stream"), FILE_YUV, "YUV", "m2v" },
 	{ N_("Microsoft WAV"), FILE_WAV, "WAV", "wav" },
-	{ N_("Quicktime for Linux"), FILE_MOV, "MOV", "mov" },
+//	{ N_("Quicktime for Linux"), FILE_MOV, "MOV", "mov" },
 	{ N_("Raw DV"), FILE_RAWDV, "RAWDV", "dv" },
 	{ N_("MPEG Audio"), FILE_AMPEG, "MPEG", "mp3" },
 	{ N_("MPEG Video"), FILE_VMPEG, "VMPEG", "m2v" },
@@ -67,8 +67,12 @@ const struct container_type ContainerSelection::media_containers[] =
 
 int FormatPopup::brender_menu[] = { FILE_JPEG_LIST, FILE_PNG_LIST, FILE_TIFF_LIST };
 int FormatPopup::frender_menu[] = { FILE_AC3 , FILE_AIFF, FILE_AU, FILE_JPEG,
-	FILE_JPEG_LIST, FILE_AVI, FILE_EXR, FILE_EXR_LIST,
-	FILE_YUV, FILE_WAV, FILE_MOV, FILE_RAWDV,
+	FILE_JPEG_LIST,
+// FILE_AVI,
+	FILE_EXR, FILE_EXR_LIST,
+	FILE_YUV, FILE_WAV,
+// FILE_MOV,
+	FILE_RAWDV,
 	FILE_AMPEG, FILE_VMPEG, FILE_OGG, FILE_PCM,
 	FILE_PNG, FILE_PNG_LIST, FILE_TGA, FILE_TGA_LIST,
 	FILE_TIFF, FILE_TIFF_LIST

commit 29415b77384811f422f0a2cbfdb0834e25484bd8

Fix ContainerSelection::media_containers
Bug was introduced by commit b586756016.
This broke indexing.

cinelerra/formattools.C
@@ -41,12 +41,12 @@ const struct container_type ContainerSelection::media_containers[] =
 	{ N_("Sun/NeXT AU"), FILE_AU, "AU", "au" },
 	{ N_("JPEG"), FILE_JPEG, "JPEG", "jpg" },
 	{ N_("JPEG Sequence"), FILE_JPEG_LIST, "JPEG_LIST", "list" },
-//	{ N_("Microsoft AVI"), FILE_AVI, "AVI", "avi" },
+	{ N_("Microsoft AVI"), FILE_AVI, "AVI", "avi" },
 	{ N_("EXR"), FILE_EXR, "EXR", "exr" },
 	{ N_("EXR Sequence"), FILE_EXR_LIST, "EXR_LIST", "list" },
 	{ N_("YUV4MPEG Stream"), FILE_YUV, "YUV", "m2v" },
 	{ N_("Microsoft WAV"), FILE_WAV, "WAV", "wav" },
-//	{ N_("Quicktime for Linux"), FILE_MOV, "MOV", "mov" },
+	{ N_("Quicktime for Linux"), FILE_MOV, "MOV", "mov" },
 	{ N_("Raw DV"), FILE_RAWDV, "RAWDV", "dv" },
 	{ N_("MPEG Audio"), FILE_AMPEG, "MPEG", "mp3" },
 	{ N_("MPEG Video"), FILE_VMPEG, "VMPEG", "m2v" },