vanakala/cinelerra-cve

commit fe9c4f9bb4791deb51d23f39a62d2a66ed3941f2

Removed FormatCheck class
There should be no knowledge about format capabilities outside
the respective File class.

cinelerra/menueffects.C
@@ -29,7 +29,6 @@
 #include "edlsession.h"
 #include "mainerror.h"
 #include "file.h"
-#include "formatcheck.h"
 #include "indexfile.h"
 #include "keyframe.h"
 #include "keys.h"
@@ -145,7 +144,7 @@ void MenuEffectThread::run()
 	}
 
 // find out which effect to run and get output file
-	int plugin_number, format_error = 0;
+	int plugin_number;
 
 	do
 	{
@@ -157,13 +156,7 @@ void MenuEffectThread::run()
 			result = window.run_window();
 			plugin_number = window.result;
 		}
-
-		if(!result)
-		{
-			FormatCheck format_check(default_asset);
-			format_error = format_check.check_format();
-		}
-	}while(format_error && !result);
+	}while(!result);
 
 // save defaults
 	save_derived_attributes(default_asset, defaults);

cinelerra/render.C
@@ -41,7 +41,6 @@
 #include "file.h"
 #include "filesystem.h"
 #include "filexml.h"
-#include "formatcheck.h"
 #include "formattools.h"
 #include "labels.h"
 #include "language.h"
@@ -279,8 +278,6 @@ void Render::stop_operation()
 
 void Render::run()
 {
-	int format_error;
-
 	result = 0;
 
 	if(mode == Render::INTERACTIVE)
@@ -295,7 +292,6 @@ void Render::run()
 		{
 			do
 			{
-				format_error = 0;
 				result = 0;
 				{
 					RenderWindow window(mwindow, this, asset);
@@ -306,14 +302,7 @@ void Render::run()
 						window.format_tools->path_recent->add_item(ContainerSelection::container_prefix(asset->format), asset->path);
 					}
 				}
-
-				if(!result)
-				{
-// Check the asset format for errors.
-					FormatCheck format_check(asset);
-					format_error = format_check.check_format();
-				}
-			}while(format_error && !result);
+			}while(!result);
 		}
 		save_defaults(asset);
 		mwindow->save_defaults();

cinelerra/Makefile.am
@@ -122,7 +122,6 @@ cinelerracve_SOURCES = aattachmentpoint.C \
 		    fileyuv.C \
 		    floatauto.C \
 		    floatautos.C \
-		    formatcheck.C \
 		    formatpresets.C \
 		    formattools.C \
 		    framecache.C \
@@ -376,7 +375,6 @@ noinst_HEADERS = aattachmentpoint.h \
 		 fileyuv.h \
 		 floatauto.h \
 		 floatautos.h \
-		 formatcheck.h \
 		 formatpresets.h \
 		 formattools.h \
 		 framecache.h \

cinelerra/formatcheck.C
@@ -1,81 +1,0 @@
-
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
-
-#include "asset.h"
-#include "file.h"
-#include "mainerror.h"
-#include "formatcheck.h"
-#include "mwindow.inc"
-
-#include <libintl.h>
-#define _(String) gettext(String)
-#define gettext_noop(String) String
-#define N_(String) gettext_noop (String)
-
-FormatCheck::FormatCheck(Asset *asset)
-{
-	this->asset = asset;
-}
-
-FormatCheck::~FormatCheck()
-{
-}
-
-int FormatCheck::check_format()
-{
-	int result = 0;
-
-	if(!result && asset->video_data)
-	{
-// Only 1 format can store video.
-		if(!File::supports_video(asset->format))
-		{
-			errorbox(_("The format you selected doesn't support video."));
-			result = 1;
-		}
-	}
-	
-	if(!result && asset->audio_data)
-	{
-		if(!File::supports_audio(asset->format))
-		{
-			errorbox(_("The format you selected doesn't support audio."));
-			result = 1;
-		}
-
-		if(!result && asset->bits == SBITS_IMA4 && asset->format != FILE_MOV)
-		{
-			errorbox(_("IMA4 compression is only available in Quicktime movies."));
-			result = 1;
-		}
-
-		if(!result && asset->bits == SBITS_ULAW && 
-			asset->format != FILE_MOV &&
-			asset->format != FILE_PCM)
-		{
-			errorbox(_("ULAW compression is only available in\n"
-				"Quicktime Movies and PCM files."));
-			result = 1;
-		}
-	}
-
-	return result;
-}

cinelerra/formatcheck.h
@@ -1,40 +1,0 @@
-
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
-
-#ifndef FORMATCHECK_H
-#define FORMATCHECK_H
-
-#include "assets.inc"
-
-class FormatCheck
-{
-public:
-	FormatCheck(Asset *asset);
-	~FormatCheck();
-
-	int check_format();
-
-private:
-	Asset *asset;
-};
-
-
-#endif

po/POTFILES.in
@@ -47,7 +47,6 @@ cinelerra/filetiff.C
 # cinelerra/filevorbis.C
 cinelerra/fileyuv.C
 # cinelerra/flipbook.C
-cinelerra/formatcheck.C
 # cinelerra/formatpopup.C
 cinelerra/formatpresets.C
 cinelerra/formattools.C

commit 1e2ff8b36f50e283e2a092a7855456666c8ca8e8

Fixed behaviour of rendering dialogs
Commit fe9c4f9bb479 broke these
Dialogs went to indefinite loop on OK.

cinelerra/menueffects.C
@@ -146,17 +146,12 @@ void MenuEffectThread::run()
 // find out which effect to run and get output file
 	int plugin_number;
 
-	do
-	{
-		{
-			MenuEffectWindow window(mwindow, 
-				this, 
-				need_plugin ? &plugin_list : 0, 
-				default_asset);
-			result = window.run_window();
-			plugin_number = window.result;
-		}
-	}while(!result);
+	MenuEffectWindow window(mwindow,
+		this,
+		need_plugin ? &plugin_list : 0,
+		default_asset);
+	result = window.run_window();
+	plugin_number = window.result;
 
 // save defaults
 	save_derived_attributes(default_asset, defaults);

cinelerra/render.C
@@ -288,25 +288,15 @@ void Render::run()
 		check_asset(mwindow->edl, *asset);
 
 // Get format from user
+		RenderWindow window(mwindow, this, asset);
+		result = window.run_window();
 		if(!result)
 		{
-			do
-			{
-				result = 0;
-				{
-					RenderWindow window(mwindow, this, asset);
-					result = window.run_window();
-					if (!result)
-					{
-						// add to recentlist only on OK
-						window.format_tools->path_recent->add_item(ContainerSelection::container_prefix(asset->format), asset->path);
-					}
-				}
-			}while(!result);
+			// add to recentlist only on OK
+			window.format_tools->path_recent->add_item(ContainerSelection::container_prefix(asset->format), asset->path);
 		}
 		save_defaults(asset);
 		mwindow->save_defaults();
-
 		if(!preferences)
 			preferences = new Preferences;
 		preferences->copy_from(mwindow->preferences);