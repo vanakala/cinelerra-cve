b96b17795ce88908e6f4f5a3baec453b218cb723
Einar Rünkaru <einarrunkaru@gmail.com>
2019-02-07 20:27:19 +0200

    Show track dimensions in Track::dump

--- cinelerra/track.C
+++ cinelerra/track.C
@@ -706,10 +706,13 @@ void Track::dump(int indent)
 		tp = "Unknown";
 		break;
 	}
-	printf("%*sType %s %c-%c-%c-%c-%c nudge %.2f\n", indent, "",
+	printf("%*sType: %s %c-%c-%c-%c-%c nudge %.2f\n", indent, "",
 		tp, master ? 'M' : 'm', play ? 'P' : 'p',
 		record ? 'A' : 'a', gang ? 'G' : 'g', draw ? 'D' : 'd', nudge);
-	printf("%*sTitle %s\n", indent, "", title);
+	if(data_type == TRACK_VIDEO)
+		printf("%*sTitle: '%s' [%d,%d]\n", indent, "", title, track_w, track_h);
+	else
+		printf("%*sTitle: '%s'\n", indent, "", title);
 	edits->dump(indent);
 	automation->dump(indent);
 	printf("%*sPlugin Sets: %d\n", indent, "", plugin_set.total);
