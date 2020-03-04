vanakala/cinelerra-cve

commit 9a73f1e3afd88c1b1900d3c740fd0a0089fdb630

Make new rendering engine active
Old rendering is disabled completely

cinelerra/pluginclient.C
@@ -224,7 +224,6 @@ void PluginClient::abort_plugin(const char *fmt, ...)
 
 KeyFrame* PluginClient::prev_keyframe_pts(ptstime pts)
 {
-#ifdef NEW_RENDERER
 	if(server->plugin)
 	{
 		if(server->plugin->shared_plugin)
@@ -232,14 +231,10 @@ KeyFrame* PluginClient::prev_keyframe_pts(ptstime pts)
 		return server->plugin->get_prev_keyframe(pts);
 	}
 	return server->keyframe;
-#else
-	return server->prev_keyframe_pts(pts);
-#endif
 }
 
 KeyFrame* PluginClient::next_keyframe_pts(ptstime pts)
 {
-#ifdef NEW_RENDERER
 	if(server->plugin)
 	{
 		if(server->plugin->shared_plugin)
@@ -247,9 +242,6 @@ KeyFrame* PluginClient::next_keyframe_pts(ptstime pts)
 		return server->plugin->get_next_keyframe(pts);
 	}
 	return server->keyframe;
-#else
-	return server->next_keyframe_pts(pts);
-#endif
 }
 
 void PluginClient::get_camera(double *x, double *y, double *z, ptstime postime)

cinelerra/audiorender.h
@@ -27,6 +27,7 @@
 #include "arraylist.h"
 #include "cinelerra.h"
 #include "edl.inc"
+#include "edit.inc"
 #include "file.inc"
 #include "levelhist.h"
 #include "renderbase.h"

cinelerra/menueffects.C
@@ -63,12 +63,8 @@ MenuEffects::MenuEffects()
 
 int MenuEffects::handle_event()
 {
-#if defined(NEW_RENDERER) || !defined(NEW_ARENDERER)
 	thread->set_title(0);
 	thread->start();
-#else
-	errorbox("Not ready");
-#endif
 	return 1;
 }
 

cinelerra/packagerenderer.C
@@ -21,6 +21,7 @@
 
 #include "aframe.h"
 #include "asset.h"
+#include "audiorender.h"
 #include "bcsignals.h"
 #include "cache.h"
 #include "clip.h"
@@ -45,16 +46,7 @@
 #include "transportcommand.h"
 #include "vframe.h"
 #include "videodevice.h"
-#ifdef NEW_ARENDERER
-#include "audiorender.h"
-#else
-#include "arender.h"
-#endif
-#ifdef NEW_RENDERER
 #include "videorender.h"
-#else
-#include "vrender.h"
-#endif
 
 
 RenderPackage::RenderPackage()

cinelerra/playbackengine.C
@@ -181,20 +181,14 @@ void PlaybackEngine::stop_tracking(ptstime position)
 		command->command = STOP;
 }
 
-#if defined(NEW_RENDERER) || defined(NEW_ARENDERER)
 void PlaybackEngine::set_tracking_position(ptstime pts)
 {
 	tracking_position = pts;
 }
-#endif
 
 ptstime PlaybackEngine::get_tracking_position()
 {
-#if defined(NEW_RENDERER) || defined(NEW_ARENDERER)
 	if(render_engine && tracking_active && !render_engine->do_video)
-#else
-	if(render_engine && tracking_active)
-#endif
 	{
 		ptstime tpts = render_engine->sync_postime() *
 			render_engine->command.get_speed();

cinelerra/playbackengine.h
@@ -69,9 +69,7 @@ public:
 	void stop_tracking(ptstime position = -1);
 // The playback cursor calls this to calculate the current tracking position
 	ptstime get_tracking_position();
-#ifdef NEW_RENDERER
 	void set_tracking_position(ptstime pts);
-#endif
 	void send_command(int command, EDL *new_edl = 0, int options = 0);
 
 	void run();

cinelerra/pluginaclient.C
@@ -81,7 +81,6 @@ int PluginAClient::plugin_process_loop(AFrame **aframes)
 
 void PluginAClient::get_frame(AFrame *frame)
 {
-#ifdef NEW_ARENDERER
 	if(renderer)
 	{
 		Track *current = renderer->get_track_number(frame->get_track());
@@ -89,9 +88,6 @@ void PluginAClient::get_frame(AFrame *frame)
 	}
 	else
 		frame->clear_frame(frame->pts, frame->source_duration);
-#else
-	server->get_aframe(frame);
-#endif
 }
 
 int PluginAClient::get_project_samplerate()

cinelerra/pluginserver.C
@@ -402,9 +402,6 @@ void PluginServer::process_buffer(VFrame **frame,
 	{
 		vclient->input[i] = frame[i];
 		vclient->output[i] = frame[i];
-#ifndef NEW_RENDERER
-		frame[i]->set_layer(i);
-#endif
 	}
 	if(plugin)
 	{
@@ -588,67 +585,6 @@ void PluginServer::stop_loop()
 	if(plugin_open)
 		client->plugin_stop_loop();
 }
-
-#ifndef NEW_RENDERER
-void PluginServer::get_vframe(VFrame *buffer,
-	int use_opengl)
-{
-	VFrame *frame;
-// Data source depends on whether we're part of a virtual console or a
-// plugin array.
-//     VirtualNode
-//     Module
-// If we're a VirtualNode, read_data in the virtual plugin node handles
-//     backward propogation and produces the data.
-// If we're a Module, render in the module produces the data.
-	if(!multichannel) buffer->set_layer(0);
-
-	int channel = buffer->get_layer();
-
-	if(nodes && nodes->total > channel)
-	{
-		// FIXIT: get rid of frame copy here
-		frame = ((VirtualVNode*)nodes->values[channel])->get_output_temp();
-		frame->copy_pts(buffer);
-		frame = ((VirtualVNode*)nodes->values[channel])->read_data();
-		buffer->copy_from(frame, 1);
-	}
-	else
-	if(modules && modules->total > channel)
-	{
-		((VModule*)modules->values[channel])->render(buffer);
-	}
-	else
-	{
-		errorbox("PluginServer::get_frame no object available for channel=%d",
-			channel);
-	}
-}
-#endif
-
-#ifndef NEW_ARENDERER
-void PluginServer::get_aframe(AFrame *aframe)
-{
-	if(!multichannel)
-		aframe->channel = 0;
-	if(nodes && nodes->total > aframe->channel)
-	{
-		((VirtualANode*)nodes->values[aframe->channel])->read_data(aframe);
-		return;
-	}
-	if(modules && modules->total > aframe->channel)
-	{
-		((AModule*)modules->values[aframe->channel])->render(aframe);
-		return;
-	}
-	aframe->clear_buffer();
-	if(aframe->buffer && aframe->source_length)
-	{
-		aframe->length = aframe->source_length;
-		aframe->duration = round((ptstime)aframe->length / aframe->samplerate);
-	}
-}
-#endif
 
 void PluginServer::raise_window()
 {

commit d94f37a72799d0d3b257af4bed052aeb9e589a29

Restore functioning of non-realtime plugins
Commit 9a73f1e3af broke non-realtime plugin

cinelerra/pluginclient.C
@@ -246,10 +246,15 @@ void PluginClient::process_buffer(VFrame **frame, ptstime total_length)
 	source_pts = frame[0]->get_pts();
 	total_len_pts = total_length;
 
-	if(server->multichannel)
-		process_frame(frame);
+	if(!server->realtime)
+		plugin_process_loop(frame);
 	else
-		process_frame(frame[0]);
+	{
+		if(server->multichannel)
+			process_frame(frame);
+		else
+			process_frame(frame[0]);
+	}
 }
 
 void PluginClient::process_buffer(AFrame **buffer, ptstime total_len)
@@ -262,18 +267,23 @@ void PluginClient::process_buffer(AFrame **buffer, ptstime total_len)
 	source_pts = aframe->pts;
 	total_len_pts = total_len;
 
-	if(server->multichannel)
-	{
-		int fragment_size = aframe->fill_length();
-
-		for(int i = 1; i < total_in_buffers; i++)
-			buffer[i]->set_fill_request(source_pts,
-				fragment_size);
-
-		process_frame(buffer);
-	}
+	if(!server->realtime)
+		plugin_process_loop(buffer);
 	else
-		process_frame(buffer[0]);
+	{
+		if(server->multichannel)
+		{
+			int fragment_size = aframe->fill_length();
+
+			for(int i = 1; i < total_in_buffers; i++)
+				buffer[i]->set_fill_request(source_pts,
+					fragment_size);
+
+			process_frame(buffer);
+		}
+		else
+			process_frame(buffer[0]);
+	}
 }
 
 void PluginClient::process_frame(AFrame **aframe)