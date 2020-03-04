80921e822da917fe50ed0f6c8c94764c9680be71
Einar Rünkaru <einarrunkaru@gmail.com>
2020-03-04 18:25:05 +0200

    Do not use get_input/get_output in Rotate plugin
    Use passed frame pointer instead

--- plugins/rotate/rotate.C
+++ plugins/rotate/rotate.C
@@ -554,8 +554,8 @@ void RotateEffect::process_frame(VFrame *frame)
 
 	if(!engine) engine = new AffineEngine(PluginClient::smp + 1, 
 		PluginClient::smp + 1);
-	engine->set_pivot((int)(config.pivot_x * get_input()->get_w() / 100), 
-		(int)(config.pivot_y * get_input()->get_h() / 100));
+	engine->set_pivot((int)(config.pivot_x * frame->get_w() / 100),
+		(int)(config.pivot_y * frame->get_h() / 100));
 
 	if(get_use_opengl())
 	{
@@ -564,9 +564,9 @@ void RotateEffect::process_frame(VFrame *frame)
 		return;
 	}
 
-	VFrame *temp_frame = PluginVClient::new_temp(get_input()->get_w(),
-		get_input()->get_h(),
-		get_input()->get_color_model());
+	VFrame *temp_frame = PluginVClient::new_temp(frame->get_w(),
+		frame->get_h(),
+		frame->get_color_model());
 	temp_frame->copy_pts(frame);
 	get_frame(temp_frame, get_use_opengl());
 
@@ -583,7 +583,7 @@ void RotateEffect::process_frame(VFrame *frame)
 	if(center_x >= 0 && center_x < w || \
 		center_y >= 0 && center_y < h) \
 	{ \
-		type *hrow = (type*)get_output()->get_row_ptr(center_y) + components * (center_x - CENTER_W / 2) + alpha_shift; \
+		type *hrow = (type*)frame->get_row_ptr(center_y) + components * (center_x - CENTER_W / 2) + alpha_shift; \
 		for(int i = center_x - CENTER_W / 2; i <= center_x + CENTER_W / 2; i++) \
 		{ \
 			if(i >= 0 && i < w) \
@@ -599,7 +599,7 @@ void RotateEffect::process_frame(VFrame *frame)
 		{ \
 			if(i >= 0 && i < h) \
 			{ \
-				type *vrow = (type*)get_output()->get_row_ptr(i) + center_x * components + alpha_shift; \
+				type *vrow = (type*)frame->get_row_ptr(i) + center_x * components + alpha_shift; \
 				vrow[0] = max - vrow[0]; \
 				vrow[1] = max - vrow[1]; \
 				vrow[2] = max - vrow[2]; \
@@ -612,7 +612,7 @@ void RotateEffect::process_frame(VFrame *frame)
 	{
 		int center_x = (int)(config.pivot_x * w / 100); \
 		int center_y = (int)(config.pivot_y * h / 100); \
-		switch(get_output()->get_color_model())
+		switch(frame->get_color_model())
 		{
 		case BC_RGB_FLOAT:
 			DRAW_CENTER(3, float, 1.0)
@@ -643,8 +643,8 @@ void RotateEffect::process_frame(VFrame *frame)
 	}
 
 // Conserve memory by deleting large frames
-	if(get_input()->get_w() > PLUGIN_MAX_W &&
-		get_input()->get_h() > PLUGIN_MAX_H)
+	if(frame->get_w() > PLUGIN_MAX_W &&
+		frame->get_h() > PLUGIN_MAX_H)
 	{
 		delete engine;
 		engine = 0;
@@ -653,6 +653,7 @@ void RotateEffect::process_frame(VFrame *frame)
 
 void RotateEffect::handle_opengl()
 {
+/* FIXIT
 #ifdef HAVE_GL
 	engine->set_opengl(1);
 	engine->rotate(get_output(), 
@@ -682,4 +683,5 @@ void RotateEffect::handle_opengl()
 		glDisable(GL_COLOR_LOGIC_OP);
 	}
 #endif
+	*/
 }
