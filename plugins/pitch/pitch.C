855267cf30d73729859ee45a78548600213d3f8d
Einar Rünkaru <einarrunkaru@gmail.com>
2020-04-24 22:12:01 +0300

    Implement v3 plugin API in "Pitch shift" audio plugin
    Make use of class Fourier.
    Use aframe buffer_length as window_size.
    Removed unused arrays anal_magn and anal_freq
    Todo: still creates clicks.

--- plugins/pitch/pitch.C
+++ plugins/pitch/pitch.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bctitle.h"
 #include "clip.h"
@@ -32,9 +16,7 @@
 #include <math.h>
 #include <string.h>
 
-#define WINDOW_SIZE 8192
-#define OVERSAMPLE 8
-
+#define OVERSAMPLE 0
 
 REGISTER_PLUGIN
 
@@ -44,13 +26,13 @@ PitchEffect::PitchEffect(PluginServer *server)
 {
 	PLUGIN_CONSTRUCTOR_MACRO
 	fft = 0;
+	input_frame = 0;
 }
 
 PitchEffect::~PitchEffect()
 {
 	PLUGIN_DESTRUCTOR_MACRO
-
-	if(fft) delete fft;
+	delete fft;
 }
 
 PLUGIN_CLASS_METHODS
@@ -101,31 +83,31 @@ void PitchEffect::save_defaults()
 	defaults->save();
 }
 
-void PitchEffect::process_frame(AFrame *aframe)
+AFrame *PitchEffect::process_tmpframe(AFrame *aframe)
 {
 	load_configuration();
+	input_frame = aframe;
 
 	if(!fft)
-	{
-		fft = new PitchFFT(this, WINDOW_SIZE);
-		fft->set_oversample(OVERSAMPLE);
-	}
+		fft = new PitchFFT(this, aframe->buffer_length / 2);
+	else if(need_reconfigure)
+		fft->reset_phase = 1;
 
-	fft->process_frame_oversample(aframe);
+	aframe = fft->process_frame(aframe);
+	return aframe;
 }
 
 
 PitchFFT::PitchFFT(PitchEffect *plugin, int window_size)
- : CrossfadeFFT(window_size)
+ : Fourier(window_size)
 {
 	this->plugin = plugin;
-	last_phase = new double[WINDOW_SIZE];
-	new_freq = new double[WINDOW_SIZE];
-	new_magn = new double[WINDOW_SIZE];
-	sum_phase = new double[WINDOW_SIZE];
-	anal_magn = new double[WINDOW_SIZE];
-	anal_freq = new double[WINDOW_SIZE];
-
+	oversample = OVERSAMPLE;
+	reset_phase = 1;
+	last_phase = new double[window_size];
+	new_freq = new double[window_size];
+	new_magn = new double[window_size];
+	sum_phase = new double[window_size];
 }
 
 PitchFFT::~PitchFFT()
@@ -134,43 +116,51 @@ PitchFFT::~PitchFFT()
 	delete [] new_freq;
 	delete [] new_magn;
 	delete [] sum_phase;
-	delete [] anal_magn;
-	delete [] anal_freq;
 }
 
-void PitchFFT::signal_process_oversample(int reset)
+int PitchFFT::signal_process()
 {
+	int window_size = get_window_size();
+	int half_size = window_size / 2;
 	double scale = plugin->config.scale;
+	double expected_phase_diff;
 
 	memset(new_freq, 0, window_size * sizeof(double));
 	memset(new_magn, 0, window_size * sizeof(double));
 
-	if (reset)
+	if(reset_phase)
 	{
-		memset (last_phase, 0, WINDOW_SIZE * sizeof(double));
-		memset (sum_phase, 0, WINDOW_SIZE * sizeof(double));
+		memset(last_phase, 0, window_size * sizeof(double));
+		memset(sum_phase, 0, window_size * sizeof(double));
+		reset_phase = 0;
 	}
 
 // expected phase difference between windows
-	double expected_phase_diff = 2.0 * M_PI / oversample; 
+	if(oversample)
+		expected_phase_diff = 2.0 * M_PI / oversample;
+	else
+		expected_phase_diff = 0;
 // frequency per bin
-	double freq_per_bin = (double)plugin->project_sample_rate / window_size;
+	double freq_per_bin = (double)plugin->input_frame->samplerate / window_size;
 
-	for (int i = 0; i < window_size/2; i++) 
+	for(int i = 0; i < half_size; i++)
 	{
 // Convert to magnitude and phase
-		double magn = sqrt(fftw_data[i][0] * fftw_data[i][0] + fftw_data[i][1] * fftw_data[i][1]);
-		double phase = atan2(fftw_data[i][1], fftw_data[i][0]);
+		double re = fftw_window[i][0];
+		double im = fftw_window[i][1];
+		double magn = sqrt(re * re + im * im);
+		double phase = atan2(im, re);
 
 // Remember last phase
 		double temp = phase - last_phase[i];
 		last_phase[i] = phase;
 
 // Substract the expected advancement of phase
-		temp -= (double)i * expected_phase_diff;
+		if(oversample)
+			temp -= (double)i * expected_phase_diff;
 
 // wrap temp into -/+ PI ...  good trick!
-		int qpd = (int)(temp/M_PI);
+		int qpd = (int)(temp / M_PI);
 		if (qpd >= 0) 
 			qpd += qpd&1;
 		else 
@@ -178,21 +168,21 @@ void PitchFFT::signal_process_oversample(int reset)
 		temp -= M_PI*(double)qpd;
 
 // Deviation from bin frequency
-		temp = oversample * temp / (2.0 * M_PI);
+		if(oversample)
+			temp = oversample * temp / (2.0 * M_PI);
 
 		temp = (double)(temp + i) * freq_per_bin;
 
 // Now temp is the real freq... move it!
 		int new_bin = (int)(i * scale);
-		if (new_bin >= 0 && new_bin < window_size/2)
+		if(new_bin >= 0 && new_bin < half_size)
 		{
-			new_freq[new_bin] = temp*scale;
+			new_freq[new_bin] = temp * scale;
 			new_magn[new_bin] += magn;
 		}
 	}
-
-	// Synthesize back the fft window 
-	for (int i = 0; i < window_size/2; i++) 
+// Synthesize back the fft window
+	for (int i = 0; i < half_size; i++)
 	{
 		double magn = new_magn[i];
 		double temp = new_freq[i];
@@ -202,33 +192,29 @@ void PitchFFT::signal_process_oversample(int reset)
 // get bin deviation from freq deviation
 		temp /= freq_per_bin;
 
-// oversample 
-		temp = 2.0 * M_PI *temp / oversample;
-
+		if(oversample)
+		{
+			temp = 2.0 * M_PI * temp / oversample;
 // add back the expected phase difference (that we substracted in analysis)
-		temp += (double)(i) * expected_phase_diff;
+			temp += (double)(i) * expected_phase_diff;
+		}
 
 // accumulate delta phase, to get bin phase
 		sum_phase[i] += temp;
 
 		double phase = sum_phase[i];
 
-		fftw_data[i][0] = magn * cos(phase);
-		fftw_data[i][1] = magn * sin(phase);
+		fftw_window[i][0] = magn * cos(phase);
+		fftw_window[i][1] = magn * sin(phase);
 	}
 
-	for (int i = window_size/2; i< window_size; i++)
+	for(int i = half_size; i < window_size; i++)
 	{
-		fftw_data[i][0] = 0;
-		fftw_data[i][1] = 0;
+		fftw_window[i][0] = 0;
+		fftw_window[i][1] = 0;
 	}
+	return 1;
 }
-
-void PitchFFT::get_frame(AFrame *aframe)
-{
-	plugin->get_frame(aframe);
-}
-
 
 PitchConfig::PitchConfig()
 {
--- plugins/pitch/pitch.h
+++ plugins/pitch/pitch.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef PITCH_H
 #define PITCH_H
@@ -25,19 +9,18 @@
 #define PLUGIN_TITLE N_("Pitch shift")
 #define PLUGIN_IS_AUDIO
 #define PLUGIN_IS_REALTIME
+#define PLUGIN_USES_TMPFRAME
 #define PLUGIN_CLASS PitchEffect
 #define PLUGIN_CONFIG_CLASS PitchConfig
 #define PLUGIN_THREAD_CLASS PitchThread
 #define PLUGIN_GUI_CLASS PitchWindow
 
 #include "pluginmacros.h"
+
 #include "bcpot.h"
-#include "bchash.inc"
 #include "fourier.h"
-#include "mutex.h"
 #include "pluginaclient.h"
 #include "pluginwindow.h"
-#include "vframe.inc"
 
 
 class PitchScale : public BC_FPot
@@ -79,22 +62,21 @@ public:
 	PLUGIN_CONFIG_CLASS_MEMBERS
 };
 
-class PitchFFT : public CrossfadeFFT
+class PitchFFT : public Fourier
 {
 public:
 	PitchFFT(PitchEffect *plugin, int window_size);
 	~PitchFFT();
-	void signal_process_oversample(int reset);
-	void get_frame(AFrame *aframe);
+	int signal_process();
 
 	PitchEffect *plugin;
 
+	int oversample;
+	int reset_phase;
 	double *last_phase;
 	double *new_freq;
 	double *new_magn;
 	double *sum_phase;
-	double *anal_freq;
-	double *anal_magn;
 };
 
 class PitchEffect : public PluginAClient
@@ -108,11 +90,12 @@ public:
 	void read_data(KeyFrame *keyframe);
 	void save_data(KeyFrame *keyframe);
 
-	void process_frame(AFrame *aframe);
+	AFrame *process_tmpframe(AFrame *aframe);
 
 	void load_defaults();
 	void save_defaults();
 
+	AFrame *input_frame;
 	PitchFFT *fft;
 };
 
