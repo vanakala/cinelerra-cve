9d0e97d7e8d1caa2e78c538807f211f87b197e1d
Einar Rünkaru <einarrunkaru@gmail.com>
2020-05-18 23:03:59 +0300

    Implement v3 plugin API in "Dissolve" video plugin
    No change in api of a transition
    Fixed crash when length is zero, some cosmetics
    Removed unused dissolve.inc

--- plugins/dissolve/dissolve.C
+++ plugins/dissolve/dissolve.C
@@ -1,26 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "dissolve.h"
-#include "edl.inc"
 #include "overlayframe.h"
 #include "picon_png.h"
 #include "vframe.h"
@@ -46,6 +29,12 @@ PLUGIN_CLASS_METHODS
 
 void DissolveMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 {
+	int w = outgoing->get_w();
+	int h = outgoing->get_h();
+
+	if(total_len_pts < EPSILON)
+		return;
+
 	fade = source_pts / total_len_pts;
 
 // Use hardware
@@ -56,8 +45,8 @@ void DissolveMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 	}
 
 // Use software
-	if(!overlayer) overlayer = new OverlayFrame(get_project_smp() + 1);
-
+	if(!overlayer)
+		overlayer = new OverlayFrame(get_project_smp() + 1);
 
 // There is a problem when dissolving from a big picture to a small picture.
 // In order to make it dissolve correctly, we have to manually decrese alpha of big picture.
@@ -65,69 +54,60 @@ void DissolveMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 	{
 	case BC_RGBA8888:
 	case BC_YUVA8888:
+		for(int i = 0; i < h; i++)
 		{
-			int w = outgoing->get_w();
-			int h = outgoing->get_h(); 
-			for(int i = 0; i < h; i++) 
-			{ 
-				uint8_t* alpha_chan = (uint8_t *)outgoing->get_row_ptr(i) + 3;
-				for(int j = 0; j < w; j++) 
-				{
-					*alpha_chan = (uint8_t) (*alpha_chan * (1-fade));
-					alpha_chan+=4;
-				} 
-			}
-			break;
-		}
-	case BC_YUVA16161616:
-		{
-			int w = outgoing->get_w();
-			int h = outgoing->get_h(); 
-			for(int i = 0; i < h; i++) 
-			{ 
-				uint16_t* alpha_chan = (uint16_t*)outgoing->get_row_ptr(i) + 3; // 3 since this is uint16_t
-				for(int j = 0; j < w; j++) 
-				{
-					*alpha_chan = (uint16_t)(*alpha_chan * (1-fade));
-					alpha_chan += 8;
-				} 
-			}
-			break;
-		}
-	case BC_AYUV16161616:
-		{
-			int w = outgoing->get_w();
-			int h = outgoing->get_h();
-			for(int i = 0; i < h; i++)
+			uint8_t* alpha_chan = (uint8_t *)outgoing->get_row_ptr(i) + 3;
+
+			for(int j = 0; j < w; j++)
 			{
-				uint16_t* alpha_chan = (uint16_t*)outgoing->get_row_ptr(i);
-				for(int j = 0; j < w; j++)
-				{
-					*alpha_chan = (uint16_t)(*alpha_chan * (1-fade));
-					alpha_chan += 4;
-				}
+				*alpha_chan = (uint8_t)(*alpha_chan * (1 - fade));
+				alpha_chan += 4;
 			}
-			break;
 		}
-	case BC_RGBA_FLOAT:
+		break;
+
+	case BC_YUVA16161616:
+	case BC_RGBA16161616:
+		for(int i = 0; i < h; i++)
 		{
-			int w = outgoing->get_w();
-			int h = outgoing->get_h(); 
-			for(int i = 0; i < h; i++) 
-			{ 
-				float* alpha_chan = (float*)outgoing->get_row_ptr(i) + 3;
-				for(int j = 0; j < w; j++) 
-				{
-					*alpha_chan = *alpha_chan * (1-fade);
-					alpha_chan += sizeof(float);
-				} 
+			uint16_t* alpha_chan = (uint16_t*)outgoing->get_row_ptr(i) + 3;
+
+			for(int j = 0; j < w; j++)
+			{
+				*alpha_chan = (uint16_t)(*alpha_chan * (1 - fade));
+				alpha_chan += 8;
 			}
-			break;
 		}
+		break;
+
+	case BC_AYUV16161616:
+		for(int i = 0; i < h; i++)
+		{
+			uint16_t* alpha_chan = (uint16_t*)outgoing->get_row_ptr(i);
+
+			for(int j = 0; j < w; j++)
+			{
+				*alpha_chan = (uint16_t)(*alpha_chan * (1-fade));
+				alpha_chan += 4;
+			}
+		}
+		break;
+
+	case BC_RGBA_FLOAT:
+		for(int i = 0; i < h; i++)
+		{
+			float* alpha_chan = (float*)outgoing->get_row_ptr(i) + 3;
+
+			for(int j = 0; j < w; j++)
+			{
+				*alpha_chan = *alpha_chan * (1 - fade);
+				alpha_chan += sizeof(float);
+			}
+		}
+		break;
 	default:
 		break;
 	}
-
 
 	overlayer->overlay(outgoing, 
 		incoming, 
