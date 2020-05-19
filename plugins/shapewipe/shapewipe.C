db6fa9174c9d8844d85c136c2dcd8a7231988529
Einar Rünkaru <einarrunkaru@gmail.com>
2020-05-19 17:19:41 +0300

    Implement v3 plugin API in "Shape Wipe" video transition
    No change in api of a transition
    Some cosmetics

--- plugins/shapewipe/shapewipe.C
+++ plugins/shapewipe/shapewipe.C
@@ -1,27 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
+
 
 #include "bctitle.h"
 #include "bchash.h"
-#include "edl.inc"
 #include "filexml.h"
 #include "language.h"
 #include "mainerror.h"
@@ -41,10 +25,7 @@ ShapeWipeW2B::ShapeWipeW2B(ShapeWipeMain *plugin,
 	ShapeWipeWindow *window,
 	int x,
 	int y)
- : BC_Radial(x, 
-		y, 
-		plugin->direction == 0, 
-		_("White to Black"))
+ : BC_Radial(x, y, plugin->direction == 0, _("White to Black"))
 {
 	this->plugin = plugin;
 	this->window = window;
@@ -63,10 +44,7 @@ ShapeWipeB2W::ShapeWipeB2W(ShapeWipeMain *plugin,
 	ShapeWipeWindow *window,
 	int x,
 	int y)
- : BC_Radial(x, 
-		y, 
-		plugin->direction == 1, 
-		_("Black to White"))
+ : BC_Radial(x, y, plugin->direction == 1, _("Black to White"))
 {
 	this->plugin = plugin;
 	this->window = window;
@@ -85,7 +63,7 @@ ShapeWipeAntiAlias::ShapeWipeAntiAlias(ShapeWipeMain *plugin,
 	ShapeWipeWindow *window,
 	int x,
 	int y)
- : BC_CheckBox (x,y,plugin->antialias, _("Anti-aliasing"))
+ : BC_CheckBox(x, y, plugin->antialias, _("Anti-aliasing"))
 {
 	this->plugin = plugin;
 	this->window = window;
@@ -102,7 +80,7 @@ ShapeWipePreserveAspectRatio::ShapeWipePreserveAspectRatio(ShapeWipeMain *plugin
 	ShapeWipeWindow *window,
 	int x,
 	int y)
- : BC_CheckBox (x,y,plugin->preserve_aspect, _("Preserve shape aspect ratio"))
+ : BC_CheckBox(x,y,plugin->preserve_aspect, _("Preserve shape aspect ratio"))
 {
 	this->plugin = plugin;
 	this->window = window;
@@ -121,7 +99,7 @@ ShapeWipeFilename::ShapeWipeFilename(
 	char *value,
 	int x,
 	int y)
- : BC_TextBox(x,y,180,1, value)
+ : BC_TextBox(x, y, 180, 1, value)
 {
 	this->plugin = plugin;
 	this->window = window;
@@ -142,7 +120,7 @@ ShapeWipeBrowseButton::ShapeWipeBrowseButton(
 	ShapeWipeFilename *filename,
 	int x,
 	int y)
- : BC_GenericButton(x,y,_("Browse..."))
+ : BC_GenericButton(x, y, _("Browse..."))
 {
 	this->plugin = plugin;
 	this->window = window;
@@ -239,7 +217,7 @@ ShapeWipeMain::ShapeWipeMain(PluginServer *server)
  : PluginVClient(server)
 {
 	direction = 0;
-	strcpy(filename, DEFAULT_SHAPE);	// is defined by a -D compiler instruction
+	strcpy(filename, DEFAULT_SHAPE);
 	last_read_filename[0] = '\0';
 	pattern_image = NULL;
 	min_value = 255;
@@ -338,22 +316,22 @@ int ShapeWipeMain::read_pattern_image(int new_frame_width, int new_frame_height)
 	frame_height = new_frame_height;
 
 	FILE *fp = fopen(filename, "rb");
-	if (!fp)
-	{
+
+	if(!fp)
 		return 1;
-	}
 
 	if(fread(header, 1, 8, fp) < 8)
 		goto erret;
+
 	is_png = !png_sig_cmp(header, 0, 8);
 
-	if (!is_png)
+	if(!is_png)
 		goto erret;
 
 	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
 		NULL, NULL, NULL);
 
-	if (!png_ptr)
+	if(!png_ptr)
 		goto erret;
 
 	/* Tell libpng we already checked the first 8 bytes */
@@ -386,25 +364,27 @@ erret:
 /* Skip the alpha channel if present 
  * stripping alpha currently doesn't work in conjunction with 
  * converting to grayscale in libpng */
-	if (color_type & PNG_COLOR_MASK_ALPHA)
+	if(color_type & PNG_COLOR_MASK_ALPHA)
 		pixel_width = 2;
 	else
 		pixel_width = 1;
 
 /* Convert 16 bit data to 8 bit */
-	if (bit_depth == 16) png_set_strip_16(png_ptr);
+	if(bit_depth == 16)
+		png_set_strip_16(png_ptr);
 
 /* Expand to 1 pixel per byte if necessary */
-	if (bit_depth < 8) png_set_packing(png_ptr);
+	if(bit_depth < 8)
+		png_set_packing(png_ptr);
 
 /* Convert to grayscale */
-	if (color_type == PNG_COLOR_TYPE_RGB || 
-		color_type == PNG_COLOR_TYPE_RGB_ALPHA)
-	png_set_rgb_to_gray_fixed(png_ptr, 1, -1, -1);
+	if(color_type == PNG_COLOR_TYPE_RGB ||
+			color_type == PNG_COLOR_TYPE_RGB_ALPHA)
+		png_set_rgb_to_gray_fixed(png_ptr, 1, -1, -1);
 
 /* Allocate memory to hold the original png image */
 	image = (png_bytep*)malloc(sizeof(png_bytep)*height);
-	for (row = 0; row < height; row++)
+	for(row = 0; row < height; row++)
 	{
 		image[row] = (png_byte*)malloc(sizeof(png_byte)*width*pixel_width);
 	}
@@ -413,74 +393,75 @@ erret:
  * used for the wipe */
 	pattern_image = (unsigned char**)malloc(sizeof(unsigned char*)*frame_height);
 
-
 	png_read_image(png_ptr, image);
 	png_read_end(png_ptr, end_info);
 
 	double row_factor, col_factor;
-	double row_offset = 0.5, col_offset = 0.5;	// for rounding
+	double row_offset = 0.5, col_offset = 0.5;         // for rounding
 
 	if(preserve_aspect && sample_aspect_ratio)
 	{
 		double aspect_ratio = sample_aspect_ratio * frame_width / frame_height;
-		row_factor = (height-1);
-		col_factor = (width-1) / aspect_ratio;
+		row_factor = (height - 1);
+		col_factor = (width - 1) / aspect_ratio;
 		if (row_factor < col_factor)
 			col_factor = row_factor;
 		else
 			row_factor = col_factor;
-		row_factor *= 1.0 / (double)(frame_height-1);
-		col_factor *= aspect_ratio / (double)(frame_width-1);
+		row_factor *= 1.0 / (double)(frame_height - 1);
+		col_factor *= aspect_ratio / (double)(frame_width - 1);
 
 		// center the pattern over the frame
-		row_offset += (height-1-(frame_height-1)*row_factor)/2;
-		col_offset += (width-1-(frame_width-1)*col_factor)/2;
+		row_offset += (height - 1 - (frame_height - 1) * row_factor) / 2;
+		col_offset += (width - 1 - (frame_width - 1) * col_factor) / 2;
 	}
 	else
 	{
 		// Stretch (or shrink) the pattern image to fill the frame
-		row_factor = (double)(height-1)/(double)(frame_height-1);
-		col_factor = (double)(width-1)/(double)(frame_width-1);
+		row_factor = (double)(height - 1) / (double)(frame_height - 1);
+		col_factor = (double)(width - 1) / (double)(frame_width - 1);
 	}
 
-	for (scaled_row = 0; scaled_row < frame_height; scaled_row++)
+	for(scaled_row = 0; scaled_row < frame_height; scaled_row++)
 	{
 		row = (int)(row_factor*scaled_row + row_offset);
-		pattern_image[scaled_row] = (unsigned char*)malloc(sizeof(unsigned char)*frame_width);
-		for (scaled_col = 0; scaled_col < frame_width; scaled_col++)
+		pattern_image[scaled_row] =
+			(unsigned char*)malloc(sizeof(unsigned char) * frame_width);
+		for(scaled_col = 0; scaled_col < frame_width; scaled_col++)
 		{
-			col = (int)(col_factor*scaled_col + col_offset)*pixel_width;
+			col = (int)(col_factor * scaled_col + col_offset) * pixel_width;
 			value = image[row][col];
 			pattern_image[scaled_row][scaled_col] = value;
-			if (value < min_value) min_value = value;
-			if (value > max_value) max_value = value;
+			if(value < min_value)
+				min_value = value;
+			if(value > max_value)
+				max_value = value;
 
 		}
 	}
 
 	png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
 	fclose(fp);
-/* Deallocate the original image as it is no longer needed */
-	for (row = 0; row < height; row++)
-	{
+// Deallocate the original image as it is no longer needed
+	for(row = 0; row < height; row++)
 		free(image[row]);
-	}
-	free (image);
+	free(image);
 	return 0;
 }
 
 void ShapeWipeMain::reset_pattern_image()
 {
 	int row;
-	if (pattern_image != NULL)
+
+	if(pattern_image)
 	{
-		for (row = 0; row < frame_height; row++)
+		for(row = 0; row < frame_height; row++)
 		{
-			free (pattern_image[row]);
+			free(pattern_image[row]);
 		}
-		free (pattern_image);
+		free(pattern_image);
 		pattern_image = NULL;
-		min_value = 255, max_value = 0;	// are recalc'd in read_pattern_image
+		min_value = 255, max_value = 0;  // are recalc'd in read_pattern_image
 	}
 }
 
@@ -491,13 +472,13 @@ void ShapeWipeMain::reset_pattern_image()
 		type *in_row = (type*)incoming->get_row_ptr(j); \
 		type *out_row = (type*)outgoing->get_row_ptr(j); \
 		pattern_row = pattern_image[j]; \
-		\
+ \
 		col_offset = 0; \
 		for(k = 0; k < w; k++) \
 		{ \
 			value = pattern_row[k]; \
-			if ((direction == 0 && value >= threshold) || \
-			(direction == 1 && value <= threshold)) \
+			if((direction == 0 && value >= threshold) || \
+				(direction == 1 && value <= threshold)) \
 			{ \
 				out_row[col_offset]     = in_row[col_offset]; \
 				out_row[col_offset + 1] = in_row[col_offset + 1]; \
@@ -512,12 +493,14 @@ void ShapeWipeMain::reset_pattern_image()
 
 #define COMPARE1(x,y) \
 { \
-	if (pattern_image[x][y] <= threshold) opacity++; \
+	if(pattern_image[x][y] <= threshold) \
+		opacity++; \
 }
 
 #define COMPARE2(x,y) \
 { \
-	if (pattern_image[x][y] >= threshold) opacity++; \
+	if(pattern_image[x][y] >= threshold) \
+		opacity++; \
 }
 
 // components is always 4
@@ -561,7 +544,7 @@ void ShapeWipeMain::reset_pattern_image()
 	float pixel_transparency = 1.0 - pixel_opacity; \
 	int col; \
  \
-	if (pixel_opacity > 0.0) \
+	if(pixel_opacity > 0.0) \
 	{ \
 		switch(incoming->get_color_model()) \
 		{ \
@@ -645,28 +628,30 @@ void ShapeWipeMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 
 	load_configuration();
 
+	if(total_len_pts < EPSILON)
+		return;
+
 	int w = incoming->get_w();
 	int h = incoming->get_h();
 
-	if (strncmp(filename,last_read_filename, BCTEXTLEN)
-		|| preserve_aspect != last_preserve_aspect)
-	{
+	if(strncmp(filename,last_read_filename, BCTEXTLEN)
+			|| preserve_aspect != last_preserve_aspect)
 		reset_pattern_image();
-	}
 
-	if (!pattern_image) {
+	if(!pattern_image)
+	{
 		read_pattern_image(w, h);
 		strncpy(last_read_filename, filename, BCTEXTLEN);
 		last_preserve_aspect = preserve_aspect;
 	}
 
-	if (!pattern_image)
+	if(!pattern_image)
 	{
 		abort_plugin("Cannot load shape %s", filename);
 		return;
 	}
 
-	if (direction)
+	if(direction)
 	{
 		threshold = (unsigned char)round(source_pts / total_len_pts * (max_value - min_value))
 			+ min_value;
@@ -679,94 +664,94 @@ void ShapeWipeMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 			+ min_value;
 	}
 
-	if (antialias)
+	if(antialias)
 	{
-		if (direction)
+		if(direction)
 		{
-			/* Top left corner */
+			// Top left corner
 			opacity = 0;
-			COMPARE1(0,0);
-			COMPARE1(0,1);
-			COMPARE1(1,0);
-			COMPARE1(1,1);
-			BLEND(0,0,4.0);
+			COMPARE1(0, 0);
+			COMPARE1(0, 1);
+			COMPARE1(1, 0);
+			COMPARE1(1, 1);
+			BLEND(0, 0, 4.0);
 
-			/* Top edge */
-			for (k = 1; k < w-1; k++)
+			// Top edge
+			for(k = 1; k < w - 1; k++)
 			{
 				opacity = 0;
-				COMPARE1(0,k-1);
-				COMPARE1(0,k);
-				COMPARE1(0,k+1);
-				COMPARE1(1,k-1);
+				COMPARE1(0, k - 1);
+				COMPARE1(0, k);
+				COMPARE1(0, k + 1);
+				COMPARE1(1, k - 1);
 				COMPARE1(1,k);
-				COMPARE1(1,k+1);
-				BLEND(0,k,6.0);
+				COMPARE1(1, k + 1);
+				BLEND(0, k, 6.0);
 			}
 
-			/* Top right corner */
+			// Top right corner
 			opacity = 0;
-			COMPARE1(0,w-1);
-			COMPARE1(0,w-2);
-			COMPARE1(1,w-1);
-			COMPARE1(1,w-2);
-			BLEND(0,w-1,4.0);
+			COMPARE1(0, w - 1);
+			COMPARE1(0, w - 2);
+			COMPARE1(1, w - 1);
+			COMPARE1(1, w - 2);
+			BLEND(0, w - 1, 4.0);
 
-			/* Left edge */
-			for (j = 1; j < h-1; j++)
+			// Left edge
+			for(j = 1; j < h - 1; j++)
 			{
 				opacity = 0;
-				COMPARE1(j-1,0);
-				COMPARE1(j,0);
-				COMPARE1(j+1,0);
-				COMPARE1(j-1,1);
-				COMPARE1(j,1);
-				COMPARE1(j+1,1);
-				BLEND(j,0,6.0);
+				COMPARE1(j - 1, 0);
+				COMPARE1(j, 0);
+				COMPARE1(j + 1, 0);
+				COMPARE1(j - 1, 1);
+				COMPARE1(j, 1);
+				COMPARE1(j + 1, 1);
+				BLEND(j, 0, 6.0);
 			}
 
-			/* Middle */
-			for (j = 1; j < h-1; j++)
+			// Middle
+			for(j = 1; j < h-1; j++)
 			{
-				for (k = 1; k < w-1; k++)
+				for(k = 1; k < w-1; k++)
 				{
 					opacity = 0;
-					COMPARE1(j-1,k-1);
-					COMPARE1(j,k-1);
-					COMPARE1(j+1,k-1);
-					COMPARE1(j-1,k);
-					COMPARE1(j,k);
-					COMPARE1(j+1,k);
-					COMPARE1(j-1,k+1);
-					COMPARE1(j,k+1);
-					COMPARE1(j+1,k+1);
-					BLEND(j,k,9.0);
+					COMPARE1(j - 1,k - 1);
+					COMPARE1(j, k -1);
+					COMPARE1(j + 1,k - 1);
+					COMPARE1(j - 1, k);
+					COMPARE1(j, k);
+					COMPARE1(j + 1, k);
+					COMPARE1(j - 1, k + 1);
+					COMPARE1(j, k + 1);
+					COMPARE1(j + 1,k + 1);
+					BLEND(j, k, 9.0);
 				}
 			}
 
-			/* Right edge */
-			for (j = 1; j < h-1; j++)
+			// Right edge
+			for(j = 1; j < h - 1; j++)
 			{
 				opacity = 0;
-				COMPARE1(j-1,w-1);
-				COMPARE1(j,w-1);
-				COMPARE1(j+1,w-1);
-				COMPARE1(j-1,w-2);
-				COMPARE1(j,w-2);
-				COMPARE1(j+1,w-2);
-				BLEND(j,w-1,6.0);
+				COMPARE1(j - 1, w - 1);
+				COMPARE1(j, w - 1);
+				COMPARE1(j + 1,w - 1);
+				COMPARE1(j - 1,w - 2);
+				COMPARE1(j, w - 2);
+				COMPARE1(j + 1,w - 2);
+				BLEND(j, w - 1, 6.0);
 			}
 
-			/* Bottom left corner */
+			// Bottom left corner
 			opacity = 0;
-			COMPARE1(h-1,0);
-			COMPARE1(h-1,1);
-			COMPARE1(h-2,0);
-			COMPARE1(h-2,1);
-			BLEND(h-1,0,4.0);
+			COMPARE1(h - 1, 0);
+			COMPARE1(h - 1, 1);
+			COMPARE1(h - 2, 0);
+			COMPARE1(h - 2, 1);
+			BLEND(h - 1, 0, 4.0);
 
-			/* Bottom edge */
-			for (k = 1; k < w-1; k++)
+			// Bottom edge
+			for(k = 1; k < w - 1; k++)
 			{
 				opacity = 0;
 				COMPARE1(h-1,k-1);
@@ -778,118 +763,118 @@ void ShapeWipeMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 				BLEND(h-1,k,6.0);
 			}
 
-			/* Bottom right corner */
+			// Bottom right corner
 			opacity = 0;
-			COMPARE1(h-1,w-1);
-			COMPARE1(h-1,w-2);
-			COMPARE1(h-2,w-1);
-			COMPARE1(h-2,w-2);
-			BLEND(h-1,w-1,4.0);
+			COMPARE1(h - 1, w - 1);
+			COMPARE1(h - 1, w - 2);
+			COMPARE1(h - 2, w - 1);
+			COMPARE1(h - 2, w - 2);
+			BLEND(h - 1, w - 1, 4.0);
 		}
 		else
 		{
-			/* Top left corner */
+			// Top left corner */
 			opacity = 0;
-			COMPARE2(0,0);
-			COMPARE2(0,1);
-			COMPARE2(1,0);
-			COMPARE2(1,1);
-			BLEND(0,0,4.0);
+			COMPARE2(0, 0);
+			COMPARE2(0, 1);
+			COMPARE2(1, 0);
+			COMPARE2(1, 1);
+			BLEND(0, 0, 4.0);
 
-			/* Top edge */
-			for (k = 1; k < w-1; k++)
+			// Top edge
+			for(k = 1; k < w - 1; k++)
 			{
 				opacity = 0;
-				COMPARE2(0,k-1);
-				COMPARE2(0,k);
-				COMPARE2(0,k+1);
-				COMPARE2(1,k-1);
-				COMPARE2(1,k);
-				COMPARE2(1,k+1);
-				BLEND(0,k,6.0);
+				COMPARE2(0, k - 1);
+				COMPARE2(0, k);
+				COMPARE2(0, k + 1);
+				COMPARE2(1, k - 1);
+				COMPARE2(1, k);
+				COMPARE2(1, k + 1);
+				BLEND(0, k, 6.0);
 			}
 
-			/* Top right corner */
+			// Top right corner
 			opacity = 0;
-			COMPARE2(0,w-1);
-			COMPARE2(0,w-2);
-			COMPARE2(1,w-1);
-			COMPARE2(1,w-2);
-			BLEND(0,w-1,4.0);
+			COMPARE2(0, w - 1);
+			COMPARE2(0, w - 2);
+			COMPARE2(1, w - 1);
+			COMPARE2(1, w - 2);
+			BLEND(0, w - 1, 4.0);
 
-			/* Left edge */
-			for (j = 1; j < h-1; j++)
+			// Left edge
+			for(j = 1; j < h - 1; j++)
 			{
 				opacity = 0;
-				COMPARE2(j-1,0);
-				COMPARE2(j,0);
-				COMPARE2(j+1,0);
-				COMPARE2(j-1,1);
-				COMPARE2(j,1);
-				COMPARE2(j+1,1);
-				BLEND(j,0,6.0);
+				COMPARE2(j - 1, 0);
+				COMPARE2(j, 0);
+				COMPARE2(j + 1, 0);
+				COMPARE2(j - 1, 1);
+				COMPARE2(j, 1);
+				COMPARE2(j + 1, 1);
+				BLEND(j, 0, 6.0);
 			}
 
-			/* Middle */
-			for (j = 1; j < h-1; j++)
+			// Middle
+			for(j = 1; j < h-1; j++)
 			{
-				for (k = 1; k < w-1; k++)
+				for(k = 1; k < w-1; k++)
 				{
 					opacity = 0;
-					COMPARE2(j-1,k-1);
-					COMPARE2(j,k-1);
-					COMPARE2(j+1,k-1);
-					COMPARE2(j-1,k);
-					COMPARE2(j,k);
-					COMPARE2(j+1,k);
-					COMPARE2(j-1,k+1);
-					COMPARE2(j,k+1);
-					COMPARE2(j+1,k+1);
-					BLEND(j,k,9.0);
+					COMPARE2(j - 1, k - 1);
+					COMPARE2(j, k - 1);
+					COMPARE2(j + 1, k - 1);
+					COMPARE2(j - 1, k);
+					COMPARE2(j, k);
+					COMPARE2(j + 1, k);
+					COMPARE2(j - 1,k + 1);
+					COMPARE2(j, k + 1);
+					COMPARE2(j + 1, k + 1);
+					BLEND(j, k, 9.0);
 				}
 			}
 
-			/* Right edge */
-			for (j = 1; j < h-1; j++)
+			// Right edge
+			for(j = 1; j < h-1; j++)
 			{
 				opacity = 0;
-				COMPARE2(j-1,w-1);
-				COMPARE2(j,w-1);
-				COMPARE2(j+1,w-1);
-				COMPARE2(j-1,w-2);
-				COMPARE2(j,w-2);
-				COMPARE2(j+1,w-2);
-				BLEND(j,w-1,6.0);
+				COMPARE2(j - 1,w - 1);
+				COMPARE2(j, w - 1);
+				COMPARE2(j + 1, w - 1);
+				COMPARE2(j - 1,w - 2);
+				COMPARE2(j, w - 2);
+				COMPARE2(j + 1,w - 2);
+				BLEND(j, w - 1, 6.0);
 			}
 
-			/* Bottom left corner */
+			// Bottom left corner
 			opacity = 0;
-			COMPARE2(h-1,0);
-			COMPARE2(h-1,1);
-			COMPARE2(h-2,0);
-			COMPARE2(h-2,1);
-			BLEND(h-1,0,4.0);
+			COMPARE2(h - 1, 0);
+			COMPARE2(h - 1, 1);
+			COMPARE2(h - 2, 0);
+			COMPARE2(h - 2, 1);
+			BLEND(h - 1, 0, 4.0);
 
-			/* Bottom edge */
-			for (k = 1; k < w-1; k++)
+			// Bottom edge
+			for(k = 1; k < w - 1; k++)
 			{
 				opacity = 0;
-				COMPARE2(h-1,k-1);
-				COMPARE2(h-1,k);
-				COMPARE2(h-1,k+1);
-				COMPARE2(h-2,k-1);
-				COMPARE2(h-2,k);
-				COMPARE2(h-2,k+1);
-				BLEND(h-1,k,6.0);
+				COMPARE2(h - 1, k - 1);
+				COMPARE2(h - 1, k);
+				COMPARE2(h - 1, k + 1);
+				COMPARE2(h - 2, k - 1);
+				COMPARE2(h - 2, k);
+				COMPARE2(h - 2, k + 1);
+				BLEND(h - 1, k, 6.0);
 			}
 
-			/* Bottom right corner */
+			// Bottom right corner
 			opacity = 0;
-			COMPARE2(h-1,w-1);
-			COMPARE2(h-1,w-2);
-			COMPARE2(h-2,w-1);
-			COMPARE2(h-2,w-2);
-			BLEND(h-1,w-1,4.0);
+			COMPARE2(h - 1, w - 1);
+			COMPARE2(h - 1, w - 2);
+			COMPARE2(h - 2, w - 1);
+			COMPARE2(h - 2, w - 2);
+			BLEND(h - 1, w - 1, 4.0);
 		}
 	}
 	else
