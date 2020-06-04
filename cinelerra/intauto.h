4dda533b4d341ef58083627d9014084869b02ce6
Einar Rünkaru <einarrunkaru@gmail.com>
2020-06-04 22:08:32 +0300

    Removed unused IntAuto::identical and operators

--- cinelerra/intauto.h
+++ cinelerra/intauto.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef INTAUTO_H
 #define INTAUTO_H
@@ -34,10 +18,6 @@ public:
 
 	void copy_from(Auto *that);
 	void copy_from(IntAuto *that);
-	int operator==(Auto &that);
-	int operator==(IntAuto &that);
-
-	int identical(IntAuto *that);
 	void load(FileXML *file);
 	void save_xml(FileXML *file);
 	void copy(Auto *that, ptstime start, ptstime end);
