05681a9525621557448f47a8b222006ab7ca83fe
Einar Rünkaru <einarrunkaru@gmail.com>
2020-06-04 21:40:47 +0300

    Removed unused CropAuto::identical

--- cinelerra/cropauto.h
+++ cinelerra/cropauto.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2019 Einar Rünkaru <einarrunkaru@gmail dot com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2019 Einar Rünkaru <einarrunkaru@gmail dot com>
 
 #ifndef CROPAUTO_H
 #define CROPAUTO_H
@@ -36,7 +20,6 @@ public:
 	void copy_from(Auto *that);
 	void copy_from(CropAuto *that);
 
-	int identical(CropAuto *that);
 	void load(FileXML *file);
 	void save_xml(FileXML *file);
 	void copy(Auto *that, ptstime start, ptstime end);
