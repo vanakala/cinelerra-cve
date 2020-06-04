4b8498a569528f5e9377323e1abee25034880124
Einar Rünkaru <einarrunkaru@gmail.com>
2020-06-04 20:15:09 +0300

    Removed unused Track::get_current_plugin

--- cinelerra/track.C
+++ cinelerra/track.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "asset.h"
 #include "aautomation.h"
@@ -640,25 +624,6 @@ void Track::detach_shared_effects(Plugin *plugin, Track *track)
 			--i;
 		}
 	}
-}
-
-Plugin* Track::get_current_plugin(ptstime postime,
-	int num,
-	int use_nudge)
-{
-	if(use_nudge) postime += nudge;
-
-	if(num >= 0 && num < plugins.total)
-	{
-		Plugin *current = plugins.values[num];
-
-		if(current->get_pts() <= postime &&
-			current->end_pts() > postime)
-		{
-			return current;
-		}
-	}
-	return 0;
 }
 
 Plugin* Track::get_current_transition(ptstime position)
