7fe7b5bdc5a3b96d79413e39d57af4ab9f1c438e
Einar Rünkaru <einarrunkaru@gmail.com>
2020-06-25 18:58:22 +0300

    Remove other colormodels from "YUV" video plugin
    Leave only 16 bit RGB and YUV with alpha
    Use SPDX-License tag

--- plugins/yuv/yuv.C
+++ plugins/yuv/yuv.C
@@ -1,32 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bchash.h"
 #include "bcslider.h"
 #include "bctitle.h"
-#include "bctimer.h"
 #include "clip.h"
 #include "colorspaces.h"
 #include "filexml.h"
-#include "language.h"
 #include "picon_png.h"
 #include "pluginvclient.h"
 #include "pluginwindow.h"
@@ -186,156 +168,78 @@ void YUVEffect::read_data(KeyFrame *keyframe)
 	}
 }
 
-#define YUV_MACRO(type, temp_type, max, components, use_yuv) \
-{ \
-	for(int i = 0; i < input->get_h(); i++) \
-	{ \
-		type *in_row = (type*)input->get_row_ptr(i); \
-		type *out_row = (type*)input->get_row_ptr(i); \
-		const float round = (sizeof(type) == 4) ? 0.0 : 0.5; \
- \
-		for(int j = 0; j < w; j++) \
-		{ \
-			if(use_yuv) \
-			{ \
-				int y = (int)((float)in_row[0] * y_scale + round); \
-				int u = (int)((float)(in_row[1] - (max / 2 + 1)) * u_scale + round) + (max / 2 + 1); \
-				int v = (int)((float)(in_row[2] - (max / 2 + 1)) * v_scale + round) + (max / 2 + 1); \
-				out_row[0] = CLIP(y, 0, max); \
-				out_row[1] = CLIP(u, 0, max); \
-				out_row[2] = CLIP(v, 0, max); \
-			} \
-			else \
-			{ \
-				temp_type y, u, v, r, g, b; \
-				if(sizeof(type) == 4) \
-				{ \
-					ColorSpaces::rgb_to_yuv_f(in_row[0], in_row[1], in_row[2], y, u, v); \
-				} \
-				else \
-				if(sizeof(type) == 2) \
-				{ \
-					ColorSpaces::rgb_to_yuv_16(in_row[0], in_row[1], in_row[2], y, u, v); \
-				} \
-				else \
-				{ \
-					ColorSpaces::rgb_to_yuv_8(in_row[0], in_row[1], in_row[2], y, u, v); \
-				} \
- \
-				if(sizeof(type) < 4) \
-				{ \
-					CLAMP(y, 0, max); \
-					CLAMP(u, 0, max); \
-					CLAMP(v, 0, max); \
- \
-					y = temp_type((float)y * y_scale + round); \
-					u = temp_type((float)(u - (max / 2 + 1)) * u_scale + round) + (max / 2 + 1); \
-					v = temp_type((float)(v - (max / 2 + 1)) * v_scale + round) + (max / 2 + 1); \
-				} \
-				else \
-				{ \
-					y = temp_type((float)y * y_scale + round); \
-					u = temp_type((float)u * u_scale + round); \
-					v = temp_type((float)v * v_scale + round); \
-				} \
- \
-				if(sizeof(type) == 4) \
-					ColorSpaces::yuv_to_rgb_f(r, g, b, y, u, v); \
-				else \
-				if(sizeof(type) == 2) \
-					ColorSpaces::yuv_to_rgb_16(r, g, b, y, u, v); \
-				else \
-					ColorSpaces::yuv_to_rgb_8(r, g, b, y, u, v); \
- \
-				out_row[0] = r; \
-				out_row[1] = g; \
-				out_row[2] = b; \
-			} \
-		 \
-			in_row += components; \
-			out_row += components; \
-		} \
-	} \
-}
-
 VFrame *YUVEffect::process_tmpframe(VFrame *input)
 {
-	load_configuration();
+	if(load_configuration())
+		update_gui();
 
 	if(!EQUIV(config.y, 0) || !EQUIV(config.u, 0) || !EQUIV(config.v, 0))
 	{
 		int w = input->get_w();
+		int h = input->get_h();
 
-		double y_scale = (double)(config.y + MAXVALUE) / MAXVALUE;
-		double u_scale = (double)(config.u + MAXVALUE) / MAXVALUE;
-		double v_scale = (double)(config.v + MAXVALUE) / MAXVALUE;
+		double y_scale = (config.y + MAXVALUE) / MAXVALUE;
+		double u_scale = (config.u + MAXVALUE) / MAXVALUE;
+		double v_scale = (config.v + MAXVALUE) / MAXVALUE;
 
 		if(u_scale > 1) u_scale = 1 + (u_scale - 1) * 4;
 		if(v_scale > 1) v_scale = 1 + (v_scale - 1) * 4;
 
 		switch(input->get_color_model())
 		{
-		case BC_RGB_FLOAT:
-			YUV_MACRO(float, float, 1, 3, 0)
-			break;
-
-		case BC_RGB888:
-			YUV_MACRO(unsigned char, int, 0xff, 3, 0)
-			break;
-
-		case BC_YUV888:
-			YUV_MACRO(unsigned char, int, 0xff, 3, 1)
-			break;
-
-		case BC_RGB161616:
-			YUV_MACRO(uint16_t, int, 0xffff, 3, 0)
-			break;
-
-		case BC_YUV161616:
-			YUV_MACRO(uint16_t, int, 0xffff, 3, 1)
-			break;
-
-		case BC_RGBA_FLOAT:
-			YUV_MACRO(float, float, 1, 4, 0)
-			break;
-
-		case BC_RGBA8888:
-			YUV_MACRO(unsigned char, int, 0xff, 4, 0)
-			break;
-
-		case BC_YUVA8888:
-			YUV_MACRO(unsigned char, int, 0xff, 4, 1)
-			break;
-
 		case BC_RGBA16161616:
-			YUV_MACRO(uint16_t, int, 0xffff, 4, 0)
-			break;
+			for(int i = 0; i < h; i++)
+			{
+				uint16_t *in_row = (uint16_t*)input->get_row_ptr(i);
 
-		case BC_YUVA16161616:
-			YUV_MACRO(uint16_t, int, 0xffff, 4, 1)
+				for(int j = 0; j < w; j++)
+				{
+					int y, u, v, r, g, b;
+
+					ColorSpaces::rgb_to_yuv_16(in_row[0], in_row[1], in_row[2], y, u, v);
+
+					y = round((double)y * y_scale);
+					u = round((double)(u - 0x8000) * u_scale);
+					v = round((double)(v - 0x8000) * v_scale);
+					u += 0x8000;
+					v += 0x8000;
+					CLAMP(y, 0, 0xffff);
+					CLAMP(u, 0, 0xffff);
+					CLAMP(v, 0, 0xffff);
+					ColorSpaces::yuv_to_rgb_16(r, g, b, y, u, v);
+
+					in_row[0] = r;
+					in_row[1] = g;
+					in_row[2] = b;
+
+					in_row += 4;
+				}
+			}
 			break;
 
 		case BC_AYUV16161616:
 			for(int i = 0; i < input->get_h(); i++)
 			{
 				uint16_t *in_row = (uint16_t*)input->get_row_ptr(i);
-				uint16_t *out_row = (uint16_t*)input->get_row_ptr(i);
 
 				for(int j = 0; j < w; j++)
 				{
-					int y = ((double)in_row[1] * y_scale);
-					int u = ((double)(in_row[2] - 0x8000) * u_scale);
-					int v = ((double)(in_row[3] - 0x8000) * v_scale);
+					int y = round((double)in_row[1] * y_scale);
+					int u = round((double)(in_row[2] - 0x8000) * u_scale);
+					int v = round((double)(in_row[3] - 0x8000) * v_scale);
+
 					u += 0x8000;
 					v += 0x8000;
-					out_row[1] = CLIP(y, 0, 0xffff);
-					out_row[2] = CLIP(u, 0, 0xffff);
-					out_row[3] = CLIP(v, 0, 0xffff);
+					in_row[1] = CLIP(y, 0, 0xffff);
+					in_row[2] = CLIP(u, 0, 0xffff);
+					in_row[3] = CLIP(v, 0, 0xffff);
 
 					in_row += 4;
-					out_row += 4;
 				}
 			}
+			break;
+		default:
+			unsupported(input->get_color_model());
 			break;
 		}
 	}
