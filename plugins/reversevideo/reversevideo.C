3b8d3781fec55c6e213c775040fdea692a1300c4
Einar Rünkaru <einarrunkaru@gmail.com>
2020-07-12 23:12:20 +0300

    Some fixes in "Reverse" video plugin
    Do not require frames outside the duration of plugin
    Use the frame received from get_frame.
    Take the duration of frame into account
    Use SPDX-License tag.

--- plugins/reversevideo/reversevideo.C
+++ plugins/reversevideo/reversevideo.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "picon_png.h"
 #include "reversevideo.h"
@@ -41,14 +25,15 @@ VFrame *ReverseVideo::process_tmpframe(VFrame *frame)
 {
 	ptstime curpts = frame->get_pts();
 	ptstime end_pts = get_end();
+	ptstime start_pts = get_start();
 	ptstime input_pts;
 
-	input_pts = end_pts - curpts + get_start();
+	input_pts = end_pts - curpts + start_pts - frame->get_duration();
 
-	if(input_pts >= 0 && input_pts < end_pts)
+	if(input_pts >= start_pts && input_pts < end_pts)
 	{
 		frame->set_pts(input_pts);
-		get_frame(frame);
+		frame = get_frame(frame);
 		frame->set_pts(curpts);
 	}
 	return frame;
