46e60191b7ac2ea3a9e98cd3390275976c964cda
Einar Rünkaru <einarrunkaru@gmail.com>
2020-08-04 22:44:00 +0300

    Remove other colormodels from "BandSlide" video transition
    Leave only 16 bit RGB and YUV with alpha.
    handle_event() should return 1 when event is handled

--- plugins/bandslide/bandslide.C
+++ plugins/bandslide/bandslide.C
@@ -30,9 +30,9 @@ BandSlideCount::BandSlideCount(BandSlideMain *plugin,
 
 int BandSlideCount::handle_event()
 {
-	plugin->bands = atol(get_text());
+	plugin->bands = atoi(get_text());
 	plugin->send_configure_change();
-	return 0;
+	return 1;
 }
 
 BandSlideIn::BandSlideIn(BandSlideMain *plugin, 
@@ -54,7 +54,7 @@ int BandSlideIn::handle_event()
 	plugin->direction = 0;
 	window->out->update(0);
 	plugin->send_configure_change();
-	return 0;
+	return 1;
 }
 
 BandSlideOut::BandSlideOut(BandSlideMain *plugin, 
@@ -76,7 +76,7 @@ int BandSlideOut::handle_event()
 	plugin->direction = 1;
 	window->in->update(0);
 	plugin->send_configure_change();
-	return 0;
+	return 1;
 }
 
 
@@ -182,137 +182,119 @@ int BandSlideMain::load_configuration()
 	return 0;
 }
 
-
-#define BANDSLIDE(type, components) \
-{ \
-	if(direction == 0) \
-	{ \
-		int x = round(w * source_pts / get_length()); \
-		for(int i = 0; i < bands; i++) \
-		{ \
-			for(int j = 0; j < band_h; j++) \
-			{ \
-				int row = i * band_h + j; \
-				 \
-				if(row >= 0 && row < h) \
-				{ \
-					type *in_row = (type*)incoming->get_row_ptr(row); \
-					type *out_row = (type*)outgoing->get_row_ptr(row); \
-					 \
-					if(i % 2) \
-					{ \
-						for(int k = 0, l = w - x; k < x; k++, l++) \
-						{ \
-							out_row[k * components + 0] = in_row[l * components + 0]; \
-							out_row[k * components + 1] = in_row[l * components + 1]; \
-							out_row[k * components + 2] = in_row[l * components + 2]; \
-							if(components == 4) out_row[k * components + 3] = in_row[l * components + 3]; \
-						} \
-					} \
-					else \
-					{ \
-						for(int k = w - x, l = 0; k < w; k++, l++) \
-						{ \
-							out_row[k * components + 0] = in_row[l * components + 0]; \
-							out_row[k * components + 1] = in_row[l * components + 1]; \
-							out_row[k * components + 2] = in_row[l * components + 2]; \
-							if(components == 4) out_row[k * components + 3] = in_row[l * components + 3]; \
-						} \
-					} \
-				} \
-			} \
-		} \
-	} \
-	else \
-	{ \
-		int x = w - (int)(round(w * source_pts / get_length())); \
-		for(int i = 0; i < bands; i++) \
-		{ \
-			for(int j = 0; j < band_h; j++) \
-			{ \
-				int row = i * band_h + j; \
- \
-				if(row >= 0 && row < h) \
-				{ \
-					type *in_row = (type*)incoming->get_row_ptr(row); \
-					type *out_row = (type*)outgoing->get_row_ptr(row); \
- \
-					if(i % 2) \
-					{ \
-						int k, l; \
-						for(k = 0, l = w - x; k < x; k++, l++) \
-						{ \
-							out_row[k * components + 0] = out_row[l * components + 0]; \
-							out_row[k * components + 1] = out_row[l * components + 1]; \
-							out_row[k * components + 2] = out_row[l * components + 2]; \
-							if(components == 4) out_row[k * components + 3] = out_row[l * components + 3]; \
-						} \
-						for( ; k < w; k++) \
-						{ \
-							out_row[k * components + 0] = in_row[k * components + 0]; \
-							out_row[k * components + 1] = in_row[k * components + 1]; \
-							out_row[k * components + 2] = in_row[k * components + 2]; \
-							if(components == 4) out_row[k * components + 3] = in_row[k * components + 3]; \
-						} \
-					} \
-					else \
-					{ \
-						for(int k = w - 1, l = x - 1; k >= w - x; k--, l--) \
-						{ \
-							out_row[k * components + 0] = out_row[l * components + 0]; \
-							out_row[k * components + 1] = out_row[l * components + 1]; \
-							out_row[k * components + 2] = out_row[l * components + 2]; \
-							if(components == 4) out_row[k * components + 3] = out_row[l * components + 3]; \
-						} \
-						for(int k = 0; k < w - x; k++) \
-						{ \
-							out_row[k * components + 0] = in_row[k * components + 0]; \
-							out_row[k * components + 1] = in_row[k * components + 1]; \
-							out_row[k * components + 2] = in_row[k * components + 2]; \
-							if(components == 4) out_row[k * components + 3] = in_row[k * components + 3]; \
-						} \
-					} \
-				} \
-			} \
-		} \
-	} \
-}
-
 void BandSlideMain::process_realtime(VFrame *incoming, VFrame *outgoing)
 {
-	load_configuration();
-
+	int cmodel = incoming->get_color_model();
 	int w = incoming->get_w();
 	int h = incoming->get_h();
+
+	load_configuration();
+
 	int band_h = ((bands == 0) ? h : (h / bands + 1));
 
 	if(get_length() < EPSILON)
 		return;
 
-	switch(incoming->get_color_model())
+	switch(cmodel)
 	{
-	case BC_RGB888:
-	case BC_YUV888:
-		BANDSLIDE(unsigned char, 3)
-		break;
-	case BC_RGB_FLOAT:
-		BANDSLIDE(float, 3);
-		break;
-	case BC_RGBA8888:
-	case BC_YUVA8888:
-		BANDSLIDE(unsigned char, 4)
-		break;
-	case BC_RGBA_FLOAT:
-		BANDSLIDE(float, 4);
-		break;
-	case BC_RGB161616:
-	case BC_YUV161616:
-		BANDSLIDE(uint16_t, 3)
-		break;
 	case BC_RGBA16161616:
-	case BC_YUVA16161616:
 	case BC_AYUV16161616:
-		BANDSLIDE(uint16_t, 4)
+		if(direction == 0)
+		{
+			int x = round(w * source_pts / get_length());
+
+			for(int i = 0; i < bands; i++)
+			{
+				for(int j = 0; j < band_h; j++)
+				{
+					int row = i * band_h + j;
+
+					if(row >= 0 && row < h)
+					{
+						uint16_t *in_row = (uint16_t*)incoming->get_row_ptr(row);
+						uint16_t *out_row = (uint16_t*)outgoing->get_row_ptr(row);
+
+						if(i % 2)
+						{
+							for(int k = 0, l = w - x; k < x; k++, l++)
+							{
+								out_row[k * 4 + 0] = in_row[l * 4 + 0];
+								out_row[k * 4 + 1] = in_row[l * 4 + 1];
+								out_row[k * 4 + 2] = in_row[l * 4 + 2];
+								out_row[k * 4 + 3] = in_row[l * 4 + 3];
+							}
+						}
+						else
+						{
+							for(int k = w - x, l = 0; k < w; k++, l++)
+							{
+								out_row[k * 4 + 0] = in_row[l * 4 + 0];
+								out_row[k * 4 + 1] = in_row[l * 4 + 1];
+								out_row[k * 4 + 2] = in_row[l * 4 + 2];
+								out_row[k * 4 + 3] = in_row[l * 4 + 3];
+							}
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			int x = w - (int)(round(w * source_pts / get_length()));
+
+			for(int i = 0; i < bands; i++)
+			{
+				for(int j = 0; j < band_h; j++)
+				{
+					int row = i * band_h + j;
+
+					if(row >= 0 && row < h)
+					{
+						uint16_t *in_row = (uint16_t*)incoming->get_row_ptr(row);
+						uint16_t *out_row = (uint16_t*)outgoing->get_row_ptr(row);
+ 
+						if(i % 2)
+						{
+							int k, l;
+							for(k = 0, l = w - x; k < x; k++, l++)
+							{
+								out_row[k * 4 + 0] = out_row[l * 4 + 0];
+								out_row[k * 4 + 1] = out_row[l * 4 + 1];
+								out_row[k * 4 + 2] = out_row[l * 4 + 2];
+								out_row[k * 4 + 3] = out_row[l * 4 + 3];
+							}
+							for( ; k < w; k++)
+							{
+								out_row[k * 4 + 0] = in_row[k * 4 + 0];
+								out_row[k * 4 + 1] = in_row[k * 4 + 1];
+								out_row[k * 4 + 2] = in_row[k * 4 + 2];
+								out_row[k * 4 + 3] = in_row[k * 4 + 3];
+							}
+						}
+						else
+						{
+							for(int k = w - 1, l = x - 1; k >= w - x; k--, l--)
+							{
+								out_row[k * 4 + 0] = out_row[l * 4 + 0];
+								out_row[k * 4 + 1] = out_row[l * 4 + 1];
+								out_row[k * 4 + 2] = out_row[l * 4 + 2];
+								out_row[k * 4 + 3] = out_row[l * 4 + 3];
+							}
+							for(int k = 0; k < w - x; k++)
+							{
+								out_row[k * 4 + 0] = in_row[k * 4 + 0];
+								out_row[k * 4 + 1] = in_row[k * 4 + 1];
+								out_row[k * 4 + 2] = in_row[k * 4 + 2];
+								out_row[k * 4 + 3] = in_row[k * 4 + 3];
+							}
+						}
+					}
+				}
+			}
+		}
+		break;
+	default:
+		unsupported(cmodel);
 		break;
 	}
 }
