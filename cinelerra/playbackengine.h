869b76e72e084019691c4188f573a3bf67c64e23
Einar Rünkaru <einarrunkaru@gmail.com>
2020-08-10 16:12:10 +0300

    New function PlaybackEngine::release_asset
    Releases provided asset in renderengine
    Removed unused constant FRAME_SPEED
    Use SPDX-License tag.

--- cinelerra/playbackengine.h
+++ cinelerra/playbackengine.h
@@ -1,27 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef PLAYBACKENGINE_H
 #define PLAYBACKENGINE_H
 
+#include "asset.inc"
 #include "arraylist.h"
 #include "audiodevice.inc"
 #include "canvas.inc"
@@ -50,6 +35,8 @@ public:
 	void start_render_engine();
 	void wait_render_engine();
 	void perform_change();
+// Close all files related to asset
+	void release_asset(Asset *asset);
 // Set wait_tracking for events that change the cursor location but
 // be sure to unlock the windows
 	void interrupt_playback(int wait_tracking = 0);
