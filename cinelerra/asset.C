1152270197ee3f34b8c2bb6fd3b7f06c9d9f4ae8
Einar Rünkaru <einarrunkaru@gmail.com>
2020-08-10 22:52:49 +0300

    New function Asset::change_decoder_format_parameter
    Merges active decoder parameters to the parameter list
    The function needs a better name
    Use SPDX-License tag.

--- cinelerra/asset.C
+++ cinelerra/asset.C
@@ -1,24 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
-
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "asset.h"
 #include "awindowgui.h"
@@ -1224,6 +1207,11 @@ void Asset::set_decoder_parameters()
 	FileAVlibs::set_decoder_format_parameters(this);
 	for(int i = 0; i < nb_streams; i++)
 		FileAVlibs::set_stream_decoder_parameters(&streams[i]);
+}
+
+void Asset::change_decoder_format_parameters()
+{
+	FileAVlibs::change_decoder_format_parameters(this);
 }
 
 void Asset::delete_decoder_parameters()
