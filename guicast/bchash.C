125e5a87f068ae6b8d9bbccce16c5dd0742c7b61
Einar Rünkaru <einarrunkaru@gmail.com>
2020-08-21 22:10:14 +0300

    New variant of BC_Hash::get with constant parameter
    Returns pointer of value or defult. Defaults are kept
    in the memory during the whole life of the program.
    The pointer of the value may change only when value is changed
    Fixed indentation. Use SPDX-License tag.

--- guicast/bchash.C
+++ guicast/bchash.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include <inttypes.h>
 #include <stdlib.h>
@@ -170,11 +154,20 @@ char* BC_Hash::get(const char *name, char *default_value)
 	char *val;
 
 	if(val = cache->find_value(name))
-{
+	{
 		if(default_value)
 			strcpy(default_value, val);
 		return val;
+	}
+	return default_value;
 }
+
+const char* BC_Hash::get(const char *name, const char *default_value)
+{
+	char *val;
+
+	if(val = cache->find_value(name))
+		return val;
 	return default_value;
 }
 
