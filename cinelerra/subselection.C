368d695c4b54b63d5077b7b3163a462ea6ff6602
Einar Rünkaru <einarrunkaru@gmail.com>
2021-01-16 15:50:00 +0200

    Use prompt in SubSelection constructor
    Prompt is used it it exists. Prompt is translated here
    Use SPDX-License tag.

--- cinelerra/subselection.C
+++ cinelerra/subselection.C
@@ -1,29 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2016 Einar Rünkaru <einarrunkaru@gmail dot com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2016 Einar Rünkaru <einarrunkaru@gmail dot com>
 
 #include "bcresources.h"
 #include "bclistboxitem.h"
 #include "bcpopupmenu.h"
 #include "bctextbox.h"
 #include "bcsignals.h"
+#include "language.h"
 #include "paramlist.h"
 #include "selection.h"
 #include "subselection.h"
@@ -32,7 +17,7 @@
 #define TEXTBOX_LIST_H 350
 
 SubSelection::SubSelection(int x, int y, int w, BC_WindowBase *base, Param *param)
- : BC_TextBox(x, y, w, 1, param->name)
+ : BC_TextBox(x, y, w, 1, param->prompt ? _(param->prompt) : param->name)
 {
 	Param *current;
 
