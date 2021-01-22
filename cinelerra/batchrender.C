61a19b46eb8828ca243e0038d2a6775db41f7cd4
Einar Rünkaru <einarrunkaru@gmail.com>
2021-01-22 20:57:04 +0200

    Fix crash in BatchRenderThread
    Create misssing edlsession and initialize it
    Fixes crash when the program starts with flag -r
    Use SPDX-License tag.

--- cinelerra/batchrender.C
+++ cinelerra/batchrender.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "asset.h"
 #include "batchrender.h"
@@ -452,6 +436,8 @@ void BatchRenderThread::start_rendering(char *config_path)
 
 // Initialize stuff which MWindow does.
 	MWindow::init_defaults(boot_defaults, config_path);
+	edlsession = new EDLSession();
+	edlsession->load_defaults(boot_defaults);
 	load_defaults(boot_defaults);
 	render_preferences = new Preferences;
 	render_preferences->load_defaults(boot_defaults);
