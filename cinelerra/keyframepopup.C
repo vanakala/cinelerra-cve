26d4d1e8451fa95506b69c1d2ff68aa787430945
Einar Rünkaru <einarrunkaru@gmail.com>
2021-02-02 20:02:54 +0200

    Use new function MWindow::update_gui in KeyframePopup
    Use SPDX-License tag.

--- cinelerra/keyframepopup.C
+++ cinelerra/keyframepopup.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "autos.h"
 #include "bcsignals.h"
@@ -31,7 +15,6 @@
 #include "language.h"
 #include "mainundo.h"
 #include "mwindow.h"
-#include "mwindowgui.h"
 #include "localsession.h"
 #include "plugin.h"
 #include "track.h"
@@ -138,7 +121,7 @@ void KeyframePopup::update(Automation *automation, Autos *autos, Auto *auto_keyf
 
 	if(!PTSEQU(current_position, new_position))
 	{
-		mwindow_global->gui->update(WUPD_SCROLLBARS | WUPD_CANVINCR |
+		mwindow_global->update_gui(WUPD_SCROLLBARS | WUPD_CANVINCR |
 			WUPD_TIMEBAR | WUPD_ZOOMBAR | WUPD_PATCHBAY | WUPD_CLOCK);
 	}
 }
@@ -187,7 +170,7 @@ int KeyframePopupDelete::handle_event()
 	mwindow_global->save_backup();
 	mwindow_global->undo->update_undo(_("delete keyframe"), LOAD_ALL);
 
-	mwindow_global->gui->update(WUPD_CANVINCR);
+	mwindow_global->update_gui(WUPD_CANVINCR);
 	mwindow_global->update_plugin_guis();
 	mwindow_global->sync_parameters();
 	return 1;
@@ -250,7 +233,7 @@ int KeyframePopupTangentMode::handle_event()
 		mwindow_global->save_backup();
 		mwindow_global->undo->update_undo(_("change keyframe tangent mode"), LOAD_ALL);
 
-		mwindow_global->gui->update(WUPD_CANVINCR);
+		mwindow_global->update_gui(WUPD_CANVINCR);
 		mwindow_global->cwindow->update(WUPD_TOOLWIN);
 		mwindow_global->update_plugin_guis();
 		mwindow_global->sync_parameters();
