07605131109d113af0e098b7e62a67d20d7c4db2
Einar Rünkaru <einarrunkaru@gmail.com>
2021-02-03 21:20:57 +0200

    Use new function MWindow::update_gui in TransitionPopup
    Use SPDX-License tag.

--- cinelerra/transitionpopup.C
+++ cinelerra/transitionpopup.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bctitle.h"
 #include "bcbutton.h"
@@ -27,7 +11,6 @@
 #include "edlsession.h"
 #include "language.h"
 #include "mwindow.h"
-#include "mwindowgui.h"
 #include "plugin.h"
 #include "track.h"
 #include "transitionpopup.h"
@@ -96,7 +79,7 @@ int TransitionLengthText::handle_event()
 			gui->transition->track->data_type == TRACK_VIDEO);
 		edlsession->default_transition_length = result;
 		mwindow_global->undo->update_undo(_("transition"), LOAD_EDITS);
-		mwindow_global->gui->update(WUPD_CANVINCR);
+		mwindow_global->update_gui(WUPD_CANVINCR);
 	}
 	return 1;
 }
