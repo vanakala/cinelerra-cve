e0c7ee03904994393f0fcddabddd4bb75eb141f4
Einar Rünkaru <einarrunkaru@gmail.com>
2021-02-03 21:39:47 +0200

    Use new function MWindow::update_gui in ViewMenu items
    Use SPDX-License tag.

--- cinelerra/viewmenu.C
+++ cinelerra/viewmenu.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "autoconf.h"
 #include "cinelerra.h"
@@ -28,7 +12,6 @@
 #include "language.h"
 #include "mainmenu.h"
 #include "mwindow.h"
-#include "mwindowgui.h"
 #include "viewmenu.h"
 
 
@@ -43,7 +26,7 @@ int ShowAssets::handle_event()
 {
 	set_checked(!get_checked());
 	edlsession->show_assets = get_checked();
-	mwindow->gui->update(WUPD_SCROLLBARS | WUPD_CANVINCR | WUPD_PATCHBAY);
+	mwindow->update_gui(WUPD_SCROLLBARS | WUPD_CANVINCR | WUPD_PATCHBAY);
 	mwindow->gwindow->gui->update_toggles();
 	return 1;
 }
@@ -60,7 +43,7 @@ int ShowTitles::handle_event()
 {
 	set_checked(!get_checked());
 	edlsession->show_titles = get_checked();
-	mwindow->gui->update(WUPD_SCROLLBARS | WUPD_CANVINCR | WUPD_PATCHBAY);
+	mwindow->update_gui(WUPD_SCROLLBARS | WUPD_CANVINCR | WUPD_PATCHBAY);
 	mwindow->gwindow->gui->update_toggles();
 	return 1;
 }
