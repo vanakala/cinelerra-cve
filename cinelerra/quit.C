31ad4650b8585bd6b5a1a5873214706203d4f369
Einar Rünkaru <einarrunkaru@gmail.com>
2021-02-03 19:04:55 +0200

    Do not check mwindow->gui in Quit
    mwindow->gui will be private in the future
    Use SPDX-License tag.

--- cinelerra/quit.C
+++ cinelerra/quit.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "mbuttons.h"
 #include "mainerror.h"
@@ -27,7 +11,6 @@
 #include "levelwindowgui.h"
 #include "mainmenu.h"
 #include "mwindow.h"
-#include "mwindowgui.h"
 #include "quit.h"
 #include "question.h"
 #include "render.h"
@@ -83,7 +66,7 @@ void Quit::run()
 	switch(result)
 	{
 	case 0:          // quit
-		if(mwindow->gui)
+		if(mwindow)
 		{
 			mwindow->interrupt_indexes();
 			mwindow->delete_brender();
