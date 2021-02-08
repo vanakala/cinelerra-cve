bc86b0d219da38d2007b8242138fe63df3d88a15
Einar Rünkaru <einarrunkaru@gmail.com>
2021-02-08 21:36:19 +0200

    Removed unused function StatusBar::set_message
    Use SPDX-License tag.

--- cinelerra/statusbar.C
+++ cinelerra/statusbar.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bctitle.h"
 #include "bcprogress.h"
@@ -82,11 +66,6 @@ void StatusBar::resize_event()
 	main_progress_cancel->reposition_window(mwindow->theme->mstatus_cancel_x, 
 		mwindow->theme->mstatus_cancel_y);
 	flash();
-}
-
-void StatusBar::set_message(const char *text)
-{
-	status_text->update(text);
 }
 
 
