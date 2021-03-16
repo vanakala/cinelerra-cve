555ceeed74ef92624a036c70eb8e26e129a83292
Einar Rünkaru <einarrunkaru@gmail.com>
2021-03-16 21:13:13 +0200

    Fixed some typos in guicast
    Use SPDX-License tag.

--- guicast/bcdisplayinfo.C
+++ guicast/bcdisplayinfo.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcdisplayinfo.h"
 #include "clip.h"
@@ -401,7 +385,7 @@ void BC_DisplayInfo::dump_xvext()
 
 	if(BC_DisplayInfo::num_adapters < 0)
 	{
-		printf("BC_DisplayInfo::Xvext dump: data is uninitalized\n");
+		printf("BC_DisplayInfo::Xvext dump: data is uninitialized\n");
 		return;
 	}
 	printf("BC_DisplayInfo::Xvext dump: number of adapters %d\n", BC_DisplayInfo::num_adapters);
--- guicast/bcwindowGL.C
+++ guicast/bcwindowGL.C
@@ -1,24 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2018 Einar Rünkaru <einarrunkaru@gmail dot com>
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2018 Einar Rünkaru <einarrunkaru@gmail dot com>
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #define GL_GLEXT_PROTOTYPES
 #include "bcresources.h"
@@ -34,7 +18,7 @@ int BC_WindowBase::enable_opengl()
 	top_level->sync_display();
 	if(resources.get_glthread()->initialize(top_level->display, win, top_level->screen))
 	{
-		fputs("BC_WindowBase::enable_opengl: Failed to initalize GLThread\n", stdout);
+		fputs("BC_WindowBase::enable_opengl: Failed to initialize GLThread\n", stdout);
 			return 1;
 	}
 	have_gl_context = 1;
