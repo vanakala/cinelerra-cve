720b2a9e884c40bdc6d49aace3673379f01eb101
Einar Rünkaru <einarrunkaru@gmail.com>
2021-04-07 19:41:23 +0300

    New variable KeyFrame::drawn_x
    drawn_x is the position of keyframe in pixels
    Used by TrackPlugin. Use SPDX-License tag.

--- cinelerra/keyframe.C
+++ cinelerra/keyframe.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "filexml.h"
 #include "keyframe.h"
@@ -29,12 +13,14 @@ KeyFrame::KeyFrame()
  : Auto()
 {
 	data = 0;
+	drawn_x = -1;
 }
 
 KeyFrame::KeyFrame(EDL *edl, KeyFrames *autos)
  : Auto(edl, (Autos*)autos)
 {
 	data = 0;
+	drawn_x = -1;
 }
 
 KeyFrame::~KeyFrame()
@@ -154,6 +140,11 @@ int KeyFrame::operator==(Auto &that)
 int KeyFrame::operator==(KeyFrame &that)
 {
 	return identical(&that);
+}
+
+void KeyFrame::drawing(int pos_x)
+{
+	drawn_x = pos_x;
 }
 
 size_t KeyFrame::get_size()
