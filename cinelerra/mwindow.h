vanakala/cinelerra-cve

commit f40ca7f7d43c6a02d6439ab6d32790b4f50447f6

Fix glitches of plugin drag handle handling
Avoid leaving a copy of handle
Do not open context menu when draggin is prepared

cinelerra/trackcanvas.C
@@ -218,7 +218,9 @@ void TrackCanvas::drag_motion()
 	{
 		gui->cursor->hide();
 		draw_overlays();
-		gui->cursor->show();
+		if(drag_handle_pixel < 0)
+			gui->cursor->show();
+		drag_handle_pixel = -1;
 		flash();
 	}
 }
@@ -293,6 +295,8 @@ void TrackCanvas::start_pluginhandle_drag(ptstime position, int drag_cmd,
 			mainsession->drag_button < 0)
 		mainsession->drag_button = 0;
 	mainsession->drag_plugin = plugin;
+	gui->cursor->hide();
+	draw_drag_handle();
 }
 
 void TrackCanvas::drag_motion_event()
@@ -1410,6 +1414,7 @@ void TrackCanvas::draw_drag_handle()
 		mainsession->current_operation == DRAG_PLUGINHANDLE1 ||
 		mainsession->current_operation == DRAG_PLUGINHANDLE2)
 	{
+		overlays_lock->lock("TrackCanvas::draw_drag_handle");
 		int pixel1 = round((mainsession->drag_position -
 			master_edl->local_session->view_start_pts) /
 			master_edl->local_session->zoom_time);
@@ -1425,6 +1430,7 @@ void TrackCanvas::draw_drag_handle()
 			flash();
 			drag_handle_pixel = pixel1;
 		}
+		overlays_lock->unlock();
 	}
 	else
 		drag_handle_pixel = -1;
@@ -1434,12 +1440,14 @@ void TrackCanvas::clear_drag_handle()
 {
 	if(drag_handle_pixel >= 0)
 	{
+		overlays_lock->lock("TrackCanvas::clear_drag_handle");
 		set_color(GREEN);
 		set_inverse();
 		draw_line(drag_handle_pixel, 0, drag_handle_pixel, get_h());
 		set_opaque();
 		flash();
 		drag_handle_pixel = -1;
+		overlays_lock->unlock();
 	}
 }
 
@@ -3055,10 +3063,12 @@ int TrackCanvas::cursor_motion_event()
 	default:
 		if(is_event_win() && cursor_inside())
 		{
-// Update clocks
 			cursor_x = get_cursor_x();
 			cursor_y = get_cursor_y();
 
+			if(do_plugin_handles(cursor_x, cursor_y, &new_cursor))
+				break;
+// Update clocks
 			position = cursor_x * master_edl->local_session->zoom_time +
 				master_edl->local_session->view_start_pts;
 			position = master_edl->align_to_frame(position);
@@ -3073,8 +3083,6 @@ int TrackCanvas::cursor_motion_event()
 			else if(do_edit_handles(cursor_x, cursor_y,
 					0, rerender, update_overlay,
 					new_cursor))
-				break;
-			else if(do_plugin_handles(cursor_x, cursor_y, &new_cursor))
 				break;
 			else if(do_edits(cursor_x, cursor_y, 0, 0,
 					update_overlay, rerender))
@@ -3574,14 +3582,13 @@ int TrackCanvas::button_press_event()
 {
 	int result = 0;
 	int cursor_x, cursor_y;
-	int new_cursor;
+	int new_cursor = default_cursor();
 	int update_opts = 0;
 
-	if(is_event_win() && cursor_inside())
+	if(is_event_win() && cursor_inside() && get_cursor() == new_cursor)
 	{
 		cursor_x = get_cursor_x();
 		cursor_y = get_cursor_y();
-		new_cursor = default_cursor();
 
 		ptstime position = cursor_x *
 			master_edl->local_session->zoom_time +

commit 35eba2c91efa6bc6fc17f879ec44a538ec3fa09e

Redraw only plugin when handle of the plugin is dragged
Whole track must be redrawn when edit handle is dragged
Fixed dragging of edit what was broken in commit f40ca7f7d43

cinelerra/mwindow.h
@@ -341,7 +341,7 @@ public:
 
 	void modify_edithandles(void);
 	void modify_pluginhandles(void);
-	void finish_modify_handles();
+	void finish_modify_handles(int upd_option = 0);
 
 // Send new EDL to caches
 	void age_caches();

cinelerra/mwindowedit.C
@@ -673,7 +673,7 @@ void MWindow::modify_edithandles(void)
 		edlsession->edit_handle_mode[mainsession->drag_button],
 		edlsession->labels_follow_edits);
 
-	finish_modify_handles();
+	finish_modify_handles(WUPD_CANVREDRAW);
 }
 
 void MWindow::modify_pluginhandles()
@@ -687,7 +687,7 @@ void MWindow::modify_pluginhandles()
 }
 
 // Common to edithandles and plugin handles
-void MWindow::finish_modify_handles()
+void MWindow::finish_modify_handles(int upd_option)
 {
 	int edit_mode = edlsession->edit_handle_mode[mainsession->drag_button];
 
@@ -703,7 +703,7 @@ void MWindow::finish_modify_handles()
 	undo->update_undo(_("drag handle"), LOAD_EDITS | LOAD_TIMEBAR);
 	sync_parameters();
 	update_plugin_guis();
-	update_gui(WUPD_SCROLLBARS | WUPD_CANVREDRAW | WUPD_TIMEBAR |
+	update_gui(upd_option | WUPD_SCROLLBARS | WUPD_TIMEBAR |
 		WUPD_ZOOMBAR | WUPD_PATCHBAY | WUPD_CLOCK);
 	cwindow->update(WUPD_POSITION | WUPD_TIMEBAR);
 }

cinelerra/trackcanvas.C
@@ -480,6 +480,13 @@ void TrackCanvas::drag_stop()
 		drag_scroll = 0;
 		clear_drag_handle();
 		mwindow_global->modify_pluginhandles();
+		if(mainsession->drag_plugin->trackplugin)
+		{
+			int px, py, pw, ph;
+
+			plugin_dimensions(mainsession->drag_plugin, px, py, pw, ph);
+			mainsession->drag_plugin->trackplugin->update(px, py, pw, ph);
+		}
 		break;
 
 	case DRAG_PLUGINKEY:
@@ -3597,13 +3604,14 @@ int TrackCanvas::button_press_event()
 {
 	int result = 0;
 	int cursor_x, cursor_y;
-	int new_cursor = default_cursor();
+	int new_cursor;
 	int update_opts = 0;
 
-	if(is_event_win() && cursor_inside() && get_cursor() == new_cursor)
+	if(is_event_win() && cursor_inside())
 	{
 		cursor_x = get_cursor_x();
 		cursor_y = get_cursor_y();
+		new_cursor = default_cursor();
 
 		ptstime position = cursor_x *
 			master_edl->local_session->zoom_time +