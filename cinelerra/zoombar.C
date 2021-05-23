a5d1f5a170d98f34aba93689f0698e383098b32b
Einar Rünkaru <einarrunkaru@gmail.com>
2021-05-23 22:14:29 +0300

    Use update_gui for updating timebar in ZoomBar::set_selection
    Use SPDX-License tag.

--- cinelerra/zoombar.C
+++ cinelerra/zoombar.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "automation.h"
 #include "bcmenuitem.h"
@@ -278,8 +262,7 @@ void ZoomBar::set_selection(int which_one)
 	master_edl->local_session->set_selectionend(
 		master_edl->align_to_frame(end_position));
 
-	mwindow->gui->timebar->update_highlights();
-	mwindow->gui->cursor->update();
+	mwindow->update_gui(WUPD_TOGLIGHTS | WUPD_CURSOR);
 	update();
 	mwindow->sync_parameters(0);
 	mwindow->gui->canvas->flash();
