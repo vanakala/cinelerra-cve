0329919c62f52c9b0f642bd2555a99c701a4fe73
Einar Rünkaru <einarrunkaru@gmail.com>
2021-05-26 21:00:55 +0300

    Use update_gui in TrackScroll::handle_event()
    Use SPDX-License tag.

--- cinelerra/trackscroll.C
+++ cinelerra/trackscroll.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcsignals.h"
 #include "edl.h"
@@ -74,8 +58,6 @@ int TrackScroll::handle_event()
 {
 	master_edl->local_session->track_start = get_value();
 	master_edl->tracks->update_y_pixels(mwindow->theme);
-	mwindow->gui->canvas->draw();
-	mwindow->gui->patchbay->update();
-	mwindow->gui->canvas->flash(1);
+	mwindow->update_gui(WUPD_CANVINCR | WUPD_PATCHBAY);
 	return 1;
 }
