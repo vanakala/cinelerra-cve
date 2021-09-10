fb1575e3f09b340c30b3a0cc80ce61616d4eaad9
Einar Rünkaru <einarrunkaru@gmail.com>
2021-09-10 22:21:13 +0300

    Copy stream number in AFrame::copy_pts
    Add streamm to AFrame::dump output

--- cinelerra/aframe.C
+++ cinelerra/aframe.C
@@ -159,6 +159,7 @@ void AFrame::copy_pts(AFrame *that)
 	duration = 0;
 	samplerate = that->samplerate;
 	channel = that->channel;
+	stream = that->stream;
 }
 
 void AFrame::copy(AFrame *that)
@@ -344,9 +345,9 @@ void AFrame::dump(int indent, int dumpdata)
 
 	printf("%*sAFrame %p dump: trackno %d\n", indent, "", this, trackno);
 	indent += 2;
-	printf("%*spts %.3f[%.3f=%d] src:pts %.3f[%.3f=%d] chnl %d rate %d sample %" PRId64 "\n", indent, "",
+	printf("%*spts %.3f[%.3f=%d] src:pts %.3f[%.3f=%d] stream %d/%d rate %d sample %" PRId64 "\n", indent, "",
 		pts, duration, length, source_pts, source_duration, source_length,
-		channel, samplerate, position);
+		stream, channel, samplerate, position);
 	printf("%*sbuffer %p float_buffer %p buffer_length %d shared %d float_data %d\n", indent, "",
 		buffer, float_buffer, buffer_length, shared, float_data);
 	if(dumpdata && length > 0)
