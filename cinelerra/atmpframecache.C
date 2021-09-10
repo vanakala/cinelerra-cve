d649cd2abe8e499c367377a405ee11eb883ba14e
Einar Rünkaru <einarrunkaru@gmail.com>
2021-09-10 22:29:50 +0300

    Copy stream number to cloned frame in ATmpFrameCache
    Use SPDX-License tag.

--- cinelerra/atmpframecache.C
+++ cinelerra/atmpframecache.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2020 Einar Rünkaru <einarrunkaru@gmail dot com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2020 Einar Rünkaru <einarrunkaru@gmail dot com>
 
 #include "aframe.h"
 #include "atmpframecache.h"
@@ -124,6 +108,7 @@ AFrame *ATmpFrameCache::clone_frame(AFrame *frame)
 		printf("ERR: Cloned the same audio frame %p\n", frame);
 	aframe->set_samplerate(frame->get_samplerate());
 	aframe->channel = frame->channel;
+	aframe->stream = frame->stream;
 	aframe->set_track(frame->get_track());
 	aframe->set_empty();
 
