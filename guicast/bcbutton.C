6cef19cbb264f400e3690d78c78694ad5a93977c
Einar Rünkaru <einarrunkaru@gmail.com>
2021-10-26 22:01:41 +0300

    New function BC_Button::set_enabled
    Sets enabled flag according to value
    Use SPDX-License tag.

--- guicast/bcbutton.C
+++ guicast/bcbutton.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcbutton.h"
 #include "bcpixmap.h"
@@ -28,7 +12,6 @@
 #include "keys.h"
 #include "language.h"
 #include "vframe.h"
-
 
 #include <string.h>
 #include <unistd.h>
@@ -103,16 +86,19 @@ void BC_Button::update_bitmaps(VFrame **data)
 
 void BC_Button::enable()
 {
-	enabled = 1;
-	draw_face();
+	set_enabled(1);
 }
 
 void BC_Button::disable()
 {
-	enabled = 0;
-	draw_face();
+	set_enabled(0);
 }
 
+void BC_Button::set_enabled(int value)
+{
+	enabled = value;
+	draw_face();
+}
 
 void BC_Button::set_underline(int number)
 {
--- guicast/bcbutton.h
+++ guicast/bcbutton.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef BCBUTTON_H
 #define BCBUTTON_H
@@ -27,7 +11,6 @@
 #include "vframe.inc"
 
 #include <stdint.h>
-
 
 class BC_Button : public BC_SubWindow
 {
@@ -43,6 +26,7 @@ public:
 	virtual void draw_face();
 	void disable();
 	void enable();
+	void set_enabled(int value);
 
 	void initialize();
 	virtual void set_images(VFrame **data);
