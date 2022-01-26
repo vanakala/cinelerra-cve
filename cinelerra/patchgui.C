48b8da7c44ee7640c48d49ca16288fbfb06bdf58
Einar Rünkaru <einarrunkaru@gmail.com>
2022-01-26 22:02:38 +0200

    Redraw the whole canvas when Draw button is pressed

--- cinelerra/patchgui.C
+++ cinelerra/patchgui.C
@@ -284,7 +284,7 @@ void PatchGUI::toggle_behavior(int type,
 
 	case Tracks::DRAW:
 		mwindow_global->undo->update_undo(_("draw patch"), LOAD_PATCHES);
-		mwindow_global->update_gui(WUPD_CANVINCR);
+		mwindow_global->update_gui(WUPD_CANVREDRAW);
 		break;
 
 	case Tracks::EXPAND:
