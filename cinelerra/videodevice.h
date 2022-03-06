b8b31ce47ccdf0770bcec3be2df31c559b2c4832
Einar Rünkaru <einarrunkaru@gmail.com>
2022-03-06 17:46:09 +0200

    Removed parameters mwindow from VideoDevice
    Removed not needed, added required includes
    Use global variables.
    Use SPDX-License tag.

--- cinelerra/videodevice.h
+++ cinelerra/videodevice.h
@@ -1,45 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef VIDEODEVICE_H
 #define VIDEODEVICE_H
 
-#include "asset.inc"
 #include "audiodevice.inc"
 #include "bctimer.h"
 #include "canvas.inc"
 #include "edl.inc"
-#include "mwindow.inc"
-#include "mutex.inc"
-#include "preferences.inc"
-#include "thread.h"
 #include "videodevice.inc"
 #include "vdevicex11.inc"
-
+#include "vframe.inc"
 
 class VideoDevice
 {
 public:
 // MWindow is required where picture settings are used, to get the defaults.
-	VideoDevice(MWindow *mwindow = 0);
+	VideoDevice();
 	~VideoDevice();
 
 	void close_all();
@@ -97,6 +76,5 @@ public:
 
 	VDeviceX11 *output_base;
 	VideoOutConfig *out_config;
-	MWindow *mwindow;
 };
 #endif
