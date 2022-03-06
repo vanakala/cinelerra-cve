994e2c040bc7486dc6db1f6684e397b8637a879d
Einar Rünkaru <einarrunkaru@gmail.com>
2022-03-06 17:44:30 +0200

    Corrected usage of includes in vdevicex11
    Removed not needed, added required
    Use SPDX-License tag.

--- cinelerra/vdevicex11.C
+++ cinelerra/vdevicex11.C
@@ -1,32 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcbitmap.h"
-#include "bccapture.h"
-#include "bcsignals.h"
 #include "bcresources.h"
+#include "bcsignals.h"
 #include "canvas.h"
 #include "colormodels.h"
 #include "edl.h"
-#include "mwindow.h"
 #include "playbackconfig.h"
 #include "preferences.h"
 #include "tmpframecache.h"
