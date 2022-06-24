08b7ab260bd3e92ac0b7b28d06e03898de4114df
Einar Rünkaru <einarrunkaru@gmail.com>
2022-06-24 21:28:17 +0300

    Cosmetics in BC_Window
    No functional change
    Use SPDX-License tag.

--- guicast/bcwindow.C
+++ guicast/bcwindow.C
@@ -1,66 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcwindow.h"
 
-
-
-
-BC_Window::BC_Window(const char *title, 
-				int x,
-				int y,
-				int w, 
-				int h, 
-				int minw, 
-				int minh, 
-				int allow_resize,
-				int private_color, 
-				int hide,
-				int bg_color,
-				const char *display_name,
-				int group_it,
-				int options)
+BC_Window::BC_Window(const char *title, int x,int y, int w, int h,
+	int minw, int minh, int allow_resize,
+	int private_color, int hide,
+	int bg_color, const char *display_name,
+	int group_it, int options)
  : BC_WindowBase()
 {
-	create_window(0,
-				title, 
-				x,
-				y,
-				w, 
-				h, 
-				(minw < 0) ? w : minw, 
-				(minh < 0) ? h : minh, 
-				allow_resize,
-				private_color, 
-				hide,
-				bg_color,
-				display_name,
-				MAIN_WINDOW,
-				0,
-				group_it,
-				options);
+	create_window(0, title, x, y, w, h,
+		(minw < 0) ? w : minw, (minh < 0) ? h : minh,
+		allow_resize, private_color, hide,
+		bg_color, display_name, MAIN_WINDOW,
+		0, group_it, options);
 }
-
-
-BC_Window::~BC_Window()
-{
-}
-
--- guicast/bcwindow.h
+++ guicast/bcwindow.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef BCWINDOW_H
 #define BCWINDOW_H
@@ -27,24 +11,12 @@
 class BC_Window : public BC_WindowBase
 {
 public:
-	BC_Window(const char *title, 
-				int x,
-				int y,
-				int w, 
-				int h, 
-				int minw = -1, 
-				int minh = -1, 
-				int allow_resize = 1,
-				int private_color = 0, 
-				int hide = 0,
-				int bg_color = -1,
-				const char *display_name = "",
-				int group_it = 1,
-				int options = 0);
-	virtual ~BC_Window();
-
-private:
+	BC_Window(const char *title, int x, int y, int w, int h,
+		int minw = -1, int minh = -1, int allow_resize = 1,
+		int private_color = 0, int hide = 0,
+		int bg_color = -1, const char *display_name = "",
+		int group_it = 1, int options = 0);
+	virtual ~BC_Window() {};
 };
-
 
 #endif
