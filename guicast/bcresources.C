6b747b5c8784fab54ba5e09fc7ad112caa687d4e
Einar Rünkaru <einarrunkaru@gmail.com>
2022-06-24 22:27:45 +0300

    Use strcpy in BC_Resources
    sprintf is not a string copy function
    Use SPDX-License tag.

--- guicast/bcresources.C
+++ guicast/bcresources.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcdisplayinfo.h"
 #include "bclistbox.inc"
@@ -542,7 +526,7 @@ BC_Resources::BC_Resources()
 	filebox_margin = 110;
 	dirbox_margin = 90;
 	filebox_mode = 0;
-	sprintf(filebox_filter, "*");
+	strcpy(filebox_filter, "*");
 	filebox_w = 640;
 	filebox_h = 480;
 	filebox_columntype[0] = FILEBOX_NAME;
