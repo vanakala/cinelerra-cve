0bd1835eae7fd5608a5605c1d9943d93aedadc83
Einar Rünkaru <einarrunkaru@gmail.com>
2022-12-14 20:41:12 +0200

    Cleanup in Auto
    Removed unneeded includes, unused variable
    Use SPDX-License tag.

--- cinelerra/auto.C
+++ cinelerra/auto.C
@@ -1,29 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "auto.h"
 #include "autos.h"
 #include "bcsignals.h"
-#include "filexml.h"
-#include "track.h"
 
 Auto::Auto()
  : ListItem<Auto>()
@@ -31,7 +13,6 @@ Auto::Auto()
 	this->edl = 0;
 	this->autos = 0;
 	pos_time = 0;
-	skip = 0;
 }
 
 Auto::Auto(EDL *edl, Autos *autos)
@@ -40,7 +21,6 @@ Auto::Auto(EDL *edl, Autos *autos)
 	this->edl = edl;
 	this->autos = autos;
 	pos_time = 0;
-	skip = 0;
 }
 
 Auto& Auto::operator=(Auto& that)
--- cinelerra/auto.h
+++ cinelerra/auto.h
@@ -1,37 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef AUTO_H
 #define AUTO_H
 
 #include "auto.inc"
-#include "bcsignals.h"
+#include "autos.inc"
 #include "datatype.h"
 #include "edl.inc"
 #include "filexml.inc"
 #include "linklist.h"
-#include "autos.inc"
 
-
-// The default constructor is used for menu effects.
 
 class Auto : public ListItem<Auto>
 {
@@ -42,8 +23,8 @@ public:
 	virtual Auto& operator=(Auto &that);
 	virtual int operator==(Auto &that) { return 0; };
 	virtual void copy_from(Auto *that);
-	/* create an interpolation using a1 and a2, (defaulting to previous and next)  */
-	/* if not possible, just fill from a1 (or from template if given) */
+// create an interpolation using a1 and a2, (defaulting to previous and next)
+//  if not possible, just fill from a1 (or from template if given)
 	virtual void interpolate_from(Auto *a1, Auto *a2, ptstime new_postime, Auto *templ);
 	virtual void save_xml(FileXML *file) {};
 	virtual void copy(Auto *that, ptstime start, ptstime end) {};
@@ -52,7 +33,6 @@ public:
 
 	virtual void dump(int indent = 0) {};
 
-	int skip;       // if added by selection event for moves
 	EDL *edl;
 	Autos *autos;
 	ptstime pos_time;
