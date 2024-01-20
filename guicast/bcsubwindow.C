3a4ee24221a726436dafe34feeae6ce2d94a1653
Einar Rünkaru <einarrunkaru@gmail.com>
2024-01-20 22:51:21 +0200

    Cleanup and cosmetics in bcsubwindow
    Removed empty destructor, define empty virtual destructor in header
    Use SPDX-License tag.

--- guicast/bcsubwindow.C
+++ guicast/bcsubwindow.C
@@ -1,38 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcsubwindow.h"
 
 BC_SubWindow::BC_SubWindow(int x, int y, int w, int h, int bg_color)
  : BC_WindowBase()
 {
-	this->x = x; 
-	this->y = y; 
-	this->w = w; 
+	this->x = x;
+	this->y = y;
+	this->w = w;
 	this->h = h;
 	this->bg_color = bg_color;
-}
-
-BC_SubWindow::~BC_SubWindow()
-{
 }
 
 void BC_SubWindow::initialize()
@@ -42,29 +22,12 @@ void BC_SubWindow::initialize()
 	else
 		color_model = bg_color;
 
-	create_window(parent_window, 
-			"Sub Window", 
-			x, 
-			y, 
-			w, 
-			h, 
-			0, 
-			0, 
-			0, 
-			0, 
-			0, 
-			bg_color, 
-			NULL, 
-			SUB_WINDOW, 
-			0,
-			0);
+	create_window(parent_window, "Sub Window",
+		x, y, w, h, 0, 0, 0, 0, 0, bg_color, NULL, SUB_WINDOW, 0, 0);
 }
+
 
 BC_SubWindowList::BC_SubWindowList()
  : ArrayList<BC_WindowBase*>()
-{
-}
-
-BC_SubWindowList::~BC_SubWindowList()
 {
 }
