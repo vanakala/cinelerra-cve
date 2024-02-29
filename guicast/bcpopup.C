77614a002f8f5fce85f81fd7ab23de99b684fd37
Einar Rünkaru <einarrunkaru@gmail.com>
2024-02-29 21:59:07 +0200

    Some cosmetics in BC_Popup
    No functional change intended
    Use SPDX-License tag.

--- guicast/bcpopup.C
+++ guicast/bcpopup.C
@@ -1,32 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcpopup.h"
 
-
-BC_FullScreen::BC_FullScreen(BC_WindowBase *parent_window, int w, int h, 
-		int bg_color,
-		int vm_scale,
-		int hide,
-		BC_Pixmap *bg_pixmap)
+BC_FullScreen::BC_FullScreen(BC_WindowBase *parent_window, int w, int h,
+	int bg_color, int vm_scale, int hide, BC_Pixmap *bg_pixmap)
  : BC_WindowBase()
 {
 	if(parent_window)
@@ -36,55 +16,24 @@ BC_FullScreen::BC_FullScreen(BC_WindowBase *parent_window, int w, int h,
 
 #ifdef HAVE_LIBXXF86VM
 	if(vm_scale)
-		create_window(parent_window,
-			"Fullscreen", 
-			0,
-			0,
-			w,
-			h,
-			w,
-			h,
-			0,
+		create_window(parent_window, "Fullscreen",
+			0, 0, w, h, w, h, 0,
 			parent_window->top_level->private_color,
-			hide,
-			bg_color,
-			NULL,
-			VIDMODE_SCALED_WINDOW,
-			bg_pixmap,
-			0);
+			hide, bg_color, NULL, VIDMODE_SCALED_WINDOW,
+			bg_pixmap, 0);
 	else
 #endif
-	create_window(parent_window,
-			"Fullscreen", 
-			0,
-			0,
-			w, 
-			h, 
-			w, 
-			h, 
-			0,
-			parent_window->top_level->private_color, 
-			hide,
-			bg_color,
-			NULL,
-			POPUP_WINDOW,
-			bg_pixmap,
-			0);
+		create_window(parent_window, "Fullscreen",
+			0, 0, w, h, w, h, 0,
+			parent_window->top_level->private_color,
+			hide, bg_color, NULL, POPUP_WINDOW,
+			bg_pixmap, 0);
 }
 
 
-BC_FullScreen::~BC_FullScreen()
-{
-}
-
-BC_Popup::BC_Popup(BC_WindowBase *parent_window, 
-				int x,
-				int y,
-				int w, 
-				int h, 
-				int bg_color,
-				int hide,
-				BC_Pixmap *bg_pixmap)
+BC_Popup::BC_Popup(BC_WindowBase *parent_window,
+	int x, int y, int w, int h,
+	int bg_color, int hide, BC_Pixmap *bg_pixmap)
  : BC_WindowBase()
 {
 	if(parent_window)
@@ -92,26 +41,8 @@ BC_Popup::BC_Popup(BC_WindowBase *parent_window,
 	else
 		color_model = bg_color;
 
-	create_window(parent_window,
-		"Popup", 
-		x,
-		y,
-		w,
-		h,
-		w,
-		h,
-		0,
-		parent_window->top_level->private_color, 
-		hide,
-		bg_color,
-		NULL,
-		POPUP_WINDOW,
-		bg_pixmap,
-		0);
-}
-
-
-BC_Popup::~BC_Popup()
-{
+	create_window(parent_window, "Popup", x, y, w, h, w, h, 0,
+		parent_window->top_level->private_color, hide,
+		bg_color, NULL, POPUP_WINDOW, bg_pixmap, 0);
 }
 
--- guicast/bcpopup.h
+++ guicast/bcpopup.h
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #ifndef BCPOPUP_H
 #define BCPOPUP_H
@@ -29,31 +13,22 @@
 class BC_Popup : public BC_WindowBase
 {
 public:
-	BC_Popup(BC_WindowBase *parent_window, 
-			int x,
-			int y,
-			int w,
-			int h,
-			int bg_color,
-			int hide = 0,
-			BC_Pixmap *bg_pixmap = 0);
-	virtual ~BC_Popup();
+	BC_Popup(BC_WindowBase *parent_window,
+		int x, int y, int w, int h,
+		int bg_color, int hide = 0,
+		BC_Pixmap *bg_pixmap = 0);
+	virtual ~BC_Popup() {};
 
 	void initialize() {};
-private:
 };
 
 class BC_FullScreen : public BC_WindowBase
 {
 public:
-	BC_FullScreen(BC_WindowBase *parent_window, 
-			int w, 
-			int h, 
-			int bg_color, 
-			int vm_scale,
-			int hide = 0, 
-			BC_Pixmap *bg_pixmap = 0);
-	virtual ~BC_FullScreen();
+	BC_FullScreen(BC_WindowBase *parent_window, int w, int h,
+		int bg_color, int vm_scale, int hide = 0,
+		BC_Pixmap *bg_pixmap = 0);
+	virtual ~BC_FullScreen() {};
 };
 
 #endif
