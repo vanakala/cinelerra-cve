5588a0ff62f82bec965a88d276c10eba05df7474
Einar Rünkaru <einarrunkaru@gmail.com>
2024-03-17 23:41:29 +0200

    Cosmetics in splashgui.C
    Compact the text. Use SPDX-License tag.

--- cinelerra/splashgui.C
+++ cinelerra/splashgui.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcdisplayinfo.h"
 #include "bcprogress.h"
@@ -31,32 +15,18 @@
 
 
 SplashGUI::SplashGUI(VFrame *bg, int x, int y)
- : BC_Window(MWindow::create_title(N_("Loading")),
-		x,
-		y,
-		bg->get_w(),
-		bg->get_h(),
-		-1,
-		-1,
-		0,
-		0,
-		1,
-		-1,
-		"",
-		0,
-		WINDOW_SPLASH)
+ : BC_Window(MWindow::create_title(N_("Loading")), x, y, bg->get_w(), bg->get_h(),
+	-1, -1, 0, 0, 1, -1, "", 0, WINDOW_SPLASH)
 {
 	draw_vframe(bg, 0, 0);
 	flash();
 	show_window();
 
-	add_subwindow(progress = new BC_ProgressBar(5, 
+	add_subwindow(progress = new BC_ProgressBar(5,
 		get_h() - get_resources()->progress_images[0]->get_h() - 5,
-		get_w() - 10,
-		0,
-		0));
+		get_w() - 10, 0, 0));
 	add_subwindow(operation = 
-		new BC_Title(5, 
+		new BC_Title(5,
 			progress->get_y() - get_text_height(MEDIUMFONT) - 5,
 			_("Loading...")));
 }
