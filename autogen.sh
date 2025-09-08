f52db02eca8e0ee55ce2b2ffc7d7c66375a429a6
Einar Rünkaru <einarrunkaru@gmail.com>
2025-09-08 21:49:27 +0300

    Build system cleanup
    There is a lot of changes in newer autoconf/automake
    Removed unused files from AC_CONFIG_MACRO_DIR
    Link needed files from system
    Run autoupdate on configure.ac

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,10 @@
 # This script allows you to create a ./configure script
 # It is only needed when building from GIT when no ./configure script is provided
 #
+# use system provided m4 files from $sysaclocal directory
+# The files are hopefully provided by development packages
+sysaclocal=/usr/share/aclocal
+
 # Make sure that cleans the files created with autotools before run autoreconf.
 rm -f aclocal.m4
 rm -rf autom4te.cache/
@@ -24,6 +28,11 @@ rm -f m4/ltversion.m4
 rm -f m4/lt~obsolete.m4
 rm -f missing
 rm -f po/Makefile.in.in
-
+for nam in alsa.m4 iconv.m4 lib-link.m4 lib-prefix.m4
+do
+    ln -s $sysaclocal/$nam m4/$nam
+done
+touch config.rpath
+autoupdate configure.ac
 autoreconf --install
-intltoolize
+intltoolize -f
