32ab15d72b1734af6a5f76615d4ca9a8eb1e34ca
Einar Rünkaru <einarrunkaru@gmail.com>
2025-09-08 22:17:47 +0300

    Killed compiler warning in rotateframe.C
    Killed gcc 13.3.0 warning "ISO C++17 does not allow 'register'
    storage class specifier"
    Use SPDX-License tag.

--- guicast/rotateframe.C
+++ guicast/rotateframe.C
@@ -1,23 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/*
- * CINELERRA
- * Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
- */
+// This file is a part of Cinelerra-CVE
+// Copyright (C) 2008 Adam Williams <broadcast at earthling dot net>
 
 #include "bcsignals.h"
 #include "clip.h"
@@ -437,9 +421,9 @@ void RotateEngine::coords_to_pixel(SourceCoord &float_pixel, float &input_y, flo
 void RotateEngine::create_matrix()
 {
 // Polar coords of pixel
-	register double k, l, magnitude, angle, offset_angle, offset_angle2;
-	register double x_offset, y_offset;
-	register int i, j;
+	double k, l, magnitude, angle, offset_angle, offset_angle2;
+	double x_offset, y_offset;
+	int i, j;
 	int *int_row;
 	SourceCoord *float_row;
 	float input_x_f, input_y_f;
